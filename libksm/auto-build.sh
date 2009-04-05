2863eab662d60c283ccf077e8515690def3b03fb
John Dickinson <jad@sinodun.com>
2009-04-05 15:05:30 +0000

    The unit tests can now be run by using make check

--- libksm/auto-build.sh
+++ libksm/auto-build.sh
@@ -5,5 +5,5 @@ autoheader
 automake --add-missing
 autoconf
 rm -rf /opt/libksm
-./configure --prefix=/opt/libksm --with-mysql=/usr/local/mysql --with-cunit=/usr/local
+./configure --prefix=/opt/libksm --with-mysql=/usr/local/mysql --with-cunit=/usr/local --with-dbname=test --with-dbhost=test1 --with-dbpass="" --with-dbuser=root
 make clean && make 
