9274ab3535e114f393dbe3d0286570aba44a0bf3
John Dickinson <jad@sinodun.com>
2009-04-06 14:55:31 +0000

    builds on Solaris now

--- enforcer/libksm/auto-build.sh
+++ enforcer/libksm/auto-build.sh
@@ -11,4 +11,8 @@ automake --add-missing
 autoconf
 rm -rf /opt/libksm
 ./configure --prefix=/opt/libksm --with-mysql=/usr/local/mysql --with-cunit=/usr/local --with-dbname=test --with-dbhost=test1 --with-dbpass="" --with-dbuser=root
-make clean && make 
+if test $OS = "SunOS"; then
+  gmake clean && gmake
+else
+  make clean && make 
+fi
