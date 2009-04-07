ebd7427231ab6c3c3c4fb75401d85efb3481705c
John Dickinson <jad@sinodun.com>
2009-04-07 13:38:19 +0000

    but it will build

--- enforcer/libksm/auto-build.sh
+++ enforcer/libksm/auto-build.sh
@@ -24,6 +24,10 @@ echo "Building for $OS"
 ./configure --prefix=/opt/libksm --with-mysql=/usr/local --with-cunit=/usr/local --with-dbname=test --with-dbhost=test1 --with-dbpass="" --with-dbuser=root
 fi
 if test $OS = "SunOS"; then
+echo "Building for $OS"
+./configure --prefix=/opt/libksm --with-mysql=/usr/sfw
+fi
+if test $OS = "SunOS"; then
   gmake clean && gmake
 else
   make clean && make && make check
