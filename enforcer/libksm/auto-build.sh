f81864816fa10bf5337abe91c83c43ef7cfe6878
John Dickinson <jad@sinodun.com>
2009-04-07 13:21:37 +0000

    auto-build.sh works better on multiple platforms - set up for me to do tests you may need something different

--- enforcer/libksm/auto-build.sh
+++ enforcer/libksm/auto-build.sh
@@ -10,9 +10,21 @@ autoheader
 automake --add-missing
 autoconf
 rm -rf /opt/libksm
+echo "Building for $OS"
+if test $OS = "Darwin"; then
+echo "Building for $OS"
 ./configure --prefix=/opt/libksm --with-mysql=/usr/local/mysql --with-cunit=/usr/local --with-dbname=test --with-dbhost=test1 --with-dbpass="" --with-dbuser=root
+fi
+if [ $OS = "Linux" ]; then
+echo "Building for $OS"
+./configure --prefix=/opt/libksm --with-mysql=/usr --with-cunit=/usr/local --with-dbname=test --with-dbhost=test1 --with-dbpass="" --with-dbuser=root
+fi
+if test $OS = "FreeBSD"; then
+echo "Building for $OS"
+./configure --prefix=/opt/libksm --with-mysql=/usr/local --with-cunit=/usr/local --with-dbname=test --with-dbhost=test1 --with-dbpass="" --with-dbuser=root
+fi
 if test $OS = "SunOS"; then
-  gmake clean && gmake
+  gmake clean && gmake && gmake check
 else
-  make clean && make 
+  make clean && make && make check
 fi
