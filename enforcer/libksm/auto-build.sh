fb0a50ba823e0e9137fc13d2b3e69f9912a3501e
John Dickinson <jad@sinodun.com>
2009-04-07 13:33:10 +0000

    tests dont run on kuriputo.kirei.se yet due to lack of cunit and ncurses - will fix that one day

--- enforcer/libksm/auto-build.sh
+++ enforcer/libksm/auto-build.sh
@@ -24,7 +24,7 @@ echo "Building for $OS"
 ./configure --prefix=/opt/libksm --with-mysql=/usr/local --with-cunit=/usr/local --with-dbname=test --with-dbhost=test1 --with-dbpass="" --with-dbuser=root
 fi
 if test $OS = "SunOS"; then
-  gmake clean && gmake && gmake check
+  gmake clean && gmake
 else
   make clean && make && make check
 fi
