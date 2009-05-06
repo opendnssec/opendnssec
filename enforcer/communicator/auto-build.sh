4cabc02ce25e84e4bc79d28552327339b62742f1
Jakob Schlyter <jakob@kirei.se>
2009-05-06 18:46:26 +0000

    bye bye

--- enforcer/communicator/auto-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-aclocal
-autoheader
-automake --add-missing
-autoconf
-CFLAGS="-g" ./configure --prefix=/opt/communicator --with-libksm-include=../../libksm/include --with-libksm-lib=../../libksm/src/.libs
-make clean && make 
--- enforcer/key-generator/auto-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-aclocal
-autoheader
-automake --add-missing
-autoconf
-./configure --prefix=/tmp/jad/opendnssec --with-libksm-include=/tmp/jad/opendnssec/include --with-libksm-lib=/tmp/jad/opendnssec/lib
-make clean && make 
