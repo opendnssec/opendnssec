6c25d2f34b194656d3aff461c21114b2b4ba2d0f
Jakob Schlyter <jakob@kirei.se>
2009-05-06 08:48:32 +0000

    use common license, ok by Nominet via sion

--- enforcer/communicator/src/daemon.c
+++ enforcer/communicator/src/daemon.c
@@ -1,18 +1,42 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2008-2009 Nominet UK. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 /* 
-* daemon.c code needed to get a daemon up and running
-*
-* edit the DAEMONCONFIG and cmlParse function
-* in daemon_util.[c|h] to add options specific
-* to your app
-*
-* gcc -o daemon daemon_util.c daemon.c
-*
-* Most of this is based on stuff I have seen in NSD
-*
-* Copyright (c) 2008 2009, John Dickinson. All rights reserved.
-*
-* See LICENSE for the license.
-*/
+ * daemon.c code needed to get a daemon up and running
+ *
+ * edit the DAEMONCONFIG and cmlParse function
+ * in daemon_util.[c|h] to add options specific
+ * to your app
+ *
+ * gcc -o daemon daemon_util.c daemon.c
+ *
+ * Most of this is based on stuff I have seen in NSD
+ */
 
 #include <sys/types.h>
 #include <syslog.h>
