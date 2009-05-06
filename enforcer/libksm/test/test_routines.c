fba00becb89db437d5ef66dd0d7c569b6d1e6e55
Jakob Schlyter <jakob@kirei.se>
2009-05-06 08:39:01 +0000

    use common license, ok by Nominet via sion

--- enforcer/libksm/test/test_routines.c
+++ enforcer/libksm/test/test_routines.c
@@ -1,3 +1,31 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2008-2009 Nominet UK. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 /*+
  * test_routines.c - Unit Testing Routines
  *
@@ -6,23 +34,6 @@
  *
  *      The unit testing routines made use of the CUint framework,
  *      available from http://cunit.sourcefourge.net.
- *
- *
- * Copyright:
- *      Copyright 2008 Nominet
- *      
- * Licence:
- *      Licensed under the Apache Licence, Version 2.0 (the "Licence");
- *      you may not use this file except in compliance with the Licence.
- *      You may obtain a copy of the Licence at
- *      
- *          http://www.apache.org/licenses/LICENSE-2.0
- *      
- *      Unless required by applicable law or agreed to in writing, software
- *      distributed under the Licence is distributed on an "AS IS" BASIS,
- *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *      See the Licence for the specific language governing permissions and
- *      limitations under the Licence.
 -*/
 
 #include <assert.h>
