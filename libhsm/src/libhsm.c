709a12705d1195d385ee595237110c9551c3fb07
Jelte Jansen <jelte@nlnetlabs.nl>
2009-05-08 19:14:21 +0000

    well if we have __EXTENSIONS__ it's better to use strdup

--- libhsm/src/libhsm.c
+++ libhsm/src/libhsm.c
@@ -68,12 +68,8 @@ hsm_module_new(const char *name, const char *path)
     module = malloc(sizeof(hsm_module_t));
     module->id = 0; /*TODO what should this value be?*/
     strl = strlen(name) + 1;
-    module->name = malloc(strl);
-    strlcpy(module->name, name, strl);
-    strl = strlen(path) + 1;
-    module->path = malloc(strl);
-    strlcpy(module->path, path, strl);
-    strcpy(module->path, path);
+    module->name = strdup(name);
+    module->path = strdup(path);
     module->handle = NULL;
     module->sym = NULL;
     return module;
