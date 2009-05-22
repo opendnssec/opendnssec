9509eb1eb64b607b2bdafbe99254d0647d4aac0a
Jakob Schlyter <jakob@kirei.se>
2009-05-22 13:19:29 +0000

    spring cleaning

--- libhsm/src/oldlibhsm.h
+++
@@ -1,7 +0,0 @@
-int PK_LinkLib(char *pklib);
-int PK_UnlinkLib();
-int PK_Startup(int slot, char *pin);
-int PK_Shutdown();
-void PK_RemoveObject(uuid_t uuid);
-void PK_ListObjects();
-void PK_GenerateObject(long keysize);
