edd8255a03bca13af0734b519d62e9270048bbaf
Jakob Schlyter <jakob@kirei.se>
2009-05-26 14:57:54 +0000

    more

--- libhsm/checks/hsmtest.c
+++ libhsm/checks/hsmtest.c
@@ -39,35 +39,45 @@ main (int argc, char *argv[])
 	//hsm_ctx_t *ctx;
 	hsm_key_t **keys;
 	hsm_key_t *key = NULL;
+	uuid_t *uuid;
 	size_t key_count = 0;
 	size_t i;
 	ldns_rr_list *rrset;
 	ldns_rr *rr, *sig;
 	ldns_status status;
 	hsm_sign_params_t *sign_params;
-	int generate = 0;
-	int sign = 0;
-	int show = 1;
-	int delete = 0;
+
+	int do_generate = 0;
+	int do_sign = 0;
+	int do_show = 1;
+	int do_delete = 0;
+	int do_random = 0;
+
 	int res;
 	uint32_t r32;
 	uint64_t r64;
 
+	const char *repository = "regress";
+
 	int ch;
 
-	while ((ch = getopt(argc, argv, "gsd")) != -1) {
+	while ((ch = getopt(argc, argv, "gsdr")) != -1) {
 		switch (ch) {
 		case 'g':
-			generate = 1;
-			show = 0;
+			do_generate = 1;
+			do_show = 0;
 			break;
 		case 's':
-			sign = 1;
-			show = 0;
+			do_sign = 1;
+			do_show = 0;
 			break;
 		case 'd':
-			delete = 1;
-			show = 0;
+			do_delete = 1;
+			do_show = 0;
+			break;
+		case 'r':
+			do_show = 0;
+			do_random = 1;
 			break;
 		default:
 			fprintf(stderr, "usage: %s [-gsd]\n", argv[0]);
@@ -78,29 +88,53 @@ main (int argc, char *argv[])
 	fprintf(stdout, "Starting HSM lib test\n");
 	result = hsm_open(getenv("HSMTEST_CONF"), hsm_prompt_pin, NULL);
 	fprintf(stdout, "hsm_open result: %d\n", result);
+
 	//ctx = hsm_create_context();
 	/*printf("global: ");
 	hsm_print_ctx(NULL);
 	printf("my: ");
 	hsm_print_ctx(ctx);
 	*/
-	keys = hsm_list_keys(NULL, &key_count);
-	printf("I have found %u keys\n", (unsigned int) key_count);
-	/* let's just use the very first key we find, and throw away
-	 * the rest */
-	for (i = 0; i < key_count; i++) {
-		if (show) {
-			hsm_print_key(keys[i]);
+
+	if (do_generate) {
+		/* generate new key */
+		key = hsm_generate_rsa_key(NULL, repository, 1024);
+		if (key) {
+			printf("Created key:\n");
+			hsm_print_key(key);
+		} else {
+			printf("Error creating key, bad token name?\n");
 		}
-		if ((sign || delete) && !key) {
-			key = hsm_find_key_by_uuid(NULL, (const uuid_t *) keys[i]->uuid);
+	} else {
+		/* use any key found */
+		keys = hsm_list_keys(NULL, &key_count);
+		printf("I have found %u keys\n", (unsigned int) key_count);
+
+		/* let's just use the very first key we find and throw away the rest */
+		for (i = 0; i < key_count; i++) {
+			if (do_show) {
+				hsm_print_key(keys[i]);
+			}
+			if ((do_sign || do_delete) && !key) {
+				/* only use keys with uuid */
+				uuid = hsm_get_uuid(NULL, keys[i]);
+				if (uuid) {
+					key = hsm_find_key_by_uuid(NULL, uuid);
+				}
+			}
+			hsm_key_free(keys[i]);
 		}
-		hsm_key_free(keys[i]);
+		free(keys);
 	}
-	free(keys);
+
+	/* print "active" key */
+	if (key) {
+		hsm_print_key(key);
+		hsm_key_free(key);
+	}
 
 	/* do some signing with it */
-	if (sign) {
+	if (do_sign) {
 		rrset = ldns_rr_list_new();
 		status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se.	IN	A	123.123.123.123", 0, NULL, NULL);
 		if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
@@ -118,32 +152,25 @@ main (int argc, char *argv[])
 		ldns_rr_free(sig);
 		hsm_sign_params_free(sign_params);
 	}
-	if (generate) {
-		key = hsm_generate_rsa_key(NULL, "softHSM", 1024);
-		if (key) {
-			printf("Created key:\n");
-			hsm_print_key(key);
-		} else {
-			printf("Error creating key bad token name?\n");
-		}
-	}
-	if (delete) {
+
+	if (do_delete) {
 		printf("Delete key:\n");
 		hsm_print_key(key);
 		//res = hsm_remove_key(ctx, key);
 		res = hsm_remove_key(NULL, key);
 		printf("Deleted key. Result: %d\n", res);
 	}
-	if (key) {
-		hsm_print_key(key);
-		hsm_key_free(key);
+
+	/* test random{32,64} functions */
+	if (do_random) {
+		r32 = hsm_random32(NULL);
+		printf("random 32: %u\n", r32);
+		r64 = hsm_random64(NULL);
+		printf("random 64: %llu\n", r64);
 	}
 
-	r32 = hsm_random32(NULL);
-	printf("random 32: %u\n", r32);
-	r64 = hsm_random64(NULL);
-	printf("random 64: %llu\n", r64);
 	//hsm_destroy_context(ctx);
+
 	result = hsm_close();
 	fprintf(stdout, "all done! hsm_close result: %d\n", result);
 	return 0;
