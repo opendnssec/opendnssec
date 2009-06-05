fc21c6f504cd6fab3eb856686acf57e0396ce3c0
Jelte Jansen <jelte@nlnetlabs.nl>
2009-06-05 09:08:19 +0000

    added two test scripts that run through the signing process the way the engine would

---
+++ signer/signer_engine/test/single_sign_op_nsec.sh
@@ -0,0 +1,32 @@
+export SOFTHSM_CONF=./softhsm.conf
+TMP_COMMANDS=`mktemp signtestXXXXX`
+TMP_ZONE=`mktemp signzoneXXXXX`
+# set the commands in a tmp file
+
+echo "" > $TMP_COMMANDS
+echo ":origin tjeb.nl" >> $TMP_COMMANDS
+echo ":soa_ttl 3600" >> $TMP_COMMANDS
+echo ":soa_minimum 3600" >> $TMP_COMMANDS
+echo ":expiration 20090701000000" >> $TMP_COMMANDS
+echo ":inception 20090605072316" >> $TMP_COMMANDS
+echo ":jitter 60" >> $TMP_COMMANDS
+echo ":refresh 20090606072616" >> $TMP_COMMANDS
+echo ":add_module test test_token ../../../softHSM/src/lib/.libs/libsofthsm.so 1111" >> $TMP_COMMANDS
+echo ":add_ksk fc9ead5ec20345ca87e61836ff327ce1 5 257" >> $TMP_COMMANDS
+echo ":add_zsk e715319b2fe146bfb4fa8e9b2c780d21 5 256" >> $TMP_COMMANDS
+# prepare the zone
+echo "Preparing zone"
+cat unsigned_zones/tjeb.nl > $TMP_ZONE
+echo "Adding keys"
+../../tools/create_dnskey -r test -n test_token -m ../../../softHSM/src/lib/.libs/libsofthsm.so -p 1111 -o tjeb.nl -a 5 -f 257 -t 3600 fc9ead5ec20345ca87e61836ff327ce1 >> $TMP_ZONE
+../../tools/create_dnskey -r test -n test_token -m ../../../softHSM/src/lib/.libs/libsofthsm.so -p 1111 -o tjeb.nl -a 5 -f 256 -t 3600 e715319b2fe146bfb4fa8e9b2c780d21 >> $TMP_ZONE
+# sort, nsec, and sign it
+echo "Signing zone"
+../../tools/zone_reader -f $TMP_ZONE -o tjeb.nl |\
+../../tools/nseccer >> $TMP_COMMANDS
+
+../../tools/signer -f $TMP_COMMANDS -w signed_zones/tjeb.nl
+
+echo "Cleaning up"
+rm $TMP_COMMANDS
+rm $TMP_ZONE
