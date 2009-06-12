e89f9b93b84b46a6b4443606396fc5e6bbaee9ae
Jakob Schlyter <jakob@kirei.se>
2009-06-12 12:11:44 +0000

    add basic thread support

--- libhsm/checks/hsmspeed.c
+++ libhsm/checks/hsmspeed.c
@@ -32,45 +32,116 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #include <libhsm.h>
 #include <libhsmdns.h>
 
+#define PTHREAD_THREADS_MAX 2048
+
 extern char *optarg;
 char *progname = NULL;
+
+typedef struct {
+	unsigned int id;
+	hsm_ctx_t *ctx;
+	hsm_key_t *key;
+	unsigned int iterations;
+} sign_arg_t;
 
 void
 usage ()
 {
-	fprintf(stderr, "usage: %s [-f config] [-i iterations] [-s keysize]\n", progname);
+	fprintf(stderr,
+		"usage: %s "
+		"[-f config] [-i iterations] [-s keysize] [-t threads]\n",
+		progname);
 }
 
-int
-main (int argc, char *argv[])
+void *
+sign (void *arg)
 {
-	int result;
-	size_t i;
-
 	hsm_ctx_t *ctx = NULL;
 	hsm_key_t *key = NULL;
-	unsigned int keysize = 1024;
-	unsigned int iterations = 1;
+	
+	size_t i;
+	unsigned int iterations = 0;
 
 	ldns_rr_list *rrset;
 	ldns_rr *rr, *sig, *dnskey_rr;
 	ldns_status status;
 	hsm_sign_params_t *sign_params;
 
+	sign_arg_t *sign_arg = arg;
+
+	ctx = sign_arg->ctx;
+	key = sign_arg->key;
+	iterations = sign_arg->iterations;
+
+	fprintf(stderr, "Signer thread #%d started...\n", sign_arg->id);
+
+	/* Prepare dummy RRset for signing */
+	rrset = ldns_rr_list_new();
+	status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 123.123.123.123", 0, NULL, NULL);
+	if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
+	status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 124.124.124.124", 0, NULL, NULL);
+	if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
+	sign_params = hsm_sign_params_new();
+	sign_params->algorithm = LDNS_RSASHA1;
+	sign_params->owner = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_DNAME, "opendnssec.se.");
+	dnskey_rr = hsm_get_dnskey(ctx, key, sign_params);
+	sign_params->keytag = ldns_calc_keytag(dnskey_rr);
+
+	/* Do some signing */
+	for (i=0; i<iterations; i++) {
+		sig = hsm_sign_rrset(ctx, rrset, key, sign_params);	
+		if (! sig) {
+			fprintf(stderr, "hsm_sign_rrset() returned error\n");
+			break;
+		}				
+		ldns_rr_free(sig);
+	}
+	
+	/* Clean up */
+	ldns_rr_list_deep_free(rrset);
+	hsm_sign_params_free(sign_params);
+	ldns_rr_free(dnskey_rr);	
+	hsm_destroy_context(ctx);
+	
+	fprintf(stderr, "Signer thread #%d done.\n", sign_arg->id);
+	
+	pthread_exit(NULL);
+}
+
+
+int
+main (int argc, char *argv[])
+{
+	int result;
+
+	hsm_ctx_t *ctx = NULL;
+	hsm_key_t *key = NULL;
+	unsigned int keysize = 1024;
+	unsigned int iterations = 1;
+	unsigned int threads = 1;
+
 	static struct timeval start,end;
 
 	char *config = NULL;
 	const char *repository = "regress";
+	
+	sign_arg_t sign_arg_array[PTHREAD_THREADS_MAX];
+	
+	pthread_t      thread_array[PTHREAD_THREADS_MAX];
+    pthread_attr_t thread_attr;
+    void          *thread_status;
 
 	int ch;
+	unsigned int n;
 
 	progname = argv[0];
 
-	while ((ch = getopt(argc, argv, "f:i:s:")) != -1) {
+	while ((ch = getopt(argc, argv, "f:i:s:t:")) != -1) {
 		switch (ch) {
 		case 'f':
 			config = strdup(optarg);
@@ -80,6 +151,9 @@ main (int argc, char *argv[])
 			break;
 		case 's':
 			keysize = atoi(optarg);
+			break;
+		case 't':
+			threads = atoi(optarg);
 			break;
 		default:
 			usage();
@@ -119,29 +193,44 @@ main (int argc, char *argv[])
 		exit(-1);
 	}
 	
-	/* Prepare dummy RRset for signing */
-	rrset = ldns_rr_list_new();
-	status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 123.123.123.123", 0, NULL, NULL);
-	if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
-	status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 124.124.124.124", 0, NULL, NULL);
-	if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
-	sign_params = hsm_sign_params_new();
-	sign_params->algorithm = LDNS_RSASHA1;
-	sign_params->owner = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_DNAME, "opendnssec.se.");
-	dnskey_rr = hsm_get_dnskey(ctx, key, sign_params);
-	sign_params->keytag = ldns_calc_keytag(dnskey_rr);
-
-	/* Do some signing */
-	fprintf(stderr, "Signing %d RRsets...\n", iterations);
-	gettimeofday(&start, NULL);
-	for (i=0; i<iterations; i++) {
-		sig = hsm_sign_rrset(ctx, rrset, key, sign_params);	
-		if (! sig) {
-			fprintf(stderr, "hsm_sign_rrset() returned error\n");
-			break;
-		}				
-		ldns_rr_free(sig);
+	/* Prepare threads */
+	pthread_attr_init(&thread_attr);
+	pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
+	
+	for (n=0; n<threads; n++) {
+		sign_arg_array[n].id = n;
+		sign_arg_array[n].ctx = hsm_create_context();
+		if (! sign_arg_array[n].ctx) {
+			fprintf(stderr, "hsm_create_context() returned error\n");
+			exit(-1);
+		}
+		sign_arg_array[n].key = key;
+		sign_arg_array[n].iterations = iterations;
 	}
+
+	fprintf(stderr, "Signing %d RRsets using %d %s...\n",
+		iterations, threads, (threads > 1 ? "threads" : "thread"));
+	gettimeofday(&start, NULL);
+	
+	/* Create threads for signing */
+	for (n=0; n<threads; n++) {
+		result = pthread_create(&thread_array[n], &thread_attr,
+			sign, (void *) &sign_arg_array[n]);
+		if (result) {
+			fprintf(stderr, "pthread_create() returned %d\n", result);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	/* Wait for threads to finish */
+	for (n=0; n<threads; n++) {
+		result = pthread_join(thread_array[n], &thread_status);
+		if (result) {
+			fprintf(stderr, "pthread_join() returned %d\n", result);
+			exit(EXIT_FAILURE);			
+		}
+	}
+
 	gettimeofday(&end, NULL);
 	fprintf(stderr, "Signing done.\n");
 
@@ -152,7 +241,7 @@ main (int argc, char *argv[])
 	double speed = iterations / elapsed;
 	printf("%d signatures, %.2f sig/s\n", iterations, speed);
 	
-	/* Delete temporary key*/
+	/* Delete temporary key */
 	fprintf(stderr, "Deleting temporary key...\n");
 	result = hsm_remove_key(ctx, key);
 	if (result) {
@@ -161,10 +250,7 @@ main (int argc, char *argv[])
 	}
 
 	/* Clean up */
-	ldns_rr_list_deep_free(rrset);
-	hsm_sign_params_free(sign_params);
-	ldns_rr_free(dnskey_rr);	
-	if (ctx) hsm_destroy_context(ctx);
+	hsm_destroy_context(ctx);
 	(void) hsm_close();
 	if (config) free(config);
 	
