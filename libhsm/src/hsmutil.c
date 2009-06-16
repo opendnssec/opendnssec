b2e72a5c370bd58a2222c4d048f0460fa03e5d95
Jakob Schlyter <jakob@kirei.se>
2009-06-16 09:23:35 +0000

    more

--- libhsm/src/hsmutil.c
+++ libhsm/src/hsmutil.c
@@ -50,6 +50,22 @@ usage ()
 		progname);
 }
 
+void
+usage_generate ()
+{
+	fprintf(stderr,
+		"usage: %s [-f config] [command] generate [repository] rsa [keysize]\n",
+		progname);
+}
+
+void
+usage_dnskey ()
+{
+	fprintf(stderr,
+		"usage: %s [-f config] [command] dnskey [id] [name]\n",
+		progname);
+}
+
 int
 cmd_list (int argc, char *argv[])
 {
@@ -67,11 +83,11 @@ cmd_list (int argc, char *argv[])
 		printf("Listing keys in repository: %s\n", repository);
 		keys = hsm_list_keys_repository(NULL, &key_count, repository);	
 	} else {
-		printf("Listing keys in all repositories\n");	
+		printf("Listing keys in all repositories.\n");	
 		keys = hsm_list_keys(NULL, &key_count);
 	}
 
-	printf("%d keys found.\n", key_count);
+	printf("%u keys found.\n", (unsigned int) key_count);
 	
 	if (!keys) {
 		return -1;
@@ -88,7 +104,37 @@ cmd_list (int argc, char *argv[])
 int
 cmd_generate (int argc, char *argv[])
 {
-	printf("generate...\n");
+	char *repository = NULL;
+	char *algorithm = NULL;
+	unsigned int keysize = 1024;
+	
+	hsm_key_t *key = NULL;
+	
+	if (argc != 3) {
+		usage_generate();
+		return -1;
+	}
+
+	repository = strdup(argv[0]);
+	algorithm = strdup(argv[1]);
+	keysize = atoi(argv[2]);
+	
+	if (!strcasecmp(algorithm, "rsa")) {
+		printf("Generating key in repository. %s\n", repository);
+		key = hsm_generate_rsa_key(NULL, repository, keysize);
+		if (key) {
+			printf("Key generation successful.\n");
+			hsm_print_key(key);
+			hsm_key_free(key);
+		} else {
+			printf("Key generation failed.\n");
+		}
+		
+	} else {
+		printf("Unknown algorithm: %s\n", algorithm);
+		return -1;
+	}
+	
 	return 0;
 }
 
@@ -102,7 +148,40 @@ cmd_delete (int argc, char *argv[])
 int
 cmd_dnskey (int argc, char *argv[])
 {
-	printf("dnskey...\n");
+	char *id;
+	char *name;
+
+	hsm_key_t *key = NULL;
+	ldns_rr *dnskey_rr;
+	hsm_sign_params_t *sign_params;
+
+	if (argc != 2) {
+		usage_dnskey();
+		return -1;
+	}
+
+	id = strdup(argv[0]);
+	name = strdup(argv[1]);
+
+	key = hsm_find_key_by_id(NULL, id);
+	
+	if (!key) {
+		printf("Key not found: %s\n", id);
+		return -1;
+	}
+	
+	sign_params = hsm_sign_params_new();
+	sign_params->algorithm = LDNS_RSASHA1;
+	sign_params->owner = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_DNAME, name);
+	dnskey_rr = hsm_get_dnskey(NULL, key, sign_params);
+	sign_params->keytag = ldns_calc_keytag(dnskey_rr);
+
+	ldns_rr_print(stdout, dnskey_rr);
+
+	hsm_sign_params_free(sign_params);
+	ldns_rr_free(dnskey_rr);
+	hsm_key_free(key);
+
 	return 0;
 }
 
