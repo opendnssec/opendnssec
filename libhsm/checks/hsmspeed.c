0cdfd12fcfe6e8c1fb03ef8031a58d7ea97b9d83
Jakob Schlyter <jakob@kirei.se>
2009-06-26 06:43:16 +0000

    tabs to 4 spaces

--- libhsm/checks/hsmspeed.c
+++ libhsm/checks/hsmspeed.c
@@ -43,218 +43,218 @@ extern char *optarg;
 char *progname = NULL;
 
 typedef struct {
-	unsigned int id;
-	hsm_ctx_t *ctx;
-	hsm_key_t *key;
-	unsigned int iterations;
+    unsigned int id;
+    hsm_ctx_t *ctx;
+    hsm_key_t *key;
+    unsigned int iterations;
 } sign_arg_t;
 
 void
 usage ()
 {
-	fprintf(stderr,
-		"usage: %s "
-		"[-f config] [-i iterations] [-s keysize] [-t threads]\n",
-		progname);
+    fprintf(stderr,
+        "usage: %s "
+        "[-f config] [-i iterations] [-s keysize] [-t threads]\n",
+        progname);
 }
 
 void *
 sign (void *arg)
 {
-	hsm_ctx_t *ctx = NULL;
-	hsm_key_t *key = NULL;
-	
-	size_t i;
-	unsigned int iterations = 0;
+    hsm_ctx_t *ctx = NULL;
+    hsm_key_t *key = NULL;
+    
+    size_t i;
+    unsigned int iterations = 0;
 
-	ldns_rr_list *rrset;
-	ldns_rr *rr, *sig, *dnskey_rr;
-	ldns_status status;
-	hsm_sign_params_t *sign_params;
+    ldns_rr_list *rrset;
+    ldns_rr *rr, *sig, *dnskey_rr;
+    ldns_status status;
+    hsm_sign_params_t *sign_params;
 
-	sign_arg_t *sign_arg = arg;
+    sign_arg_t *sign_arg = arg;
 
-	ctx = sign_arg->ctx;
-	key = sign_arg->key;
-	iterations = sign_arg->iterations;
+    ctx = sign_arg->ctx;
+    key = sign_arg->key;
+    iterations = sign_arg->iterations;
 
-	fprintf(stderr, "Signer thread #%d started...\n", sign_arg->id);
+    fprintf(stderr, "Signer thread #%d started...\n", sign_arg->id);
 
-	/* Prepare dummy RRset for signing */
-	rrset = ldns_rr_list_new();
-	status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 123.123.123.123", 0, NULL, NULL);
-	if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
-	status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 124.124.124.124", 0, NULL, NULL);
-	if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
-	sign_params = hsm_sign_params_new();
-	sign_params->algorithm = LDNS_RSASHA1;
-	sign_params->owner = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_DNAME, "opendnssec.se.");
-	dnskey_rr = hsm_get_dnskey(ctx, key, sign_params);
-	sign_params->keytag = ldns_calc_keytag(dnskey_rr);
+    /* Prepare dummy RRset for signing */
+    rrset = ldns_rr_list_new();
+    status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 123.123.123.123", 0, NULL, NULL);
+    if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
+    status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 124.124.124.124", 0, NULL, NULL);
+    if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
+    sign_params = hsm_sign_params_new();
+    sign_params->algorithm = LDNS_RSASHA1;
+    sign_params->owner = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_DNAME, "opendnssec.se.");
+    dnskey_rr = hsm_get_dnskey(ctx, key, sign_params);
+    sign_params->keytag = ldns_calc_keytag(dnskey_rr);
 
-	/* Do some signing */
-	for (i=0; i<iterations; i++) {
-		sig = hsm_sign_rrset(ctx, rrset, key, sign_params);	
-		if (! sig) {
-			fprintf(stderr, "hsm_sign_rrset() returned error\n");
-			break;
-		}				
-		ldns_rr_free(sig);
-	}
-	
-	/* Clean up */
-	ldns_rr_list_deep_free(rrset);
-	hsm_sign_params_free(sign_params);
-	ldns_rr_free(dnskey_rr);	
-	hsm_destroy_context(ctx);
-	
-	fprintf(stderr, "Signer thread #%d done.\n", sign_arg->id);
-	
-	pthread_exit(NULL);
+    /* Do some signing */
+    for (i=0; i<iterations; i++) {
+        sig = hsm_sign_rrset(ctx, rrset, key, sign_params); 
+        if (! sig) {
+            fprintf(stderr, "hsm_sign_rrset() returned error\n");
+            break;
+        }               
+        ldns_rr_free(sig);
+    }
+    
+    /* Clean up */
+    ldns_rr_list_deep_free(rrset);
+    hsm_sign_params_free(sign_params);
+    ldns_rr_free(dnskey_rr);    
+    hsm_destroy_context(ctx);
+    
+    fprintf(stderr, "Signer thread #%d done.\n", sign_arg->id);
+    
+    pthread_exit(NULL);
 }
 
 
 int
 main (int argc, char *argv[])
 {
-	int result;
+    int result;
 
-	hsm_ctx_t *ctx = NULL;
-	hsm_key_t *key = NULL;
-	unsigned int keysize = 1024;
-	unsigned int iterations = 1;
-	unsigned int threads = 1;
+    hsm_ctx_t *ctx = NULL;
+    hsm_key_t *key = NULL;
+    unsigned int keysize = 1024;
+    unsigned int iterations = 1;
+    unsigned int threads = 1;
 
-	static struct timeval start,end;
+    static struct timeval start,end;
 
-	char *config = NULL;
-	const char *repository = "default";
-	
-	sign_arg_t sign_arg_array[PTHREAD_THREADS_MAX];
-	
-	pthread_t      thread_array[PTHREAD_THREADS_MAX];
-	pthread_attr_t thread_attr;
-	void          *thread_status;
+    char *config = NULL;
+    const char *repository = "default";
+    
+    sign_arg_t sign_arg_array[PTHREAD_THREADS_MAX];
+    
+    pthread_t      thread_array[PTHREAD_THREADS_MAX];
+    pthread_attr_t thread_attr;
+    void          *thread_status;
 
-	int ch;
-	unsigned int n;
+    int ch;
+    unsigned int n;
 
-	progname = argv[0];
+    progname = argv[0];
 
-	while ((ch = getopt(argc, argv, "f:i:s:t:")) != -1) {
-		switch (ch) {
-		case 'f':
-			config = strdup(optarg);
-			break;
-		case 'i':
-			iterations = atoi(optarg);
-			break;
-		case 's':
-			keysize = atoi(optarg);
-			break;
-		case 't':
-			threads = atoi(optarg);
-			break;
-		default:
-			usage();
-			exit(1);
-		}
-	}
+    while ((ch = getopt(argc, argv, "f:i:s:t:")) != -1) {
+        switch (ch) {
+        case 'f':
+            config = strdup(optarg);
+            break;
+        case 'i':
+            iterations = atoi(optarg);
+            break;
+        case 's':
+            keysize = atoi(optarg);
+            break;
+        case 't':
+            threads = atoi(optarg);
+            break;
+        default:
+            usage();
+            exit(1);
+        }
+    }
 
-	if (!config) {
-		usage();
-		exit(1);
-	}
+    if (!config) {
+        usage();
+        exit(1);
+    }
 
-	/* Open HSM library */
-	fprintf(stderr, "Opening HSM Library...\n");
-	result = hsm_open(config, hsm_prompt_pin, NULL);	
-	if (result) {
-		fprintf(stderr, "hsm_open() returned %d\n", result);
-		exit(-1);
-	}
+    /* Open HSM library */
+    fprintf(stderr, "Opening HSM Library...\n");
+    result = hsm_open(config, hsm_prompt_pin, NULL);    
+    if (result) {
+        fprintf(stderr, "hsm_open() returned %d\n", result);
+        exit(-1);
+    }
 
-	/* Create HSM context */
-	ctx = hsm_create_context();
-	if (! ctx) {
-		fprintf(stderr, "hsm_create_context() returned error\n");
-		exit(-1);
-	}
+    /* Create HSM context */
+    ctx = hsm_create_context();
+    if (! ctx) {
+        fprintf(stderr, "hsm_create_context() returned error\n");
+        exit(-1);
+    }
 
-	/* Generate a temporary key */
-	fprintf(stderr, "Generating temporary key...\n");
-	key = hsm_generate_rsa_key(ctx, repository, keysize);
-	if (key) {
-		char *id = hsm_get_key_id(ctx, key);
-		fprintf(stderr, "Temporary key created: %s\n", id);
-		free(id);			
-	} else {
-		fprintf(stderr, "hsm_generate_rsa_key() returned %d\n", result);
-		exit(-1);
-	}
-	
-	/* Prepare threads */
-	pthread_attr_init(&thread_attr);
-	pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
-	
-	for (n=0; n<threads; n++) {
-		sign_arg_array[n].id = n;
-		sign_arg_array[n].ctx = hsm_create_context();
-		if (! sign_arg_array[n].ctx) {
-			fprintf(stderr, "hsm_create_context() returned error\n");
-			exit(-1);
-		}
-		sign_arg_array[n].key = key;
-		sign_arg_array[n].iterations = iterations;
-	}
+    /* Generate a temporary key */
+    fprintf(stderr, "Generating temporary key...\n");
+    key = hsm_generate_rsa_key(ctx, repository, keysize);
+    if (key) {
+        char *id = hsm_get_key_id(ctx, key);
+        fprintf(stderr, "Temporary key created: %s\n", id);
+        free(id);           
+    } else {
+        fprintf(stderr, "hsm_generate_rsa_key() returned %d\n", result);
+        exit(-1);
+    }
+    
+    /* Prepare threads */
+    pthread_attr_init(&thread_attr);
+    pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_JOINABLE);
+    
+    for (n=0; n<threads; n++) {
+        sign_arg_array[n].id = n;
+        sign_arg_array[n].ctx = hsm_create_context();
+        if (! sign_arg_array[n].ctx) {
+            fprintf(stderr, "hsm_create_context() returned error\n");
+            exit(-1);
+        }
+        sign_arg_array[n].key = key;
+        sign_arg_array[n].iterations = iterations;
+    }
 
-	fprintf(stderr, "Signing %d RRsets using %d %s...\n",
-		iterations, threads, (threads > 1 ? "threads" : "thread"));
-	gettimeofday(&start, NULL);
-	
-	/* Create threads for signing */
-	for (n=0; n<threads; n++) {
-		result = pthread_create(&thread_array[n], &thread_attr,
-			sign, (void *) &sign_arg_array[n]);
-		if (result) {
-			fprintf(stderr, "pthread_create() returned %d\n", result);
-			exit(EXIT_FAILURE);
-		}
-	}
+    fprintf(stderr, "Signing %d RRsets using %d %s...\n",
+        iterations, threads, (threads > 1 ? "threads" : "thread"));
+    gettimeofday(&start, NULL);
+    
+    /* Create threads for signing */
+    for (n=0; n<threads; n++) {
+        result = pthread_create(&thread_array[n], &thread_attr,
+            sign, (void *) &sign_arg_array[n]);
+        if (result) {
+            fprintf(stderr, "pthread_create() returned %d\n", result);
+            exit(EXIT_FAILURE);
+        }
+    }
 
-	/* Wait for threads to finish */
-	for (n=0; n<threads; n++) {
-		result = pthread_join(thread_array[n], &thread_status);
-		if (result) {
-			fprintf(stderr, "pthread_join() returned %d\n", result);
-			exit(EXIT_FAILURE);			
-		}
-	}
+    /* Wait for threads to finish */
+    for (n=0; n<threads; n++) {
+        result = pthread_join(thread_array[n], &thread_status);
+        if (result) {
+            fprintf(stderr, "pthread_join() returned %d\n", result);
+            exit(EXIT_FAILURE);         
+        }
+    }
 
-	gettimeofday(&end, NULL);
-	fprintf(stderr, "Signing done.\n");
+    gettimeofday(&end, NULL);
+    fprintf(stderr, "Signing done.\n");
 
-	/* Report results */
-	end.tv_sec -= start.tv_sec;
-	end.tv_usec-= start.tv_usec;
-	double elapsed =(double)(end.tv_sec)+(double)(end.tv_usec)*.000001;
-	double speed = iterations / elapsed * threads;
-	printf("%d %s, %d signatures per thread, %.2f sig/s (RSA %d bits)\n",
-		threads, (threads > 1 ? "threads" : "thread"), iterations,
-		speed, keysize);
-	
-	/* Delete temporary key */
-	fprintf(stderr, "Deleting temporary key...\n");
-	result = hsm_remove_key(ctx, key);
-	if (result) {
-		fprintf(stderr, "hsm_remove_key() returned %d\n", result);
-		exit(-1);
-	}
+    /* Report results */
+    end.tv_sec -= start.tv_sec;
+    end.tv_usec-= start.tv_usec;
+    double elapsed =(double)(end.tv_sec)+(double)(end.tv_usec)*.000001;
+    double speed = iterations / elapsed * threads;
+    printf("%d %s, %d signatures per thread, %.2f sig/s (RSA %d bits)\n",
+        threads, (threads > 1 ? "threads" : "thread"), iterations,
+        speed, keysize);
+    
+    /* Delete temporary key */
+    fprintf(stderr, "Deleting temporary key...\n");
+    result = hsm_remove_key(ctx, key);
+    if (result) {
+        fprintf(stderr, "hsm_remove_key() returned %d\n", result);
+        exit(-1);
+    }
 
-	/* Clean up */
-	hsm_destroy_context(ctx);
-	(void) hsm_close();
-	if (config) free(config);
-	
-	return 0;
+    /* Clean up */
+    hsm_destroy_context(ctx);
+    (void) hsm_close();
+    if (config) free(config);
+    
+    return 0;
 }
--- libhsm/checks/hsmtest.c
+++ libhsm/checks/hsmtest.c
@@ -42,210 +42,210 @@ char *progname = NULL;
 void
 usage ()
 {
-	fprintf(stderr, "usage: %s [-f config] [-gsd]\n", progname);
+    fprintf(stderr, "usage: %s [-f config] [-gsd]\n", progname);
 }
 
 int
 main (int argc, char *argv[])
 {
-	int result;
-	hsm_ctx_t *ctx;
-	hsm_key_t **keys;
-	hsm_key_t *key = NULL;
-	char *id;
-	size_t key_count = 0;
-	size_t i;
-	ldns_rr_list *rrset;
-	ldns_rr *rr, *sig, *dnskey_rr;
-	ldns_status status;
-	hsm_sign_params_t *sign_params;
+    int result;
+    hsm_ctx_t *ctx;
+    hsm_key_t **keys;
+    hsm_key_t *key = NULL;
+    char *id;
+    size_t key_count = 0;
+    size_t i;
+    ldns_rr_list *rrset;
+    ldns_rr *rr, *sig, *dnskey_rr;
+    ldns_status status;
+    hsm_sign_params_t *sign_params;
 
-	int do_generate = 0;
-	int do_sign = 0;
-	int do_delete = 0;
-	int do_random = 0;
+    int do_generate = 0;
+    int do_sign = 0;
+    int do_delete = 0;
+    int do_random = 0;
 
-	int res;
-	uint32_t r32;
-	uint64_t r64;
+    int res;
+    uint32_t r32;
+    uint64_t r64;
 
-	char *config = NULL;
-	const char *repository = "default";
+    char *config = NULL;
+    const char *repository = "default";
 
-	progname = argv[0];
+    progname = argv[0];
 
-	int ch;
+    int ch;
 
-	while ((ch = getopt(argc, argv, "hgsdrf:")) != -1) {
-		switch (ch) {
-		case 'f':
-			config = strdup(optarg);
-			break;
-		case 'g':
-			do_generate = 1;
-			break;
-		case 'h':
-			usage();
-			exit(0);
-			break;
-		case 's':
-			do_sign = 1;
-			break;
-		case 'd':
-			do_delete = 1;
-			break;
-		case 'r':
-			do_random = 1;
-			break;
-		default:
-			usage();
-			exit(1);
-		}
-	}
+    while ((ch = getopt(argc, argv, "hgsdrf:")) != -1) {
+        switch (ch) {
+        case 'f':
+            config = strdup(optarg);
+            break;
+        case 'g':
+            do_generate = 1;
+            break;
+        case 'h':
+            usage();
+            exit(0);
+            break;
+        case 's':
+            do_sign = 1;
+            break;
+        case 'd':
+            do_delete = 1;
+            break;
+        case 'r':
+            do_random = 1;
+            break;
+        default:
+            usage();
+            exit(1);
+        }
+    }
 
-	if (!config) {
-		usage();
-		exit(1);
-	}
+    if (!config) {
+        usage();
+        exit(1);
+    }
 
-	/*
-	 * Open HSM library
-	 */
-	fprintf(stdout, "Starting HSM lib test\n");
-	result = hsm_open(config, hsm_prompt_pin, NULL);
-	fprintf(stdout, "hsm_open result: %d\n", result);
+    /*
+     * Open HSM library
+     */
+    fprintf(stdout, "Starting HSM lib test\n");
+    result = hsm_open(config, hsm_prompt_pin, NULL);
+    fprintf(stdout, "hsm_open result: %d\n", result);
 
-	/*
-	 * Create HSM context
-	 */
-	ctx = hsm_create_context();
-	printf("global: ");
-	hsm_print_ctx(NULL);
-	printf("my: ");
-	hsm_print_ctx(ctx);
+    /*
+     * Create HSM context
+     */
+    ctx = hsm_create_context();
+    printf("global: ");
+    hsm_print_ctx(NULL);
+    printf("my: ");
+    hsm_print_ctx(ctx);
 
-	/*
-	 * Generate a new key OR find any key with an ID
-	 */
-	if (do_generate) {
-		key = hsm_generate_rsa_key(ctx, repository, 1024);
+    /*
+     * Generate a new key OR find any key with an ID
+     */
+    if (do_generate) {
+        key = hsm_generate_rsa_key(ctx, repository, 1024);
 
-		if (key) {
-			printf("\nCreated key!\n");
-			hsm_print_key(key);
-			printf("\n");
-		} else {
-			printf("Error creating key, bad token name?\n");
+        if (key) {
+            printf("\nCreated key!\n");
+            hsm_print_key(key);
+            printf("\n");
+        } else {
+            printf("Error creating key, bad token name?\n");
             hsm_print_error(ctx);
-			exit(1);
-		}
-	} else if (do_sign || do_delete) {
-		keys = hsm_list_keys(ctx, &key_count);
-		printf("I have found %u keys\n", (unsigned int) key_count);
+            exit(1);
+        }
+    } else if (do_sign || do_delete) {
+        keys = hsm_list_keys(ctx, &key_count);
+        printf("I have found %u keys\n", (unsigned int) key_count);
 
-		/* let's just use the very first key we find and throw away the rest */
-		for (i = 0; i < key_count && !key; i++) {
-			printf("\nFound key!\n");
-			hsm_print_key(keys[i]);
+        /* let's just use the very first key we find and throw away the rest */
+        for (i = 0; i < key_count && !key; i++) {
+            printf("\nFound key!\n");
+            hsm_print_key(keys[i]);
 
-			id = hsm_get_key_id(ctx, keys[i]);
+            id = hsm_get_key_id(ctx, keys[i]);
 
-			if (id) {
-				printf("Using key ID: %s\n", id);
-				if (key) hsm_key_free(key);
-				key = hsm_find_key_by_id(ctx, id);
-				printf("ptr: 0x%p\n", (void *) key);
-				free(id);
-			} else {
-				printf("Got no key ID (broken key?), skipped...\n");
-			}
+            if (id) {
+                printf("Using key ID: %s\n", id);
+                if (key) hsm_key_free(key);
+                key = hsm_find_key_by_id(ctx, id);
+                printf("ptr: 0x%p\n", (void *) key);
+                free(id);
+            } else {
+                printf("Got no key ID (broken key?), skipped...\n");
+            }
 
-			hsm_key_free(keys[i]);
-		}
-		free(keys);
+            hsm_key_free(keys[i]);
+        }
+        free(keys);
 
-		if (!key) {
-			printf("Failed to find useful key\n");
-			exit(1);
-		}
-	}
+        if (!key) {
+            printf("Failed to find useful key\n");
+            exit(1);
+        }
+    }
 
-	/*
-	 * Do some signing
-	 */
-	if (do_sign) {
-		printf("\nSigning with:\n");
-		hsm_print_key(key);
-		printf("\n");
+    /*
+     * Do some signing
+     */
+    if (do_sign) {
+        printf("\nSigning with:\n");
+        hsm_print_key(key);
+        printf("\n");
 
-		rrset = ldns_rr_list_new();
+        rrset = ldns_rr_list_new();
 
-		status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 123.123.123.123", 0, NULL, NULL);
-		if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
-		status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 124.124.124.124", 0, NULL, NULL);
-		if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
+        status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 123.123.123.123", 0, NULL, NULL);
+        if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
+        status = ldns_rr_new_frm_str(&rr, "regress.opendnssec.se. IN A 124.124.124.124", 0, NULL, NULL);
+        if (status == LDNS_STATUS_OK) ldns_rr_list_push_rr(rrset, rr);
 
-		sign_params = hsm_sign_params_new();
-		sign_params->algorithm = LDNS_RSASHA1;
-		sign_params->owner = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_DNAME, "opendnssec.se.");
-		dnskey_rr = hsm_get_dnskey(ctx, key, sign_params);
-		sign_params->keytag = ldns_calc_keytag(dnskey_rr);
+        sign_params = hsm_sign_params_new();
+        sign_params->algorithm = LDNS_RSASHA1;
+        sign_params->owner = ldns_rdf_new_frm_str(LDNS_RDF_TYPE_DNAME, "opendnssec.se.");
+        dnskey_rr = hsm_get_dnskey(ctx, key, sign_params);
+        sign_params->keytag = ldns_calc_keytag(dnskey_rr);
 
-		sig = hsm_sign_rrset(ctx, rrset, key, sign_params);
+        sig = hsm_sign_rrset(ctx, rrset, key, sign_params);
         if (sig) {
-		    ldns_rr_list_print(stdout, rrset);
-    		ldns_rr_print(stdout, sig);
-    		ldns_rr_print(stdout, dnskey_rr);
-    		ldns_rr_free(sig);
+            ldns_rr_list_print(stdout, rrset);
+            ldns_rr_print(stdout, sig);
+            ldns_rr_print(stdout, dnskey_rr);
+            ldns_rr_free(sig);
         } else {
             hsm_print_error(ctx);
             exit(-1);
         }
 
-		/* cleanup */
-		ldns_rr_list_deep_free(rrset);
-		hsm_sign_params_free(sign_params);
-		ldns_rr_free(dnskey_rr);
-	}
+        /* cleanup */
+        ldns_rr_list_deep_free(rrset);
+        hsm_sign_params_free(sign_params);
+        ldns_rr_free(dnskey_rr);
+    }
 
-	/*
-	 * Delete key
-	 */
-	if (do_delete) {
-		printf("\nDelete key:\n");
-		hsm_print_key(key);
-		//res = hsm_remove_key(ctx, key);
-		res = hsm_remove_key(ctx, key);
-		printf("Deleted key. Result: %d\n", res);
-		printf("\n");
-	}
+    /*
+     * Delete key
+     */
+    if (do_delete) {
+        printf("\nDelete key:\n");
+        hsm_print_key(key);
+        //res = hsm_remove_key(ctx, key);
+        res = hsm_remove_key(ctx, key);
+        printf("Deleted key. Result: %d\n", res);
+        printf("\n");
+    }
 
-	if (key) hsm_key_free(key);
+    if (key) hsm_key_free(key);
 
-	/*
-	 * Test random{32,64} functions
-	 */
-	if (do_random) {
-		r32 = hsm_random32(ctx);
-		printf("random 32: %u\n", r32);
-		r64 = hsm_random64(ctx);
-		printf("random 64: %llu\n", r64);
-	}
+    /*
+     * Test random{32,64} functions
+     */
+    if (do_random) {
+        r32 = hsm_random32(ctx);
+        printf("random 32: %u\n", r32);
+        r64 = hsm_random64(ctx);
+        printf("random 64: %llu\n", r64);
+    }
 
-	/*
-	 * Destroy HSM context
-	 */
-	if (ctx) {
-		hsm_destroy_context(ctx);
-	}
+    /*
+     * Destroy HSM context
+     */
+    if (ctx) {
+        hsm_destroy_context(ctx);
+    }
 
-	/*
-	 * Close HSM library
-	 */
-	result = hsm_close();
-	fprintf(stdout, "all done! hsm_close result: %d\n", result);
+    /*
+     * Close HSM library
+     */
+    result = hsm_close();
+    fprintf(stdout, "all done! hsm_close result: %d\n", result);
 
-	if (config) free(config);
-	
-	return 0;
+    if (config) free(config);
+    
+    return 0;
 }
