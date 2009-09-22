aad2c0b5acda6f5bb5065dd62e9b18924dafb7ba
Matthijs Mekking <matthijs@nlnetlabs.nl>
2009-09-22 11:51:47 +0000

    one config to rule them all modify location of 'F'

--- signer/tools/zone_fetcher.c
+++ signer/tools/zone_fetcher.c
@@ -43,19 +43,26 @@
 #include <libxml/xmlsave.h>
 
 /**
+ * Config.
+ */
+typedef struct config_struct config_type;
+struct config_struct
+{
+    char* server_name;
+    char* tsig_name;
+    char* tsig_algo;
+    char* tsig_secret;
+    int use_tsig;
+};
+
+/**
  * Zone list.
  */
 typedef struct zonelist_struct zonelist_type;
 struct zonelist_struct
 {
     char* name;
-    char* axfr_output;
-    char* axfr_config;
     char* input_file;
-    char* server_name;
-    char* tsig_name;
-    char* tsig_algo;
-    char* tsig_secret;
     zonelist_type* next;
 };
 
@@ -69,13 +76,35 @@ usage(FILE *out)
     fprintf(out, "-z <file>\tThe zonelist.xml <file>\n");
 }
 
+static config_type*
+new_config(void)
+{
+    config_type* cfg = (config_type*) malloc(sizeof(config_type));
+    cfg->server_name = NULL;
+    cfg->tsig_name = NULL;
+    cfg->tsig_algo = NULL;
+    cfg->tsig_secret = NULL;
+    return cfg;
+}
+
+static void
+free_config(config_type* cfg)
+{
+    if (cfg) {
+        if (cfg->tsig_name)   free((void*) cfg->tsig_name);
+        if (cfg->tsig_algo)   free((void*) cfg->tsig_algo);
+        if (cfg->tsig_secret) free((void*) cfg->tsig_secret);
+        if (cfg->server_name) free((void*) cfg->server_name);
+        free((void*) cfg);
+    }
+}
+
 static zonelist_type*
-new_zone(char* zone_name, char* input_file, char* axfr_config)
+new_zone(char* zone_name, char* input_file)
 {
     zonelist_type* zlt = (zonelist_type*) malloc(sizeof(zonelist_type));
     zlt->name = strdup(zone_name);
     zlt->input_file = strdup(input_file);
-    zlt->axfr_config = strdup(axfr_config);
     zlt->next = NULL;
     return zlt;
 }
@@ -85,83 +114,58 @@ free_zonelist(zonelist_type* zlt)
 {
     if (zlt) {
         free_zonelist(zlt->next);
-        if (zlt->tsig_name)            free((void*) zlt->tsig_name);
-        if (zlt->tsig_algo)            free((void*) zlt->tsig_algo);
-        if (zlt->tsig_secret)          free((void*) zlt->tsig_secret);
-        if (zlt->server_name)          free((void*) zlt->server_name);
         free((void*) zlt->name);
         free((void*) zlt->input_file);
-        free((void*) zlt->axfr_config);
         free((void*) zlt);
     }
 }
 
 static int
-read_axfr_config(zonelist_type* zonelist)
+read_axfr_config(const char* filename, config_type* cfg)
 {
     int ret;
     int use_tsig = 0;
-    char* tag_name, *zone_name, *tsig_name, *tsig_algo, *tsig_secret, *server_name;
+    char* tag_name, *tsig_name, *tsig_algo, *tsig_secret, *server_name;
 
     xmlTextReaderPtr reader = NULL;
     xmlDocPtr doc = NULL;
     xmlXPathContextPtr xpathCtx = NULL;
     xmlXPathObjectPtr xpathObj = NULL;
-    xmlChar *name_expr = (unsigned char*) "name";
-    xmlChar *tsig_expr = (unsigned char*) "//Zone/TSIG";
-    xmlChar *tsig_name_expr = (unsigned char*) "//Zone/TSIG/Name";
-    xmlChar *tsig_algo_expr = (unsigned char*) "//Zone/TSIG/Algorithm";
-    xmlChar *tsig_secret_expr = (unsigned char*) "//Zone/TSIG/Secret";
-    xmlChar *server_expr = (unsigned char*) "//Zone/Server";
+    xmlChar *tsig_expr = (unsigned char*) "//ZoneFetch/Default/TSIG";
+    xmlChar *tsig_name_expr = (unsigned char*) "//ZoneFetch/Default/TSIG/Name";
+    xmlChar *tsig_algo_expr = (unsigned char*) "//ZoneFetch/Default/TSIG/Algorithm";
+    xmlChar *tsig_secret_expr = (unsigned char*) "//ZoneFetch/Default/TSIG/Secret";
+    xmlChar *server_expr = (unsigned char*) "//ZoneFetch/Default/Address";
 
     /* In case zonelist is huge use the XmlTextReader API so that we don't hold the whole file in memory */
-    reader = xmlNewTextReaderFilename(zonelist->axfr_config);
+    reader = xmlNewTextReaderFilename(filename);
     if (reader != NULL) {
         ret = xmlTextReaderRead(reader);
         while (ret == 1) {
             tag_name = (char*) xmlTextReaderLocalName(reader);
             /* Found <Zone> */
-            if (strncmp(tag_name, "Zone", 4) == 0 &&
+            if (strncmp(tag_name, "ZoneFetch", 8) == 0 &&
                 xmlTextReaderNodeType(reader) == 1) {
-                /* Get the zone name (TODO what if this is null?) */
-                zone_name = (char*) xmlTextReaderGetAttribute(reader, name_expr);
-                /* Make sure that we got something */
-                if (zone_name == NULL || strcmp(zone_name, zonelist->name) != 0) {
-                    /* error */
-                    fprintf(stderr, "zone_fetcher: error extracting zone name from %s\n", zonelist->axfr_config);
-                    /* Don't return? try to parse the rest of the zones? */
-                    ret = xmlTextReaderRead(reader);
-                    free(tag_name);
-                    continue;
-                }
+
                 /* Expand this node and get the rest of the info with XPath */
                 xmlTextReaderExpand(reader);
                 doc = xmlTextReaderCurrentDoc(reader);
                 if (doc == NULL) {
-                    fprintf(stderr, "zone_fetcher: can not read AXFR config for zone \"%s\"; skipping", zone_name);
-                    /* Don't return? try to parse the rest of the zones? */
-                    ret = xmlTextReaderRead(reader);
-                    free(tag_name);
-                    continue;
+                    fprintf(stderr, "zone_fetcher: can not read config file %s\n", filename);
+                    exit(1);
                 }
                 xpathCtx = xmlXPathNewContext(doc);
                 if (xpathCtx == NULL) {
-                    fprintf(stderr, "zone_fetcher: can not create XPath context for AXFR config for zone \"%s\"; "
-                        "skipping zone", zone_name);
-                    /* Don't return? try to parse the rest of the zones? */
-                    ret = xmlTextReaderRead(reader);
-                    free(tag_name);
-                    continue;
+                    fprintf(stderr, "zone_fetcher: can not create XPath context for %s\n",
+                        filename);
+                    exit(1);
                 }
-
+                /* Extract the master server address */
                 xpathObj = xmlXPathEvalExpression(server_expr, xpathCtx);
                 if (xpathObj == NULL) {
-                    fprintf(stderr, "zone_fetcher: can not locate master server for zone \"%s\"; "
-                        "skipping zone", zone_name);
-                    /* Don't return? try to parse the rest of the zones? */
-                    ret = xmlTextReaderRead(reader);
-                    free(tag_name);
-                    continue;
+                    fprintf(stderr, "zone_fetcher: can not locate master server(s) in %s\n",
+                        filename);
+                    exit(1);
                 }
                 server_name = (char*) xmlXPathCastToString(xpathObj);
 
@@ -173,49 +177,37 @@ read_axfr_config(zonelist_type* zonelist)
                 if (use_tsig) {
                     xpathObj = xmlXPathEvalExpression(tsig_name_expr, xpathCtx);
                     if (xpathObj == NULL) {
-                        fprintf(stderr, "zone_fetcher: can not locate TSIG name for zone \"%s\"; "
-                            "skipping zone", zone_name);
-                        /* Don't return? try to parse the rest of the zones? */
-                        ret = xmlTextReaderRead(reader);
-                        free(server_name);
-                        free(tag_name);
-                        continue;
+                        fprintf(stderr, "zone_fetcher: can not locate TSIG name in %s\n",
+                            filename);
+                        exit(1);
                     }
                     tsig_name = (char*) xmlXPathCastToString(xpathObj);
 
                     xpathObj = xmlXPathEvalExpression(tsig_algo_expr, xpathCtx);
                     if (xpathObj == NULL) {
-                        fprintf(stderr, "zone_fetcher: can not locate TSIG algo for zone \"%s\"; "
-                            "skipping zone", zone_name);
-                        /* Don't return? try to parse the rest of the zones? */
-                        ret = xmlTextReaderRead(reader);
-                        free(server_name);
-                        free(tag_name);
-                        continue;
+                        fprintf(stderr, "zone_fetcher: can not locate TSIG algorithm in %s\n",
+                            filename);
+                        exit(1);
                     }
                     tsig_algo = (char*) xmlXPathCastToString(xpathObj);
 
                     xpathObj = xmlXPathEvalExpression(tsig_secret_expr, xpathCtx);
                     if (xpathObj == NULL) {
-                        fprintf(stderr, "zone_fetcher: can not locate TSIG secret for zone \"%s\"; "
-                            "skipping zone", zone_name);
-                        /* Don't return? try to parse the rest of the zones? */
-                        ret = xmlTextReaderRead(reader);
-                        free(server_name);
-                        free(tag_name);
-                        continue;
+                        fprintf(stderr, "zone_fetcher: can not locate TSIG secret in %s\n",
+                            filename);
+                        exit(1);
                     }
                     tsig_secret = (char*) xmlXPathCastToString(xpathObj);
 
-                    zonelist->tsig_name = strdup(tsig_name);
-                    zonelist->tsig_algo = strdup(tsig_algo);
-                    zonelist->tsig_secret = strdup(tsig_secret);
+                    cfg->tsig_name = strdup(tsig_name);
+                    cfg->tsig_algo = strdup(tsig_algo);
+                    cfg->tsig_secret = strdup(tsig_secret);
                     free(tsig_name);
                     free(tsig_algo);
                     free(tsig_secret);
                 }
 
-                zonelist->server_name = strdup(server_name);
+                cfg->server_name = strdup(server_name);
                 free(server_name);
             }
 
@@ -225,12 +217,15 @@ read_axfr_config(zonelist_type* zonelist)
         }
         xmlFreeTextReader(reader);
         if (ret != 0) {
-            fprintf(stderr, "zone_fetcher: failed to parse axfr_config %s\n", zonelist->axfr_config);
+            fprintf(stderr, "zone_fetcher: failed to parse config file %s\n", filename);
+            exit(1);
         }
     } else {
-        fprintf(stderr, "zone_fetcher: unable to open axfr config %s\n", zonelist->axfr_config);
+        fprintf(stderr, "zone_fetcher: unable to open config file %s\n", filename);
+        exit(1);
     }
 
+    cfg->use_tsig = use_tsig;
     return 0;
 }
 
@@ -238,7 +233,7 @@ static zonelist_type*
 read_zonelist(const char* filename)
 {
     zonelist_type* zonelist = NULL, *zonelist_start = NULL;
-    char* tag_name, *zone_name, *input_file, *axfr_config;
+    char* tag_name, *zone_name, *input_file;
     int ret;
 
     xmlTextReaderPtr reader = NULL;
@@ -247,7 +242,6 @@ read_zonelist(const char* filename)
     xmlXPathObjectPtr xpathObj = NULL;
     xmlChar *name_expr = (unsigned char*) "name";
     xmlChar *adapter_expr = (unsigned char*) "//Zone/Adapters/Input/File";
-    xmlChar *axfr_expr = (unsigned char*) "//Zone/AxfrConfiguration";
 
     /* In case zonelist is huge use the XmlTextReader API so that we don't hold the whole file in memory */
     reader = xmlNewTextReaderFilename(filename);
@@ -273,30 +267,22 @@ read_zonelist(const char* filename)
                 xmlTextReaderExpand(reader);
                 doc = xmlTextReaderCurrentDoc(reader);
                 if (doc == NULL) {
-                    fprintf(stderr, "zone_fetcher: can not read zone \"%s\"; skipping", zone_name);
+                    fprintf(stderr, "zone_fetcher: can not read zone \"%s\"; skipping\n", zone_name);
                     /* Don't return? try to parse the rest of the zones? */
                     ret = xmlTextReaderRead(reader);
                     continue;
                 }
                 xpathCtx = xmlXPathNewContext(doc);
                 if (xpathCtx == NULL) {
-                    fprintf(stderr, "zone_fetcher: can not create XPath context for \"%s\"; skipping zone", zone_name);
+                    fprintf(stderr, "zone_fetcher: can not create XPath context for \"%s\"; skipping zone\n", zone_name);
                     /* Don't return? try to parse the rest of the zones? */
                     ret = xmlTextReaderRead(reader);
                     continue;
                 }
-                /* Extract the AXFR Configuration filename */
-                xpathObj = xmlXPathEvalExpression(axfr_expr, xpathCtx);
-                if (xpathObj == NULL) {
-                    ret = xmlTextReaderRead(reader);
-                    continue;
-                }
-                axfr_config = (char*) xmlXPathCastToString(xpathObj);
-
                 /* Extract the Input File Adapter filename */
                 xpathObj = xmlXPathEvalExpression(adapter_expr, xpathCtx);
                 if (xpathObj == NULL) {
-                    fprintf(stderr, "zone_fetcher: unable to evaluate xpath expression: %s; skipping zone", adapter_expr);
+                    fprintf(stderr, "zone_fetcher: unable to evaluate xpath expression: %s; skipping zone\n", adapter_expr);
                     /* Don't return? try to parse the rest of the zones? */
                     ret = xmlTextReaderRead(reader);
                     continue;
@@ -304,11 +290,11 @@ read_zonelist(const char* filename)
                 input_file = (char*) xmlXPathCastToString(xpathObj);
 
                 if (zonelist == NULL) {
-                    zonelist = new_zone(zone_name, input_file, axfr_config);
+                    zonelist = new_zone(zone_name, input_file);
                     zonelist_start = zonelist;
                 }
                 else {
-                    zonelist->next = new_zone(zone_name, input_file, axfr_config);
+                    zonelist->next = new_zone(zone_name, input_file);
                     zonelist = zonelist->next;
                 }
 				free(zone_name);
@@ -332,16 +318,18 @@ read_zonelist(const char* filename)
 int
 main(int argc, char **argv)
 {
-    const char* zonelist_file = NULL;
-    const char* axfr_dir = "/opt/opendnssec/var/opendnssec/axfr/";
-
+    const char* zonelist_file = NULL, *config_file = NULL;
     zonelist_type* zonelist = NULL, *zonelist_start = NULL;
+    config_type* config = NULL;
     uint32_t serial = 0;
     FILE* fd;
     int c;
 
-    while ((c = getopt(argc, argv, "hz:")) != -1) {
+    while ((c = getopt(argc, argv, "c:hz:")) != -1) {
         switch (c) {
+        case 'c':
+            config_file = optarg;
+            break;
         case 'h':
             usage(stdout);
             exit(EXIT_SUCCESS);
@@ -365,34 +353,41 @@ main(int argc, char **argv)
     /* read zone list */
     zonelist_start = read_zonelist(zonelist_file);
 
-    /* foreach zone, do a single axfr request */
+    /* read transfer configuration */
+    config = new_config();
+    if (config_file)
+        c = read_axfr_config(config_file, config);
+
     /* [TODO] daemonize */
     /* [TODO] listen to NOTIFY messages */
     /* [TODO] respect the EXPIRE value? */
-    zonelist = zonelist_start;
-    while (zonelist != NULL) {
-        /* get latest serial */
-        fd = fopen(zonelist->input_file, "r");
-        if (!fd) {
-            serial = 0;
-        } else {
-            serial = lookup_serial(fd);
-        }
 
-        if (zonelist->axfr_config && strlen(zonelist->axfr_config) > 0) {
-            /* get tsig info */
-            c = read_axfr_config(zonelist);
-            /* send the request */
-            if (zonelist->server_name && strlen(zonelist->server_name) > 0) {
-                fprintf(stderr, "nsd-xfer -s %u [-T <%s:%s:%s>] -z %s -f %s%s %s\n",
-                    serial, zonelist->tsig_name, zonelist->tsig_algo, zonelist->tsig_secret,
-                    zonelist->name,  axfr_dir, zonelist->name, zonelist->server_name);
+    if (config->server_name && strlen(config->server_name) > 0) {
+        /* foreach zone, do a single axfr request */
+        zonelist = zonelist_start;
+        while (zonelist != NULL) {
+            /* get latest serial */
+            fd = fopen(zonelist->input_file, "r");
+            if (!fd) {
+                serial = 0;
+            } else {
+                serial = lookup_serial(fd);
             }
+            /* send the request */
+            if (config->use_tsig) {
+                fprintf(stderr, "nsd-xfer -s %u [-T <%s:%s:%s>] -z %s -f %s.axfr %s\n",
+                    serial, config->tsig_name, config->tsig_algo, config->tsig_secret,
+                    zonelist->name, zonelist->input_file, config->server_name);
+            } else {
+                fprintf(stderr, "nsd-xfer -s %u -z %s -f %s.axfr %s\n",
+                    serial, zonelist->name, zonelist->input_file, config->server_name);
+            }
+            /* next */
+            zonelist = zonelist->next;
         }
-        /* next */
-        zonelist = zonelist->next;
     }
 
+    free_config(config);
     free_zonelist(zonelist);
     return 0;
 }
