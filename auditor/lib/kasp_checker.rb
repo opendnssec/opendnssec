bfe9360ee7cc904605647c714b0390609e213f5b
Alex Dalitz <alexd@nominet.org.uk>
2009-09-30 10:08:47 +0000

    Building kasp_check up

--- auditor/lib/kasp_checker.rb
+++ auditor/lib/kasp_checker.rb
@@ -4,6 +4,18 @@ module KASPChecker
   class Checker
     attr_accessor :conf_file, :kasp_file
     def check
+      conf_file = @conf_file
+      if (!conf_file)
+        KASPAuditor.exit("No configuration file specified", 1)
+      end
+      # @TODO@ Call Sion's code to validate the conf.xml against the RNG
+      # @TODO@ Now load the config file
+      kasp_file = check_config_file(conf_file)
+
+      # @TODO@ Call Sion's code to validate the kasp.xml against the RNG
+      # @TODO@ Now load the kasp file
+      check_kasp_file(kasp_file)
+
     end
   end
 end
