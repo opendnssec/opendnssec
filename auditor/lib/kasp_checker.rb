ea7a58ebf7cc8b28fb5306dcad7be602e92f32ab
Alex Dalitz <alexd@nominet.org.uk>
2009-09-30 13:23:38 +0000

    Now trap output of xmllint, and pipe to syslog (if enabled)

--- auditor/lib/kasp_checker.rb
+++ auditor/lib/kasp_checker.rb
@@ -22,10 +22,10 @@ module KASPChecker
       end
 
       if (kasp_file)
-      # Validate the kasp.xml against the RNG
-      validate_file(kasp_file, KASP_FILE)
-      # Now check the kasp file
-      check_kasp_file(kasp_file)
+        # Validate the kasp.xml against the RNG
+        validate_file(kasp_file, KASP_FILE)
+        # Now check the kasp file
+        check_kasp_file(kasp_file)
       else
         log(LOG_ERR, "KASP configuration file cannot be found")
       end
@@ -42,22 +42,48 @@ module KASPChecker
     def validate_file(file, type)
       # Actually call xmllint to do the validation
       if (file)
-      rng_location = nil
-      if (type == CONF_FILE)
-        rng_location = rng_path + "/conf.rng"
-      else
-        rng_location = rng_path + "/kasp.rng"
-      end
-      # @TODO@ Should capture output of this process, then pipe to log() method
-      ret = system("#{xmllint} --noout --relaxng #{rng_location} #{file}")
-      if (!ret)
-        log(LOG_ERR, "Errors found validating " + 
-            ((file== nil)? "unknown file" : file) +
-            " against " + ((type == CONF_FILE) ? "conf" : "kasp") + ".rng")
-      end
+        rng_location = nil
+        if (type == CONF_FILE)
+          rng_location = @rng_path + "/conf.rng"
+        else
+          rng_location = @rng_path + "/kasp.rng"
+        end
+
+        stderr = IO::pipe
+        pid = fork {
+          stderr[0].close
+          STDERR.reopen(stderr[1])
+          stderr[1].close
+
+          options = Syslog::LOG_PERROR | Syslog::LOG_NDELAY
+
+          Syslog.open("kasp_check_internal", options) {|syslog|
+            ret = system("#{xmllint} --noout --relaxng #{rng_location} #{file}")
+            exit!(ret)
+          }
+        }
+        stderr[1].close
+        Process.waitpid(pid)
+        ret_val = $?.exitstatus
+
+        # Now rewrite captured output from xmllint to log method
+        while (line = stderr[0].gets)
+          line.chomp!
+          if line.index(" validates")
+            log(LOG_INFO, line)
+          else
+            log(LOG_ERR, line)
+          end
+        end
+
+        if (!ret_val)
+          log(LOG_ERR, "Errors found validating " +
+              ((file== nil)? "unknown file" : file) +
+              " against " + ((type == CONF_FILE) ? "conf" : "kasp") + ".rng")
+        end
       else
         log(LOG_ERR, "Not validating : no file passed to validate against " +
-          (((type == CONF_FILE) ? "conf" : "kasp") + ".rng"))
+            (((type == CONF_FILE) ? "conf" : "kasp") + ".rng"))
       end
     end
 
@@ -66,7 +92,7 @@ module KASPChecker
     # called.
     # Returns the configured location of the kasp.xml configuration file.
     def check_config_file(conf_file)
-#     @TODO@
+      #     @TODO@
     end
 
 
