2112b7cd3f9f95172ab30f014ba9448f723914be
Matthijs Mekking <matthijs@nlnetlabs.nl>
2009-10-01 11:34:12 +0000

    facility

--- signer/tools/zone_fetcher.c
+++ signer/tools/zone_fetcher.c
@@ -1222,6 +1222,66 @@ xfrd_ns(sockets_type* sockets, config_type* cfg)
     }
 }
 
+static int
+facility2int(const char* facility)
+{
+     facilities = {"KERN": syslog.LOG_KERN,
+                  "USER": syslog.LOG_USER,
+                  "MAIL": syslog.LOG_MAIL,
+                  "DAEMON": syslog.LOG_DAEMON,
+                  "AUTH": syslog.LOG_AUTH,
+                  "LPR": syslog.LOG_LPR,
+                  "NEWS": syslog.LOG_NEWS,
+                  "UUCP": syslog.LOG_UUCP,
+                  "CRON": syslog.LOG_CRON,
+                  "LOCAL0": syslog.LOG_LOCAL0,
+                  "LOCAL1": syslog.LOG_LOCAL1,
+                  "LOCAL2": syslog.LOG_LOCAL2,
+                  "LOCAL3": syslog.LOG_LOCAL3,
+                  "LOCAL4": syslog.LOG_LOCAL4,
+                  "LOCAL5": syslog.LOG_LOCAL5,
+                  "LOCAL6": syslog.LOG_LOCAL6,
+                  "LOCAL7": syslog.LOG_LOCAL7
+                 }
+
+    if (strncmp(facility, "KERN", 4) && strlen(facility) == 4)
+        return LOG_KERN;
+    else if (strncmp(facility, "USER", 4) && strlen(facility) == 4)
+        return LOG_USER;
+    else if (strncmp(facility, "MAIL", 4) && strlen(facility) == 4)
+        return LOG_MAIL;
+    else if (strncmp(facility, "DAEMON", 6) && strlen(facility) == 6)
+        return LOG_DAEMON;
+    else if (strncmp(facility, "AUTH", 4) && strlen(facility) == 4)
+        return LOG_AUTH;
+    else if (strncmp(facility, "LPR", 3) && strlen(facility) == 3)
+        return LOG_LPR;
+    else if (strncmp(facility, "NEWS", 4) && strlen(facility) == 4)
+        return LOG_NEWS;
+    else if (strncmp(facility, "UUCP", 4) && strlen(facility) == 4)
+        return LOG_UUCP;
+    else if (strncmp(facility, "CRON", 4) && strlen(facility) == 4)
+        return LOG_CRON;
+    else if (strncmp(facility, "LOCAL0", 6) && strlen(facility) == 6)
+        return LOG_LOCAL0;
+    else if (strncmp(facility, "LOCAL1", 6) && strlen(facility) == 6)
+        return LOG_LOCAL1;
+    else if (strncmp(facility, "LOCAL2", 6) && strlen(facility) == 6)
+        return LOG_LOCAL2;
+    else if (strncmp(facility, "LOCAL3", 6) && strlen(facility) == 6)
+        return LOG_LOCAL3;
+    else if (strncmp(facility, "LOCAL4", 6) && strlen(facility) == 6)
+        return LOG_LOCAL4;
+    else if (strncmp(facility, "LOCAL5", 6) && strlen(facility) == 6)
+        return LOG_LOCAL5;
+    else if (strncmp(facility, "LOCAL6", 6) && strlen(facility) == 6)
+        return LOG_LOCAL6;
+    else if (strncmp(facility, "LOCAL7", 6) && strlen(facility) == 6)
+        return LOG_LOCAL7;
+
+    return LOG_DAEMON;
+}
+
 int
 main(int argc, char **argv)
 {
@@ -1242,6 +1302,9 @@ main(int argc, char **argv)
             break;
         case 'd':
             run_as_daemon = 1;
+            break;
+        case 'f':
+            facility = facility2int(optarg);
             break;
         case 'h':
             usage(stdout);
