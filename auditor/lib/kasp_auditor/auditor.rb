4ff94aa4b1535a3a8d397287b00a3bdaa0675aad
Alex Dalitz <alexd@nominet.org.uk>
2009-10-16 08:19:37 +0000

    Handle empty lines in unsigned zonefile

--- auditor/lib/kasp_auditor/auditor.rb
+++ auditor/lib/kasp_auditor/auditor.rb
@@ -683,7 +683,7 @@ module KASPAuditor
     # There is an extra RR in the unsigned file to the signed file.
     # Error if it is in zone, warn if it is out of zone.
     def process_additional_unsigned_rr(unsigned_rr)
-      if !out_of_zone(unsigned_rr.name)
+      if unsigned_rr && !out_of_zone(unsigned_rr.name)
         if ([Types::RRSIG, Types::RRSIG, Types::NSEC, Types::NSEC3, Types::NSEC3PARAM].include?unsigned_rr.type)
           # Ignore DNSSEC data in input zone?
           log(LOG_WARNING, "#{unsigned_rr.type} RR present in unsigned file : #{unsigned_rr}")
