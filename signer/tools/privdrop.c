c42575d2e8e6628e636d8279d54ef4aa3ba5d4c7
Matthijs Mekking <matthijs@nlnetlabs.nl>
2009-10-22 10:01:15 +0000

    use the same privdrop

--- signer/tools/privdrop.c
+++ signer/tools/privdrop.c
@@ -1,7 +1,9 @@
 /*
- * $Id: privdrop.c 1933 2009-09-30 09:06:07Z jakob $
+ * $Id: privdrop.c 2284 2009-10-20 14:30:54Z sion $
  *
  * Copyright (c) 2009 Nominet UK. All rights reserved.
+ *
+ * Based heavily on uidswap.c from openssh-5.2p1
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,16 +33,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <syslog.h>
-#include <sys/types.h>
 #include <stdarg.h>
 #include <errno.h>
 #include <pwd.h>
 #include <grp.h>
 #include <ctype.h>
-#include <errno.h>
 
 #include "config.h"
-#include "logging.h"
 #include "privdrop.h"
 
 
@@ -55,18 +54,22 @@ privdrop(const char *username, const char *groupname, const char *newroot)
     uid_t uid, olduid;
     gid_t gid, oldgid;
 
+    long ngroups_max;
+    gid_t *final_groups;
+    int final_group_len = -1;
+
     /* Save effective uid/gid */
-    uid = olduid = getuid();
-    gid = oldgid = getgid();
+    uid = olduid = geteuid();
+    gid = oldgid = getegid();
 
     /* Check if we're going to drop uid */
     if (username) {
         /* Lookup the user id in /etc/passwd */
         if ((pwd = getpwnam(username)) == NULL) {
-            syslog(LOG_ERR, "user '%s' does not exist. exiting...", username);
+            syslog(LOG_ERR, "user '%s' does not exist. exiting...\n", username);
             exit(1);
         } else {
-            uid = pwd->pw_uid;
+	    uid = pwd->pw_uid;
         }
         endpwent();
     }
@@ -75,7 +78,7 @@ privdrop(const char *username, const char *groupname, const char *newroot)
     if (groupname) {
         /* Lookup the group id in /etc/groups */
         if ((grp = getgrnam(groupname)) == NULL) {
-            syslog(LOG_ERR, "group '%s' does not exist. exiting...", groupname);
+            syslog(LOG_ERR, "group '%s' does not exist. exiting...\n", groupname);
             exit(1);
         } else {
             gid = grp->gr_gid;
@@ -86,48 +89,92 @@ privdrop(const char *username, const char *groupname, const char *newroot)
     /* Change root if requested */
     if (newroot) {
        if (chroot(newroot) != 0 || chdir("/") != 0) {
-            syslog(LOG_ERR, "chroot to '%s' failed. exiting...", newroot);
+            syslog(LOG_ERR, "chroot to '%s' failed. exiting...\n", newroot);
             exit(1);
        }
+    }
 
-       syslog(LOG_INFO, "changed root to '%s'", newroot);
+    /* Do Additional groups first */
+    if (username != NULL && !olduid) {
+        if (initgroups(username, gid) < 0) {
+            syslog(LOG_ERR, "initgroups failed: %s: %.100s", username, strerror(errno));
+            exit(1);
+        }
+
+        ngroups_max = sysconf(_SC_NGROUPS_MAX) + 1;
+        final_groups = (gid_t *)malloc(ngroups_max *sizeof(gid_t));
+        if (final_groups == NULL) {
+            syslog(LOG_ERR, "Malloc for group struct failed");
+            exit(1);
+        }
+
+        final_group_len = getgroups(ngroups_max, final_groups);
+        /* If we are root then drop all groups other than the final one */
+        if (!olduid) setgroups(final_group_len, final_groups);
+
+        free(final_groups);
+    }
+    else {
+        /* If we are root then drop all groups other than the final one */
+        if (!olduid) setgroups(1, &(gid));
     }
 
     /* Drop gid? */
     if (groupname) {
-#ifdef HAVE_SETRESGID
-        if ((status = setresgid(gid, gid, gid)) != 0)
-#elif defined(HAVE_SETREGID) && !defined(DARWIN_BROKEN_SETREUID)
-            if ((status = setregid(gid)) != 0)
-#else /* use setgid */
-                if ((status = setgid(gid)) != 0)
-#endif /* HAVE_SETRESGID */
-                    syslog(LOG_ERR, "unable to set group id of %s (%lu): %s",
-                        groupname, (unsigned long) gid, strerror(errno));
 
-        if (status != 0)
-            return status;
-        else
-            syslog(LOG_INFO, "dropped group privileges to %s (%lu)",
-                        groupname, (unsigned long) gid);
+#if defined(HAVE_SETRESGID) && !defined(BROKEN_SETRESGID)
+        status = setresgid(gid, gid, gid);
+#elif defined(HAVE_SETREGID) && !defined(BROKEN_SETREGID)
+        status = setregid(gid, gid);
+#else
+        status = setegid(gid);
+        if (status != 0) {
+           syslog(LOG_ERR, "unable to drop group privileges: %s (%lu). exiting...\n",
+               groupname, (unsigned long) gid);
+           exit(1);
+        }
+        status = setgid(gid);
+#endif
+
+        if (status != 0) {
+           syslog(LOG_ERR, "unable to drop group privileges: %s (%lu). exiting...\n",
+               groupname, (unsigned long) gid);
+           exit(1);
+           return -1;
+        } else {
+            syslog(LOG_ERR, "group set to: %s (%lu)\n", groupname, (unsigned long) gid);
+        }
     }
 
     /* Drop uid? */
     if (username) {
-#ifdef HAVE_SETRESUID
-        if ((status = setresuid(uid,uid,uid)) != 0)
-#elif defined(HAVE_SETREUID) && !defined(DARWIN_BROKEN_SETREUID)
-            if ((status = setreuid(uid,uid)) != 0)
-#else /* use setuid */
-                if ((status = setuid(uid)) != 0)
-#endif /* HAVE_SETRESUID */
-                    syslog(LOG_ERR, "unable to set user id of %s (%lu): %s",
-                        username, (unsigned long) uid, strerror(errno));
-        if (status != 0)
-            return status;
-        else
-            syslog(LOG_INFO, "dropped user privileges to %s (%lu)",
-                        username, (unsigned long) uid);
+        /* Set the user to drop to if specified; else just set the uid as the real one */
+#if defined(HAVE_SETRESUID) && !defined(BROKEN_SETRESUID)
+        status = setresuid(uid, uid, uid);
+#elif defined(HAVE_SETREUID) && !defined(BROKEN_SETREUID)
+        status = setreuid(uid, uid);
+#else
+
+# ifndef SETEUID_BREAKS_SETUID
+        status = seteuid(uid);
+        if (status != 0) {
+           syslog(LOG_ERR, "unable to drop user privileges (seteuid): %s (%lu). exiting...\n",
+               username, (unsigned long) uid);
+           exit(1);
+        }
+# endif  /* SETEUID_BREAKS_SETUID */
+
+        status = setuid(uid);
+#endif
+
+        if (status != 0) {
+           syslog(LOG_ERR, "unable to drop user privileges: %s (%lu). exiting...\n",
+               username, (unsigned long) uid);
+           exit(1);
+           return -1;
+        } else {
+            syslog(LOG_ERR, "user set to: %s (%lu)\n", username, (unsigned long) uid);
+        }
     }
 
     return 0;
