d1d8661c028923f99e9b94fc4e65bfaad533ea35
Jakob Schlyter <jakob@kirei.se>
2010-02-01 19:09:55 +0000

    use local version.m4 and let autogen.sh create hard link to top-level version.m4 (ticket #83)

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,24 @@
 #
 # $Id$
 
+SUBDIRS="auditor libhsm enforcer signer conf"
+VERSION=version.m4
+
+if [ `dirname $0` = "." ]; then
+	for SUBDIR in ${SUBDIRS}; do
+		if [ -d $SUBDIR ]; then
+			echo Creating ${SUBDIR}/${VERSION}
+			rm -f ${SUBDIR}/${VERSION}
+			ln ${VERSION} ${SUBDIR}/${VERSION}
+		fi
+	done
+elif [ `dirname $0` = ".." ]; then
+	if [ -f ../${VERSION} ]; then
+		echo Creating ${VERSION}
+		rm -f ${VERSION}
+		ln ../${VERSION} ${VERSION}
+	fi
+fi
+
+echo "Running autoreconf"
 autoreconf --install --force
