b18f93690e906a18a04d10a7418e622cca03970d
Jakob Schlyter <jakob@kirei.se>
2010-04-22 15:12:25 +0000

    use variables

--- test/scripts/build_and_test.sh
+++ test/scripts/build_and_test.sh
@@ -2,12 +2,16 @@
 #
 # $Id$
 
+SANDBOX_ROOT=${HOME}/ODS
+LIBSOFTHSM=/usr/local/lib/libsofthsm.so
+
+
 # Build and test OpenDNSSEC
 # Uses $HOME/ODS for install sandbox
-rm -rf $HOME/ODS
+rm -rf ${SANDBOX_ROOT}
 
 sh autogen.sh
-./configure --prefix=$HOME/ODS --with-pkcs11-softhsm=/usr/local/lib/libsofthsm.so
+./configure --prefix=${SANDBOX_ROOT} --with-pkcs11-softhsm=${LIBSOFTHSM}
 rc=$?
 if [[ $rc != 0 ]] ; then
     exit $rc
@@ -26,7 +30,7 @@ fi
 
 export SOFTHSM_CONF=test/scripts/softhsm.conf
 
-echo "yes" | $HOME/ODS/bin/ods-ksmutil setup
+echo "yes" | ${SANDBOX_ROOT}/bin/ods-ksmutil setup
 
 softhsm --init-token --slot 0  --pin 1234 --so-pin 1234 --label "OpenDNSSEC"
 rc=$?
@@ -34,33 +38,33 @@ if [[ $rc != 0 ]] ; then
     exit $rc
 fi
 
-cp test/zonedata/unknown.rr.org $HOME/ODS/var/opendnssec/unsigned/.
-$HOME/ODS/bin/ods-ksmutil zone add -z  unknown.rr.org -p default
-cp test/zonedata/example.com $HOME/ODS/var/opendnssec/unsigned/.
-$HOME/ODS/bin/ods-ksmutil zone add -z  example.com -p default
-cp test/zonedata/all.rr.org $HOME/ODS/var/opendnssec/unsigned/.
-$HOME/ODS/bin/ods-ksmutil zone add -z  all.rr.org -p default
-cp test/zonedata/all.rr.binary.org $HOME/ODS/var/opendnssec/unsigned/.
-$HOME/ODS/bin/ods-ksmutil zone add -z  all.rr.binary.org -p default
-$HOME/ODS/bin/ods-ksmutil update all
-$HOME/ODS/bin/ods-ksmutil key generate --interval P1Y --policy default
+cp test/zonedata/unknown.rr.org ${SANDBOX_ROOT}/var/opendnssec/unsigned/.
+${SANDBOX_ROOT}/bin/ods-ksmutil zone add -z  unknown.rr.org -p default
+cp test/zonedata/example.com ${SANDBOX_ROOT}/var/opendnssec/unsigned/.
+${SANDBOX_ROOT}/bin/ods-ksmutil zone add -z  example.com -p default
+cp test/zonedata/all.rr.org ${SANDBOX_ROOT}/var/opendnssec/unsigned/.
+${SANDBOX_ROOT}/bin/ods-ksmutil zone add -z  all.rr.org -p default
+cp test/zonedata/all.rr.binary.org ${SANDBOX_ROOT}/var/opendnssec/unsigned/.
+${SANDBOX_ROOT}/bin/ods-ksmutil zone add -z  all.rr.binary.org -p default
+${SANDBOX_ROOT}/bin/ods-ksmutil update all
+${SANDBOX_ROOT}/bin/ods-ksmutil key generate --interval P1Y --policy default
 
 # TODO - this should be replaced by ods-control start when it is fixed
-$HOME/ODS/sbin/ods-enforcerd -1
+${SANDBOX_ROOT}/sbin/ods-enforcerd -1
 sleep 5
-$HOME/ODS/sbin/ods-enforcerd 
+${SANDBOX_ROOT}/sbin/ods-enforcerd 
 sleep 5
-$HOME/ODS/sbin/ods-signerd
+${SANDBOX_ROOT}/sbin/ods-signerd
 sleep 5
-$HOME/ODS/sbin/ods-signer zones
+${SANDBOX_ROOT}/sbin/ods-signer zones
 sleep 1
-$HOME/ODS/sbin/ods-signer sign unknown.rr.org
-$HOME/ODS/sbin/ods-signer sign example.com
-echo $HOME/ODS/sbin/ods-signer sign all.rr.org
-echo $HOME/ODS/sbin/ods-signer sign all.rr.binary.org
+${SANDBOX_ROOT}/sbin/ods-signer sign unknown.rr.org
+${SANDBOX_ROOT}/sbin/ods-signer sign example.com
+echo ${SANDBOX_ROOT}/sbin/ods-signer sign all.rr.org
+echo ${SANDBOX_ROOT}/sbin/ods-signer sign all.rr.binary.org
 sleep 5
 
-$HOME/ODS/sbin/ods-control stop
+${SANDBOX_ROOT}/sbin/ods-control stop
 
 echo "NOW CHECK THAT ZONES HAVE BEEN SIGNED"
 echo Still to check all.rr.org and all.rr.binary.org
