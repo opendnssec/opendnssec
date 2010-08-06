6626e56f1806161282f07cbbd078f862012074bc
Matthijs Mekking <matthijs@nlnetlabs.nl>
2010-08-06 14:16:00 +0000

    stats.*

---
+++ signer/src/signer/stats.c
@@ -0,0 +1,108 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2009 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Signer statistics.
+ *
+ */
+
+#include "signer/stats.h"
+#include "util/log.h"
+#include "util/se_malloc.h"
+
+/**
+ * Initialize statistics.
+ *
+ */
+stats_type*
+stats_create(void)
+{
+    stats_type* stats = (stats_type*) se_malloc(sizeof(stats_type));
+    stats_clear(stats);
+    return stats;
+}
+
+
+/**
+ * Clear statistics.
+ *
+ */
+void
+stats_clear(stats_type* stats)
+{
+    se_log_assert(stats);
+    stats->sort_count = 0;
+    stats->sort_time = 0;
+    stats->nsec_count = 0;
+    stats->nsec_time = 0;
+    stats->sig_count = 0;
+    stats->sig_reuse = 0;
+    stats->sig_time = 0;
+}
+
+
+/**
+ * Log statistics.
+ *
+ */
+void
+stats_log(stats_type* stats, char* name, ldns_rr_type nsec_type)
+{
+    uint32_t avsign = 0;
+
+    se_log_assert(stats);
+    se_log_assert(name);
+    if (stats->sig_time) {
+        avsign = (uint32_t) (stats->sig_count/stats->sig_time);
+    }
+
+    se_log_info("[STATS] %s RR[count=%u time=%u(sec)) "
+        "NSEC%s[count=%u time=%u(sec)] "
+        "RRSIG[new=%u reused=%u time=%u(sec) avg=%u(sig/sec)]",
+        name, stats->sort_count, stats->sort_time,
+        nsec_type==LDNS_RR_TYPE_NSEC3?"3":"", stats->nsec_count,
+        stats->nsec_time,
+        stats->sig_count, stats->sig_reuse, stats->sig_time, avsign);
+    return;
+}
+
+
+/**
+ * Clean up statistics.
+ *
+ */
+void
+stats_cleanup(stats_type* stats)
+{
+    if (stats) {
+	se_free((void*) stats);
+    } else {
+        se_log_warning("cleanup empty statistics");
+    }
+    return;
+}
---
+++ signer/src/signer/stats.h
@@ -0,0 +1,93 @@
+/*
+ * $Id$
+ *
+ * Copyright (c) 2009 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Signer statistics.
+ *
+ */
+
+#ifndef SIGNER_STATS_H
+#define SIGNER_STATS_H
+
+#include <config.h>
+#include <ctype.h>
+#include <ldns/ldns.h>
+#include <stdint.h>
+#include <time.h>
+#ifdef HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+/**
+ * Statistics structure.
+ */
+typedef struct stats_struct stats_type;
+struct stats_struct {
+    uint32_t    sort_count;
+    time_t      sort_time;
+    uint32_t    nsec_count;
+    time_t      nsec_time;
+    uint32_t    sig_count;
+    uint32_t    sig_reuse;
+    time_t      sig_time;
+};
+
+/**
+ * Initialize statistics.
+ * \return the initialized stats;
+ *
+ */
+stats_type* stats_create(void);
+
+/**
+ * Log statistics.
+ * \param[in] stats statistics
+ * \param[in] name zone name
+ * \param[in] nsec_type NSEC or NSEC3
+ *
+ */
+void stats_log(stats_type* stats, char* name, ldns_rr_type nsec_type);
+
+/**
+ * Clear statistics.
+ * \param[in] stats statistics to be cleared
+ *
+ */
+void stats_clear(stats_type* stats);
+
+/**
+ * Clean up statistics.
+ * \param[in] stats statistics to be deleted
+ *
+ */
+void stats_cleanup(stats_type* stats);
+
+#endif /* SIGNER_STATS_H */
