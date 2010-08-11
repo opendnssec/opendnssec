527d771071c94e433a5e05418a163c1d3d09a328
Matthijs Mekking <matthijs@nlnetlabs.nl>
2010-08-11 09:27:23 +0000

    full pathname for cp
    http://www.pivotaltracker.com/story/show/3999464

--- signer/src/util/file.c
+++ signer/src/util/file.c
@@ -292,7 +292,8 @@ se_file_copy(const char* file1, const char* file2)
     se_log_assert(file1);
     se_log_assert(file2);
 
-    snprintf(str, SYSTEM_MAXLEN, "cp %s %s", file1, file2);
+    snprintf(str, SYSTEM_MAXLEN, "%s %s %s",
+        OPENDNSSEC_CP_COMMAND, file1, file2);
     se_log_debug("system call: %s", str);
     return system(str);
 }
