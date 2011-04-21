158eb0e6be89849a1a9d76912069219b084f90b8
René Post <rene@xpt.nl>
2011-04-21 07:17:35 +0000

    Change key pre-generation algorithm to keep a separate key pool per bitsize

--- enforcer-ng/src/hsmkey/keypregen_task.cpp
+++ enforcer-ng/src/hsmkey/keypregen_task.cpp
@@ -29,6 +29,19 @@ bool generate_key(int num, unsigned int bits, std::string &locator)
 void 
 perform_keypregen(int sockfd, engineconfig_type *config)
 {
+    typedef struct {
+        unsigned int bits;
+        unsigned int nneeded;
+        unsigned int navailable;
+    
+    } keygencfg_t; 
+    keygencfg_t keygencfg[] = {
+        {1024,20},
+        {1536,15},
+        {2048,40}
+    };
+    const unsigned int nkeygencfg = sizeof(keygencfg)/sizeof(keygencfg_t);
+    
     char buf[ODS_SE_MAXLINE];
     const char *datastore = config->datastore;
     
@@ -51,36 +64,60 @@ perform_keypregen(int sockfd, engineconfig_type *config)
         }
         close(fd);
     }
+
     
-    // Check how many of them have been used
+    // Establish the number of keys need to be generated
     int nfreekeys = 0;
     for (int k=0; k<hsmkeyDoc->keys_size(); ++k) {
         const ::ods::hsmkey::HsmKey& key = hsmkeyDoc->keys(k);
-        if (!key.has_inception())
-            ++nfreekeys;
-    }
-
-    // Generate keys until certain minimum number is available.
-    bool bkeysgenerated = false;
-    for ( ;nfreekeys<42; ++nfreekeys) {
-        
-        ::ods::hsmkey::HsmKey* key = hsmkeyDoc->add_keys();
-        if (!key) {
-            ods_log_error("[%s] Unable to add keys to the key info list",
-                          keypregen_task_str);
-            break;
+        if (!key.has_inception()) {
+            // this key is available
+            unsigned int keybits = key.bits();
+            for (int c=0; c<nkeygencfg; ++c) {
+                if (keybits == keygencfg[c].bits) {
+                    ++keygencfg[c].navailable;
+                    break;
+                }
+            }
         }
-        bkeysgenerated = true;
+    }
+    
+    // Generate the keys of different sizes that are needed
+    bool bkeysgenerated = false;
+    for (int c=0; c<nkeygencfg; ++c) {
+        unsigned int nbits = keygencfg[c].bits;
+        int ngen = keygencfg[c].nneeded-keygencfg[c].navailable;
+        if (ngen <= 0) continue;
 
-        std::string locator;
-        unsigned int bits = nfreekeys&1 ? 1536 : 2048;
-        if (generate_key(hsmkeyDoc->keys_size(), bits, locator)) {
-            key->set_locator(locator);
-            key->set_bits(bits);
-        } else {
-            ods_log_error("[%s] Error during key generation",
-                          keypregen_task_str);
-            break;
+        (void)snprintf(buf, ODS_SE_MAXLINE, "generating %d keys of %d bits.\n",
+                       ngen,nbits);
+        ods_writen(sockfd, buf, strlen(buf));
+
+        // Generate additional keys until certain minimum number is available.
+        for ( ;ngen; --ngen) {
+            std::string locator;
+            if (generate_key(hsmkeyDoc->keys_size(), nbits,locator)) 
+            {
+                bkeysgenerated = true;
+                ::ods::hsmkey::HsmKey* key = hsmkeyDoc->add_keys();
+                key->set_locator(locator);
+                key->set_bits(nbits);
+            } else {
+                // perhaps this HSM can't generate keys of this size.
+                ods_log_error("[%s] Error during key generation",
+                              keypregen_task_str);
+                (void)snprintf(buf, ODS_SE_MAXLINE, "unable to generate a key "
+                               "of %d bits.\n", nbits);
+                ods_writen(sockfd, buf, strlen(buf));
+                break;
+            }
+            ++keygencfg[c].navailable;
+        }
+
+        if (ngen==0) {
+            (void)snprintf(buf, ODS_SE_MAXLINE, "finished generating %d bit "
+                           "keys.\n", nbits);
+            ods_writen(sockfd, buf, strlen(buf));
         }
     }
     
@@ -99,8 +136,7 @@ perform_keypregen(int sockfd, engineconfig_type *config)
         close(fd);
     }
     
-    (void)snprintf(buf, ODS_SE_MAXLINE, "pre-generation of a key collection "
-                                        "complete.\n");
+    (void)snprintf(buf, ODS_SE_MAXLINE, "key pre-generation complete.\n");
     ods_writen(sockfd, buf, strlen(buf));
 }
 
@@ -116,8 +152,8 @@ keypregen_task_perform(task_type *task)
 task_type *
 keypregen_task(engineconfig_type *config)
 {
-    task_id keypregen_task_id = task_register_how("keypregen_task_perform",
-                                                 keypregen_task_perform);
-	return task_create(keypregen_task_id,time_now(),"keypregen",
-                       (void*)config,keypregen_task_perform);
+    task_id what = task_register("keypregen",
+                                 "keypregen_task_perform",
+                                 keypregen_task_perform);
+	return task_create(what, time_now(), "all", (void*)config);
 }
