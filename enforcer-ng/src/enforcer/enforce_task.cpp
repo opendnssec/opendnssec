bdcad306f42f17d1cc20df364e69f436eb9b7b71
René Post <rene@xpt.nl>
2011-05-04 11:37:43 +0000

    Layout cleanups

--- enforcer-ng/src/enforcer/enforce_task.cpp
+++ enforcer-ng/src/enforcer/enforce_task.cpp
@@ -23,7 +23,7 @@ extern "C" {
 #include "enforcer/enforcerzone.h"
 #include "enforcer/hsmkeyfactory.h"
 
-static const char *enforce_task_str = "enforce_task";
+static const char *module_str = "enforce_task";
 
 time_t perform_enforce(int sockfd, engineconfig_type *config)
 {
@@ -46,10 +46,10 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
         int fd = open(datapath.c_str(),O_RDONLY);
         if (kaspDoc->ParseFromFileDescriptor(fd)) {
             ods_log_debug("[%s] policies have been loaded",
-                          enforce_task_str);
+                          module_str);
         } else {
             ods_log_error("[%s] policies could not be loaded from \"%s\"",
-                          enforce_task_str,datapath.c_str());
+                          module_str,datapath.c_str());
             bFailedToLoad = true;
         }
         close(fd);
@@ -63,10 +63,10 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
         int fd = open(datapath.c_str(),O_RDONLY);
         if (keystateDoc->ParseFromFileDescriptor(fd)) {
             ods_log_debug("[%s] keystates have been loaded",
-                          enforce_task_str);
+                          module_str);
         } else {
             ods_log_error("[%s] keystates could not be loaded from \"%s\"",
-                          enforce_task_str,datapath.c_str());
+                          module_str,datapath.c_str());
         }
         close(fd);
     }
@@ -78,11 +78,11 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
         datapath += ".hsmkey.pb";
         int fd = open(datapath.c_str(),O_RDONLY);
         if (hsmkeyDoc->ParseFromFileDescriptor(fd)) {
-            ods_log_debug("[%s] HSM key info list has been loaded",
-                          enforce_task_str);
+            ods_log_debug("[%s] HSM key list has been loaded",
+                          module_str);
         } else {
-            ods_log_error("[%s] HSM key info list could not be loaded from \"%s\"",
-                          enforce_task_str,datapath.c_str());
+            ods_log_error("[%s] HSM key list could not be loaded from \"%s\"",
+                          module_str,datapath.c_str());
         }
         close(fd);
     }
@@ -93,7 +93,7 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
         delete keystateDoc;
         delete hsmkeyDoc;
         ods_log_error("[%s] unable to continue",
-                      enforce_task_str);
+                      module_str);
         return -1;
     }
 
@@ -120,7 +120,7 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
             if (kasp.policies(p).name() == ks_zone.policy()) {
                 policy = &kasp.policies(p);
                 ods_log_debug("[%s] policy %s found for zone %s",
-                              enforce_task_str,policy->name().c_str(),
+                              module_str,policy->name().c_str(),
                               ks_zone.name().c_str());
                 break;
             }
@@ -128,10 +128,10 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
 
         if (policy == NULL) {
             ods_log_error("[%s] policy %s could not be found for zone %s",
-                          enforce_task_str,ks_zone.policy().c_str(),
+                          module_str,ks_zone.policy().c_str(),
                           ks_zone.name().c_str());
             ods_log_error("[%s] unable to enforce zone %s",
-                          enforce_task_str,ks_zone.name().c_str());
+                          module_str,ks_zone.name().c_str());
             continue;
         }
 
@@ -144,7 +144,7 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
         
         if (t_next < time_now()) {
             ods_log_error("[%s] enforcer asked to be scheduled in the past for "
-                          "zone %s", enforce_task_str, ks_zone.name().c_str());
+                          "zone %s", module_str, ks_zone.name().c_str());
             continue;
         }
 
@@ -165,17 +165,21 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
         int fd = open(datapath.c_str(),O_WRONLY|O_CREAT, 0644);
         if (keystateDoc->SerializeToFileDescriptor(fd)) {
             ods_log_debug("[%s] key states have been updated",
-                          enforce_task_str);
+                          module_str);
 
-            (void)snprintf(buf, ODS_SE_MAXLINE, "update of key states completed.\n");
+            (void)snprintf(buf, ODS_SE_MAXLINE,
+                           "update of key states completed.\n");
             ods_writen(sockfd, buf, strlen(buf));
         } else {
-            (void)snprintf(buf, ODS_SE_MAXLINE, "error: key states file could not be written.\n");
+            (void)snprintf(buf, ODS_SE_MAXLINE,
+                           "error: key states file could not be written.\n");
             ods_writen(sockfd, buf, strlen(buf));
         }
         close(fd);
     } else {
-        (void)snprintf(buf, ODS_SE_MAXLINE, "error: a message in the key states is missing mandatory information.\n");
+        (void)snprintf(buf, ODS_SE_MAXLINE,
+                       "error: a message in the key states is missing "
+                       "mandatory information.\n");
         ods_writen(sockfd, buf, strlen(buf));
     }
 
@@ -187,17 +191,21 @@ time_t perform_enforce(int sockfd, engineconfig_type *config)
         int fd = open(datapath.c_str(),O_WRONLY|O_CREAT, 0644);
         if (hsmkeyDoc->SerializeToFileDescriptor(fd)) {
             ods_log_debug("[%s] HSM keys have been updated",
-                          enforce_task_str);
+                          module_str);
             
-            (void)snprintf(buf, ODS_SE_MAXLINE, "update of HSM keys completed.\n");
+            (void)snprintf(buf, ODS_SE_MAXLINE, 
+                           "update of HSM keys completed.\n");
             ods_writen(sockfd, buf, strlen(buf));
         } else {
-            (void)snprintf(buf, ODS_SE_MAXLINE, "error: HSM keys file could not be written.\n");
+            (void)snprintf(buf, ODS_SE_MAXLINE, 
+                           "error: HSM keys file could not be written.\n");
             ods_writen(sockfd, buf, strlen(buf));
         }
         close(fd);
     } else {
-        (void)snprintf(buf, ODS_SE_MAXLINE, "error: a message in the HSM keys is missing mandatory information.\n");
+        (void)snprintf(buf, ODS_SE_MAXLINE, 
+                       "error: a message in the HSM keys is missing "
+                       "mandatory information.\n");
         ods_writen(sockfd, buf, strlen(buf));
     }
     
