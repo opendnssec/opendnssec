cdfff2460ab15e9a6e051f67af99ac86a20a47fb
René Post <rene@xpt.nl>
2011-05-05 11:29:03 +0000

    HSM key list now uses only the hsmkey data and does not access libhsm.

--- enforcer-ng/src/hsmkey/hsmkey_list_task.cpp
+++ enforcer-ng/src/hsmkey/hsmkey_list_task.cpp
@@ -2,7 +2,7 @@ extern "C" {
 #include "hsmkey/hsmkey_list_task.h"
 #include "shared/file.h"
 #include "shared/duration.h"
-#include "libhsm.h"
+#include "shared/str.h"
 }
 
 #include <google/protobuf/descriptor.h>
@@ -17,43 +17,11 @@ extern "C" {
 
 static const char *module_str = "hsmkey_list_task";
 
-
-static void list_all_keys_in_all_hsms(int sockfd, engineconfig_type *config)
-{
-    char buf[ODS_SE_MAXLINE];
-    // hsm_open(config->cfg_filename,NULL,NULL);
-    hsm_ctx_t * hsm_ctx = hsm_create_context();
-    size_t nkeys;
-    hsm_key_t **kl = hsm_list_keys(hsm_ctx, &nkeys);
-    
-    for (int i=0; i<nkeys; ++i) {
-        hsm_key_info_t *kinf = hsm_get_key_info(hsm_ctx,kl[i]);
-
-        (void)snprintf(buf, ODS_SE_MAXLINE, 
-                       "key [%s] algorithm=%s (%lu), size=%lu\n",
-                       kinf->id,
-                       kinf->algorithm_name,kinf->algorithm,
-                       kinf->keysize);
-
-        ods_writen(sockfd, buf, strlen(buf));
-        
-        
-        hsm_key_info_free(kinf);
-    }
-
-    
-    hsm_key_list_free(kl,nkeys);
-    hsm_destroy_context(hsm_ctx);
-    // hsm_close();
-}
-
 void 
 perform_hsmkey_list(int sockfd, engineconfig_type *config)
 {
     char buf[ODS_SE_MAXLINE];
     const char *datastore = config->datastore;
-    
-    list_all_keys_in_all_hsms(sockfd,config);    
     
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
     
@@ -75,33 +43,38 @@ perform_hsmkey_list(int sockfd, engineconfig_type *config)
         close(fd);
     }
 
+
+    (void)snprintf(buf, ODS_SE_MAXLINE,
+                   "HSM keys:\n"
+                   "Id:                                      "
+                   "Algorithm: "
+                   "Bits:   "
+                   "HSM:       "
+                   "First use:                 "
+                   "\n"
+                   );
+    ods_writen(sockfd, buf, strlen(buf));
     
     // Enumerate the keys found in the doc file on disk.
     for (int k=0; k<hsmkeyDoc->keys_size(); ++k) {
         const ::ods::hsmkey::HsmKey& key = hsmkeyDoc->keys(k);
-        std::string loc = key.locator();
-        uint32_t keyalgo = key.algorithm();
-        uint32_t keybits = key.bits();
-        
-        (void)snprintf(buf, ODS_SE_MAXLINE,
-                       "key [%s] algorithm= (%u), size=%u\n",
-                       loc.c_str(),
-                       keyalgo,
-                       keybits);
-        
+        std::string algo  = key.algorithm_name();
+        uint32_t bits = key.bits();
+        std::string loca = key.locator();
+        std::string hsm = key.hsm_name();
+        char incep[32];
+        if (key.inception() != 0) {
+            if (!ods_ctime_r(incep,sizeof(incep),key.inception())) {
+                strncpy(incep,"(invalid date/time)",sizeof(incep));
+                incep[sizeof(incep)-1] = '\0';
+            }
+        } else {
+            strncpy(incep,"(never)",sizeof(incep));
+            incep[sizeof(incep)-1] = '\0';
+        }
+        (void)snprintf(buf, ODS_SE_MAXLINE, "%-40s %-10s %-7u %-10s %-26s \n",
+                       loca.c_str(), algo.c_str(), bits, hsm.c_str(), incep);
         ods_writen(sockfd, buf, strlen(buf));
-
-#if 0
-            optional bool candidate_for_sharing = 2 [default = false];
-            optional uint32 bits = 3 [default = 2048];
-            optional string policy = 4 [default = "default"];
-            optional uint32 algorithm = 5 [default = 1];
-            optional keyrole role = 6 [default = ZSK];
-            repeated string used_by_zones = 7;
-            optional uint32 inception = 8;
-            optional bool revoke = 9 [default = false];
-#endif
-
     }
 }
 
