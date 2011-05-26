323051a6c6d02100d1bae208c7c3f45724eea83c
Matthijs Mekking <matthijs@nlnetlabs.nl>
2011-05-26 11:25:29 +0000

    ifdef

--- signer/src/tools/zone_fetcher.h
+++ signer/src/tools/zone_fetcher.h
@@ -39,6 +39,10 @@
 #include <unistd.h>
 
 #include "config.h"
+#include "shared/locks.h"
+
+#ifndef TOOLS_ZONEFETCHER_H
+#define TOOLS_ZONEFETCHER_H
 
 #define DNS_PORT_STRING "53"
 #define INBUF_SIZE      4096 /* max size for incoming queries */
@@ -130,6 +134,7 @@ struct handle_tcp_userdata {
  */
 int
 tools_zone_fetcher(const char* config_file, const char* zonelist_file,
-    const char* group, const char* user, const char* chroot, const char* log_file,
-    int use_syslog, int verbosity);
+    const char* group, const char* user, const char* chroot,
+    const char* log_file, int use_syslog, int verbosity);
 
+#endif /* TOOLS_ZONEFETCHER_H */
