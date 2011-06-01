6ae08adb782adfbc97a79929892126867f8c843a
René Post <rene@xpt.nl>
2011-06-01 13:06:15 +0000

    Handle stdout console output throttling that would truncate daemon output intermittently.

--- enforcer-ng/src/ods-enforcer.c
+++ enforcer-ng/src/ods-enforcer.c
@@ -147,36 +147,63 @@ interface_run(FILE* fp, int sockfd, char* cmd)
                     /* error occurred */
                     fprintf(stderr, "error: %s\n", strerror(errno));
                     exit(1);
-                } else if (stdineof == 1) {
-                    /* normal termination */
-                    return;
                 } else {
-                    /* weird termination */
-                    fprintf(stderr, "enforcer engine terminated "
-                        "prematurely\n");
+                    /* n==0 */
+                    if (stdineof == 1) {
+                        /* normal termination */
+                        return;
+                    } else {
+                        /* weird termination */
+                        fprintf(stderr, "enforcer engine terminated "
+                                "prematurely\n");
+                        exit(1);
+                    }
+                }
+            }
+
+            if (cmd) {
+                if (n < SE_CLI_CMDLEN) {
+                    /* not enough data received */
+                    fprintf(stderr, "not enough response data received "
+                            "from daemon.\n");
                     exit(1);
                 }
-            }
-
-            if (cmd && strncmp(buf+n-SE_CLI_CMDLEN, "\ncmd> ",
-                SE_CLI_CMDLEN) == 0) {
-                /* we have the full response */
-                if (n > SE_CLI_CMDLEN) {
-                    ret = (int) write(fileno(stdout), buf, n-SE_CLI_CMDLEN);
+                
+                /* n >= SE_CLI_CMDLEN : and so it is safe to do buffer 
+                    manipulations below. */
+                if (strncmp(buf+n-SE_CLI_CMDLEN,"\ncmd> ",SE_CLI_CMDLEN) == 0) {
+                
+                    /* we have the full response */
+                    n -= SE_CLI_CMDLEN;
+                    buf[n] = '\0';
+                    cmd_response = 1;
                 }
-                buf[(n-SE_CLI_CMDLEN)] = '\0';
-                cmd_response = 1;
-                ret = 1;
-            } else {
-                /* we can expect more */
-                ret = (int) write(fileno(stdout), buf, n);
             }
 
-            /* error and shutdown handling */
-            if (ret == 0) {
-                fprintf(stderr, "no write\n");
-            } else if (ret < 0) {
-                fprintf(stderr, "write error: %s\n", strerror(errno));
+            /* n > 0 : when we get to this line... */
+            int written;
+            for (written=0; written < n; written += ret) {
+                /* write what we got to stdout */
+                ret = (int) write(fileno(stdout), &buf[written], n-written);
+                /* error and shutdown handling */
+                if (ret == 0) {
+                    fprintf(stderr, "no write\n");
+                    break;
+                }
+                if (ret < 0) {
+                    if (errno == EINTR || errno == EWOULDBLOCK) {
+                        ret = 0;
+                        continue; // try again...
+                    }
+                    fprintf(stderr, "\n\nwrite error: %s\n", strerror(errno));
+                    break;
+                }
+                /* ret > 0 : when we get here... */
+                if (written+ret > n) {
+                    fprintf(stderr, "\n\nwrite error: more bytes (%d) written than required (%d)\n", written+ret, n);
+                    break;
+                }
+                /* written+ret < n : means partial write, requires us to loop... */
             }
             if (ods_strcmp(buf, ODS_SE_STOP_RESPONSE) == 0 || cmd_response) {
                 fprintf(stderr, "\n");
