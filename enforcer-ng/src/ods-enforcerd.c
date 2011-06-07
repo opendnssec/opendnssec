8517b51105f032a4dce3f8646d06c96e27cda594
René Post <rene@xpt.nl>
2011-06-07 07:35:16 +0000

    Initial implementation of key state export for transferring zone enforcing between enforcers

--- enforcer-ng/src/ods-enforcerd.c
+++ enforcer-ng/src/ods-enforcerd.c
@@ -49,6 +49,7 @@
 #include "keystate/keystate_show_cmd.h"
 #include "keystate/keystate_ds_submit_cmd.h"
 #include "keystate/keystate_ds_seen_cmd.h"
+#include "keystate/keystate_export_cmd.h"
 
 #include "enforcer/enforce_cmd.h"
 #include "signconf/signconf_cmd.h"
@@ -128,6 +129,7 @@ static help_xxxx_cmd_type enforcer_help[] = {
     help_keystate_show_cmd,
     help_keystate_ds_submit_cmd,
     help_keystate_ds_seen_cmd,
+    help_keystate_export_cmd,
     
     help_enforce_zones_cmd,
     help_signconf_cmd,
@@ -160,6 +162,7 @@ enforcer_commands[] = {
     handled_keystate_show_cmd,
     handled_keystate_ds_submit_cmd,
     handled_keystate_ds_seen_cmd,
+    handled_keystate_export_cmd,
     
     handled_enforce_zones_cmd,
     handled_signconf_cmd,
