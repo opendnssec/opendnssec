60a8de1ea83c50be06b9a130de33ccf861a82fd2
Matthijs Mekking <matthijs@nlnetlabs.nl>
2011-06-23 11:20:56 +0000

    Pivotal #14922121 in trunk

--- signer/src/daemon/cmdhandler.c
+++ signer/src/daemon/cmdhandler.c
@@ -207,7 +207,7 @@ cmdhandler_handle_cmd_update(int sockfd, cmdhandler_type* cmdc,
         zone = zonelist_lookup_zone_by_name(cmdc->engine->zonelist, tbd,
             LDNS_RR_CLASS_IN);
         /* If this zone is just added, don't update (it might not have a task yet) */
-        if (zone->just_added) {
+        if (zone && zone->just_added) {
             zone = NULL;
         }
         /* [UNLOCK] zonelist */
@@ -304,7 +304,7 @@ cmdhandler_handle_cmd_sign(int sockfd, cmdhandler_type* cmdc, const char* tbd)
         zone = zonelist_lookup_zone_by_name(cmdc->engine->zonelist, tbd,
             LDNS_RR_CLASS_IN);
         /* If this zone is just added, don't update (it might not have a task yet) */
-        if (zone->just_added) {
+        if (zone && zone->just_added) {
             zone = NULL;
         }
         /* [UNLOCK] zonelist */
