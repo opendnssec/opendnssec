4ba1cf4858daee3a039c4de0a0de1df1a3b494f0
René Post <rene@xpt.nl>
2011-09-01 11:08:39 +0000

    Remove --task parameter from ds-submit command

--- enforcer-ng/src/keystate/keystate_ds_submit_cmd.cpp
+++ enforcer-ng/src/keystate/keystate_ds_submit_cmd.cpp
@@ -28,7 +28,6 @@ void help_keystate_ds_submit_cmd(int sockfd)
         "  --id <id>     (aka -k) perform submit for key with id <id>.\n"
         "  --auto        (aka -a) perform submit for all keys that actually "
                         "need it.\n"
-        "  --task        (aka -t) schedule command to run once as a separate task.\n"
         );
     ods_writen(sockfd, buf, strlen(buf));
 }
@@ -69,7 +68,6 @@ int handled_keystate_ds_submit_cmd(int sockfd, engine_type* engine,
     (void)ods_find_arg_and_param(&argc,argv,"zone","z",&zone);
     (void)ods_find_arg_and_param(&argc,argv,"id","k",&id);
     bool bAutomatic = ods_find_arg(&argc,argv,"auto","a") != -1;
-    bool bScheduleTask = ods_find_arg(&argc,argv,"task","t") != -1;
     if (argc) {
         ods_log_warning("[%s] unknown arguments for %s command",
                         module_str,scmd);
@@ -78,47 +76,13 @@ int handled_keystate_ds_submit_cmd(int sockfd, engine_type* engine,
         return 1; // errors, but handled
     }
     
-    if (bScheduleTask) {
-        if (zone || id)  {
-            ods_log_crit("[%s] --zone and/or --id options not allowed when "
-                         "scheduling as %s task", module_str, scmd);
-            (void)snprintf(buf, ODS_SE_MAXLINE,
-                           "Unable to schedule %s task, --zone and/or --id "
-                           "not allowed\n",scmd);
-            ods_writen(sockfd, buf, strlen(buf));
-        } else {
-            /* schedule task */
-            task = keystate_ds_submit_task(engine->config,scmd);
-            if (!task) {
-                ods_log_crit("[%s] failed to create %s task",
-                             module_str,scmd);
-                (void)snprintf(buf, ODS_SE_MAXLINE,
-                               "Failed to create %s task.\n",scmd);
-                ods_writen(sockfd, buf, strlen(buf));
-            } else {
-                status = schedule_task_from_thread(engine->taskq, task, 0);
-                if (status != ODS_STATUS_OK) {
-                    ods_log_crit("[%s] failed to create %s task",
-                                 module_str,scmd);
-                    (void)snprintf(buf, ODS_SE_MAXLINE,
-                                   "Unable to schedule %s task.\n",scmd);
-                    ods_writen(sockfd, buf, strlen(buf));
-                } else {
-                    (void)snprintf(buf, ODS_SE_MAXLINE,"Scheduled %s task.\n",
-                                   scmd);
-                    ods_writen(sockfd, buf, strlen(buf));
-                }
-            }
-        }
-    } else {
-        /* perform task immediately */
-        time_t tstart = time(NULL);
-        perform_keystate_ds_submit(sockfd,engine->config,zone,id,bAutomatic?1:0);
-        if (!zone && !id) {
-            (void)snprintf(buf, ODS_SE_MAXLINE, "%s completed in %ld seconds.\n",
-                           scmd,time(NULL)-tstart);
-            ods_writen(sockfd, buf, strlen(buf));
-        }
+    /* perform task immediately */
+    time_t tstart = time(NULL);
+    perform_keystate_ds_submit(sockfd,engine->config,zone,id,bAutomatic?1:0);
+    if (!zone && !id) {
+        (void)snprintf(buf, ODS_SE_MAXLINE, "%s completed in %ld seconds.\n",
+                       scmd,time(NULL)-tstart);
+        ods_writen(sockfd, buf, strlen(buf));
     }
 
     return 1;
