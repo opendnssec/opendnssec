351fade074d6b6b716a1419194c0ad5b3af9a91f
René Post <rene@xpt.nl>
2011-09-01 11:09:12 +0000

    Remove resalt from setup, just reload the engine after setup causing the autostart tasks to be started one of which is the resalt task.

--- enforcer-ng/src/enforcer/setup_cmd.cpp
+++ enforcer-ng/src/enforcer/setup_cmd.cpp
@@ -10,7 +10,6 @@ extern "C" {
 #include "enforcer/setup_cmd.h"
 
 #include "policy/update_kasp_task.h"
-#include "policy/policy_resalt_task.h"
 #include "keystate/update_keyzones_task.h"
 
 #include "shared/duration.h"
@@ -30,32 +29,16 @@ void help_setup_cmd(int sockfd)
     char buf[ODS_SE_MAXLINE];
     (void) snprintf(buf, ODS_SE_MAXLINE,
         "setup           delete existing database files and then perform:\n"
-        "                  update kasp\n"
-        "                  policy resalt\n"
-        "                  update zonelist\n"
+        "                  update kasp - to import kasp.xml\n"
+        "                  update zonelist - to import zonelist.xml\n"
         );
     ods_writen(sockfd, buf, strlen(buf));
 }
 
-/**
- * Handle the 'setup' command.
- *
- */
-int handled_setup_cmd(int sockfd, engine_type* engine,
-                      const char *cmd, ssize_t n)
+static void
+delete_database_files(int sockfd, const char *datastore)
 {
     char buf[ODS_SE_MAXLINE];
-    const char *scmd = "setup";
-
-    cmd = ods_check_command(cmd,n,scmd);
-    if (!cmd)
-        return 0; // not handled
-    
-    ods_log_debug("[%s] %s command", module_str, scmd);
-
-    time_t tstart = time(NULL);
-
-    const char *datastore = engine->config->datastore;
     std::string policy_pb = std::string(datastore) + ".policy.pb";
     std::string keystate_pb = std::string(datastore) + ".keystate.pb";
     std::string hsmkey_pb = std::string(datastore) + ".hsmkey.pb";
@@ -80,10 +63,48 @@ int handled_setup_cmd(int sockfd, engine_type* engine,
                        hsmkey_pb.c_str(),strerror(errno));
         ods_writen(sockfd, buf, strlen(buf));
     }
-    
+}
+
+static void reload_engine(int sockfd, engine_type* engine)
+{
+    char buf[ODS_SE_MAXLINE];
+    ods_log_assert(engine);
+    engine->need_to_reload = 1;
+    lock_basic_lock(&engine->signal_lock);
+    /* [LOCK] signal */
+    lock_basic_alarm(&engine->signal_cond);
+    /* [UNLOCK] signal */
+    lock_basic_unlock(&engine->signal_lock);
+    ods_log_debug("[%s] reloading engine...", module_str);
+    (void)snprintf(buf, ODS_SE_MAXLINE, "reloading engine...\n");
+    ods_writen(sockfd, buf, strlen(buf));
+}
+
+/**
+ * Handle the 'setup' command.
+ *
+ */
+int handled_setup_cmd(int sockfd, engine_type* engine,
+                      const char *cmd, ssize_t n)
+{
+    char buf[ODS_SE_MAXLINE];
+    const char *scmd = "setup";
+
+    cmd = ods_check_command(cmd,n,scmd);
+    if (!cmd)
+        return 0; // not handled
+
+    ods_log_debug("[%s] %s command", module_str, scmd);
+
+    time_t tstart = time(NULL);
+
+    delete_database_files(sockfd, engine->config->datastore);
+
     perform_update_kasp(sockfd, engine->config);
-    perform_policy_resalt(sockfd, engine->config);
+
     perform_update_keyzones(sockfd, engine->config);
+
+    reload_engine(sockfd, engine);
 
     (void)snprintf(buf, ODS_SE_MAXLINE, "%s completed in %ld seconds.\n",
                    scmd,time(NULL)-tstart);
