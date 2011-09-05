285c45363b00c4c5efe264d18b4c5ed6bd33f979
René Post <rene@xpt.nl>
2011-09-05 10:47:40 +0000

    Flush the enforcer task after an 'update zonelist' command, so it picks up and enforces newly added zones automatically.

--- enforcer-ng/src/keystate/update_keyzones_cmd.cpp
+++ enforcer-ng/src/keystate/update_keyzones_cmd.cpp
@@ -7,6 +7,7 @@
 extern "C" {
 #include "keystate/update_keyzones_cmd.h"
 #include "keystate/update_keyzones_task.h"
+#include "enforcer/enforce_task.h"
 #include "shared/duration.h"
 #include "shared/file.h"
 #include "shared/str.h"
@@ -39,13 +40,27 @@ handled_update_keyzones_cmd(int sockfd, engine_type* engine, const char *cmd,
         return 0; // not handled
 
     ods_log_debug("[%s] %s command", module_str, scmd);
-
+    
     /* perform task immediately */
     time_t tstart = time(NULL);
     perform_update_keyzones(sockfd,engine->config);
     (void)snprintf(buf, ODS_SE_MAXLINE, "%s completed in %ld seconds.\n",
                    scmd,time(NULL)-tstart);
     ods_writen(sockfd, buf, strlen(buf));
+
+    /* flush (force to run) the enforcer task when it is waiting in the 
+       task list. */
+    task_type *enf = enforce_task(engine,"enforce","next zone");
+    lock_basic_lock(&engine->taskq->schedule_lock);
+    /* [LOCK] schedule */
+    task_type *running_enforcer = schedule_lookup_task(engine->taskq, enf);
+    task_cleanup(enf);
+    if (running_enforcer)
+        running_enforcer->flush = 1;
+    /* [UNLOCK] schedule */
+    lock_basic_unlock(&engine->taskq->schedule_lock);
+    if (running_enforcer)
+        engine_wakeup_workers(engine);
 
     return 1;
 }
