fb6adc54a9f3d69dc013c3710baec9ccdbeb3052
Jerry Lundström <jerry.lundstrom@iis.se>
2011-09-21 07:16:08 +0000

    Pulled some stuff from trunk, use prepdist.sh to make a dist now.

---
+++ prepdist.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# $Id: prepdist.sh 5634 2011-09-13 15:20:29Z jerry $
+
+PREFIX=/var/tmp/opendnssec-release
+
+sh autogen.sh &&
+sh configure \
+	--prefix=${PREFIX} \
+	--enable-auditor $@ &&
+make dist
