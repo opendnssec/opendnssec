a2c7aba9353a1871bfbc8cebdb57a26fe907aac1
Jerry Lundström <jerry.lundstrom@iis.se>
2012-02-07 10:10:34 +0000

    The almighty test framework

---
+++ testing/build-ldns.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+source `dirname "$0"`/lib.sh && init || exit 1
+
+LDNS="ldns-1.6.12"
+LDNS_URL="http://nlnetlabs.nl/downloads/ldns/$LDNS.tar.gz"
+LDNS_FILENAME="$LDNS.tar.gz"
+LDNS_HASH_TYPE="sha1"
+LDNS_HASH="1d61df0f666908551d5a62768f77d63e727810aa"
+
+check_if_built ldns && exit 0
+start_build ldns
+
+LDNS_SRC=`fetch_src "$LDNS_URL" "$LDNS_FILENAME" "$LDNS_HASH_TYPE" "$LDNS_HASH"`
+
+build_ok=0
+case "$DISTRIBUTION" in
+	centos | \
+	redhat | \
+	fedora | \
+	sl | \
+	ubuntu | \
+	debian | \
+	opensuse | \
+	freebsd | \
+	sunos | \
+	openbsd )
+		(
+			gunzip -c "$LDNS_SRC" | tar xf - &&
+			cd "$LDNS" &&
+			./configure --prefix="$INSTALL_ROOT" \
+				--disable-gost &&
+			$MAKE &&
+			$MAKE install
+		) &&
+		build_ok=1
+		;;
+	netbsd )
+		(
+			gunzip -c "$LDNS_SRC" | tar xf - &&
+			cd "$LDNS" &&
+			patch -p1 < ../ldns-1.6.12-doxyparse.pl-netbsd.patch &&
+			./configure --prefix="$INSTALL_ROOT" \
+				--disable-gost &&
+			$MAKE &&
+			$MAKE install
+		) &&
+		build_ok=1
+		;;
+esac
+
+if [ "$build_ok" -eq 1 ]; then
+	set_build_ok ldns || exit 1
+	exit 0
+fi
+
+exit 1
---
+++ testing/build-opendnssec.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+source `dirname "$0"`/lib.sh && init || exit 1
+
+require ldns
+require softhsm
+
+check_if_built opendnssec && exit 0
+start_build opendnssec
+
+build_ok=0
+case "$DISTRIBUTION" in
+	openbsd )
+		export AUTOCONF_VERSION="2.68"
+		export AUTOMAKE_VERSION="1.11"
+		;;
+	sunos | \
+	netbsd | \
+	freebsd )
+		append_cflags "-std=c99"
+		;;
+	opensuse )
+		append_ldflags "-lncurses"
+		;;
+esac
+case "$DISTRIBUTION" in
+	centos | \
+	redhat | \
+	fedora | \
+	sl | \
+	ubuntu | \
+	debian | \
+	openbsd | \
+	sunos | \
+	opensuse )
+		(
+			sh autogen.sh &&
+			mkdir -p build &&
+			cd build &&
+			../configure --prefix="$INSTALL_ROOT" \
+				--with-database-backend=sqlite3 \
+				--with-dbname=opendnssec-build-test \
+				--disable-auditor \
+				--enable-timeshift &&
+			$MAKE &&
+			$MAKE check &&
+			$MAKE install &&
+			cp "conf/conf.xml" "$INSTALL_ROOT/etc/opendnssec/conf.xml.build" &&
+			cp "conf/kasp.xml" "$INSTALL_ROOT/etc/opendnssec/kasp.xml.build" &&
+			cp "conf/zonefetch.xml" "$INSTALL_ROOT/etc/opendnssec/zonefetch.xml.build" &&
+			cp "conf/zonelist.xml" "$INSTALL_ROOT/etc/opendnssec/zonelist.xml.build"
+		) &&
+		build_ok=1
+		;;
+	netbsd )
+		(
+			sh autogen.sh &&
+			mkdir -p build &&
+			cd build &&
+			../configure --prefix="$INSTALL_ROOT" \
+				--with-cunit=/usr/pkg \
+				--with-database-backend=sqlite3 \
+				--with-dbname=opendnssec-build-test \
+				--disable-auditor \
+				--enable-timeshift \
+				--with-sqlite3=/usr/pkg &&
+			$MAKE &&
+			$MAKE check &&
+			$MAKE install &&
+			cp "conf/conf.xml" "$INSTALL_ROOT/etc/opendnssec/conf.xml.build" &&
+			cp "conf/kasp.xml" "$INSTALL_ROOT/etc/opendnssec/kasp.xml.build" &&
+			cp "conf/zonefetch.xml" "$INSTALL_ROOT/etc/opendnssec/zonefetch.xml.build" &&
+			cp "conf/zonelist.xml" "$INSTALL_ROOT/etc/opendnssec/zonelist.xml.build"
+		) &&
+		build_ok=1
+		;;
+	freebsd )
+		(
+			sh autogen.sh &&
+			mkdir -p build &&
+			cd build &&
+			../configure --prefix="$INSTALL_ROOT" \
+				--with-database-backend=sqlite3 \
+				--with-dbname=opendnssec-build-test \
+				--disable-auditor \
+				--enable-timeshift &&
+			$MAKE &&
+			#$MAKE check && # segfaults #0  0x00000008019363dc in _pthread_mutex_init_calloc_cb () from /lib/libc.so.7
+			$MAKE install &&
+			cp "conf/conf.xml" "$INSTALL_ROOT/etc/opendnssec/conf.xml.build" &&
+			cp "conf/kasp.xml" "$INSTALL_ROOT/etc/opendnssec/kasp.xml.build" &&
+			cp "conf/zonefetch.xml" "$INSTALL_ROOT/etc/opendnssec/zonefetch.xml.build" &&
+			cp "conf/zonelist.xml" "$INSTALL_ROOT/etc/opendnssec/zonelist.xml.build"
+		) &&
+		build_ok=1
+		;;
+esac
+
+if [ "$build_ok" -eq 1 ]; then
+	set_build_ok opendnssec || exit 1
+	exit 0
+fi
+
+exit 1
