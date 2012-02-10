47e29c65cb8a4ab7fc70b14a98aedb28b0c41742
Jerry Lundström <jerry.lundstrom@iis.se>
2012-02-10 11:58:34 +0000

    Can not rely on Engine shut down to be present on all platforms

--- testing/test-cases.d/00-start-and-stop/test.sh
+++ testing/test-cases.d/00-start-and-stop/test.sh
@@ -7,7 +7,6 @@ log_grep ods-control-start stdout 'Engine running' &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
 syslog_grep 'ods-enforcerd: .*Sleeping for' &&
 log_this ods-control-stop ods-control stop &&
-log_grep ods-control-stop stdout 'Engine shut down' &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' &&
 syslog_grep 'ods-enforcerd: .*all done' &&
 return
