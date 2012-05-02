095dfa7710fcf70f1651125a635046c2f989ab02
Siôn Llyod <sion@nominet.org.uk>
2012-05-02 08:02:02 +0000

    OPENDNSSEC-249: ods-ksmutil: If key export finds nothing to do then say so rather than display nothing which might be misinterpreted.

--- enforcer/utils/ksmutil.c
+++ enforcer/utils/ksmutil.c
@@ -1469,6 +1469,8 @@ cmd_exportkeys ()
     size_t  nchar;          /* Number of characters written */
     char    buffer[256];    /* For constructing part of the command */
 
+	int done_something = 0; /* Have we exported any keys? */
+
     /* See what arguments we were passed (if any) otherwise set the defaults */
     /* Check keystate, can be state or keytype */
     if (o_keystate != NULL) {
@@ -1679,6 +1681,8 @@ cmd_exportkeys ()
                 ldns_rr_print(stdout, ds_sha256_rr);
             }
 
+			done_something = 1;
+
             hsm_sign_params_free(sign_params);
             hsm_key_free(key);
             status = KsmKey(result, &data);
@@ -1695,7 +1699,16 @@ cmd_exportkeys ()
 		printf("\nWARNING: No active or ready keys seen for this zone. Do not load any DS records to the parent unless you understand the possible consequences.\n");
 	} else if (ds_flag == 1 && red_seen == 1 && act_seen == 1) {
 		printf("\nWARNING: BOTH ready and active keys seen for this zone. Probably a key rollover is happening and you may only want the ready key to be submitted.\n");
-				}
+	}
+
+	/* If we did nothing then explain why not */
+	if (!done_something) {
+		if (state_id != -1) {
+			printf("No keys in %s state to export.\n", KsmKeywordStateValueToName(state_id) );
+		} else {
+			printf("No keys in READY state or higher to export.\n");
+		}
+	}
 
     /* TODO when the above is working then replicate it twice for the case where keytype == -1 */
 
