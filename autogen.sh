342760094e40068b2a11bdff17612dc36db1ea7e
Jerry Lundström <jerry.lundstrom@iis.se>
2012-05-04 08:30:36 +0000

    Try and make a symbolic link of the version.m4 and if even that fails just do a copy, to support file systems that does not have hard links.

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,9 @@ if [ `dirname $0` = ".." ]; then
 	if [ -f ../${VERSION} ]; then
 		echo Creating ${VERSION} &&
 		rm -f ${VERSION} &&
-		ln ../${VERSION} ${VERSION}
+		ln ../${VERSION} ${VERSION} 2>/dev/null ||
+		ln -s ../${VERSION} ${VERSION} 2>/dev/null ||
+		cp ../${VERSION} ${VERSION}
 	fi
 fi &&
 
