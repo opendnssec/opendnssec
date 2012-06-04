24f95e18bdaa4391a024dfa8f7dd9171d56a9fbf
Yuri Schaeffer <yuri@NLnetLabs.nl>
2012-06-04 10:23:44 +0000

    newline on shutdown is special case. #OPENDNSSEC-270

--- enforcer-ng/src/ods-enforcer.c
+++ enforcer-ng/src/ods-enforcer.c
@@ -206,6 +206,8 @@ interface_run(FILE* fp, int sockfd, char* cmd)
                 /* written+ret < n : means partial write, requires us to loop... */
             }
             if (ods_strcmp(buf, ODS_SE_STOP_RESPONSE) == 0 || cmd_response) {
+                /* we do no further reading, flush */
+                write(fileno(stdout), "\n", 1);
                 return;
             }
         }
