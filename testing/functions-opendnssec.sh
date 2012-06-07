adce66f734735f381647f783985cd01387484a8b
Jerry Lundström <jerry.lundstrom@iis.se>
2012-06-07 09:43:22 +0000

    Update test scripts and function from trunk

--- testing/functions-opendnssec.sh
+++ testing/functions-opendnssec.sh
@@ -2,31 +2,12 @@
 
 ods_pre_test ()
 {
-	if [ -e softhsm.conf ]; then
-		if ! cp softhsm.conf "$INSTALL_ROOT/etc/softhsm.conf" 2>/dev/null; then
-			echo "pre_test: unable to copy/install test specific softhsm.conf to $INSTALL_ROOT/etc/softhsm.conf" >&2
-			return 1
-		fi
-	else
-		if ! cp "$INSTALL_ROOT/etc/softhsm.conf.build" "$INSTALL_ROOT/etc/softhsm.conf" 2>/dev/null; then
-			echo "pre_test: unable to copy/install build default $INSTALL_ROOT/etc/softhsm.conf.build to $INSTALL_ROOT/etc/softhsm.conf" >&2
-			return 1
-		fi
-	fi
-
-	for file in addns.xml conf.xml kasp.xml zonelist.xml; do
-		if [ -e "$file" ]; then
-			if ! cp "$file" "$INSTALL_ROOT/etc/opendnssec/$file" 2>/dev/null; then
-				echo "pre_test: unable to copy/install test specific $file to $INSTALL_ROOT/etc/opendnssec/$file" >&2
-				return 1
-			fi
-		else
-			if ! cp "$INSTALL_ROOT/etc/opendnssec/$file.build" "$INSTALL_ROOT/etc/opendnssec/$file" 2>/dev/null; then
-				echo "pre_test: unable to copy/install build default $INSTALL_ROOT/etc/opendnssec/$file.build to $INSTALL_ROOT/etc/opendnssec/$file" >&2
-				return 1
-			fi
-		fi
-	done
+	ods_nuke_env &&
+	ods_setup_conf &&
+	ods_setup_zone &&
+	return 0
+	
+	return 1
 }
 
 ods_post_test ()
@@ -34,14 +15,245 @@ ods_post_test ()
 	true
 }
 
+ods_nuke_env ()
+{
+	local tmp_files=`ls "$INSTALL_ROOT/var/opendnssec/tmp/" 2>/dev/null`
+	local unsigned_files=`ls "$INSTALL_ROOT/var/opendnssec/unsigned/" 2>/dev/null`
+	local signed_files=`ls "$INSTALL_ROOT/var/opendnssec/signed/" 2>/dev/null`
+	local softhsm_files=`ls "$INSTALL_ROOT/var/softhsm/" 2>/dev/null`
+	
+	if [ -n "$tmp_files" ]; then
+		(
+			cd "$INSTALL_ROOT/var/opendnssec/tmp/" &&
+			rm -rf -- $tmp_files 2>/dev/null
+		)
+	fi &&
+	if [ -n "$unsigned_files" ]; then
+		(
+			cd "$INSTALL_ROOT/var/opendnssec/unsigned/" &&
+			rm -f -- $unsigned_files 2>/dev/null
+		)
+	fi &&
+	if [ -n "$signed_files" ]; then
+		(
+			cd "$INSTALL_ROOT/var/opendnssec/signed/" &&
+			rm -f -- $signed_files 2>/dev/null
+		)
+	fi &&
+	if [ -n "$softhsm_files" ]; then
+		(
+			cd "$INSTALL_ROOT/var/softhsm/" &&
+			rm -f -- $softhsm_files 2>/dev/null
+		)
+	fi &&
+	return 0
+	
+	return 1
+}
+
+ods_setup_conf ()
+{
+	local conf="$1"
+	local file="$2"
+	local conf_file
+	
+	if [ -n "$conf" ]; then
+		case "$conf" in
+			softhsm.conf | addns.xml | conf.xml | kasp.xml | zonelist.xml )
+				;;
+			* )
+				echo "ods_setup_conf: Unknown conf file specified: $conf" >&2
+				return 1
+				;;
+		esac
+	fi
+	
+	if [ -n "$file" -a ! -f "$file" ]; then
+		echo "ods_setup_conf: Conf file $file does not exist" >&2
+		return 1
+	fi
+
+	# Conf files under /etc	
+	for conf_file in softhsm.conf; do
+		if [ -n "$conf" -a "$conf" != "$conf_file" ]; then
+			continue
+		fi
+
+		if [ -n "$file" ]; then
+			if ! cp -- "$file" "$INSTALL_ROOT/etc/$conf_file" 2>/dev/null; then
+				echo "ods_setup_conf: unable to copy/install test specific $file to $INSTALL_ROOT/etc/$conf_file" >&2
+				return 1
+			fi
+		elif [ -f "$conf_file" ]; then
+			if ! cp -- "$conf_file" "$INSTALL_ROOT/etc/$conf_file" 2>/dev/null; then
+				echo "ods_setup_conf: unable to copy/install test specific $conf_file to $INSTALL_ROOT/etc/$conf_file" >&2
+				return 1
+			fi
+		else
+			if ! cp -- "$INSTALL_ROOT/etc/$conf_file.build" "$INSTALL_ROOT/etc/$conf_file" 2>/dev/null; then
+				echo "ods_setup_conf: unable to copy/install build default $INSTALL_ROOT/etc/$conf_file.build to $INSTALL_ROOT/etc/$conf_file" >&2
+				return 1
+			fi
+		fi
+		
+		apply_parameter "INSTALL_ROOT" "$INSTALL_ROOT" "$INSTALL_ROOT/etc/$conf_file" ||
+		return 1
+	done
+
+	# Conf files under /etc/opendnssec
+	for conf_file in addns.xml conf.xml kasp.xml zonelist.xml; do
+		if [ -n "$conf" -a "$conf" != "$conf_file" ]; then
+			continue
+		fi
+		
+		if [ -n "$file" ]; then
+			if ! cp -- "$file" "$INSTALL_ROOT/etc/opendnssec/$conf_file" 2>/dev/null; then
+				echo "ods_setup_conf: unable to copy/install test specific $file to $INSTALL_ROOT/etc/opendnssec/$conf_file" >&2
+				return 1
+			fi
+		elif [ -f "$conf_file" ]; then
+			if ! cp -- "$conf_file" "$INSTALL_ROOT/etc/opendnssec/$conf_file" 2>/dev/null; then
+				echo "ods_setup_conf: unable to copy/install test specific $conf_file to $INSTALL_ROOT/etc/opendnssec/$conf_file" >&2
+				return 1
+			fi
+		else
+			if ! cp -- "$INSTALL_ROOT/etc/opendnssec/$conf_file.build" "$INSTALL_ROOT/etc/opendnssec/$conf_file" 2>/dev/null; then
+				echo "ods_setup_conf: unable to copy/install build default $INSTALL_ROOT/etc/opendnssec/$conf_file.build to $INSTALL_ROOT/etc/opendnssec/$conf_file" >&2
+				return 1
+			fi
+		fi
+		
+		apply_parameter "INSTALL_ROOT" "$INSTALL_ROOT" "$INSTALL_ROOT/etc/opendnssec/$conf_file" &&
+		apply_parameter "SOFTHSM_MODULE" "$SOFTHSM_MODULE" "$INSTALL_ROOT/etc/opendnssec/$conf_file" ||
+		return 1
+	done
+	
+	return 0
+}
+
+ods_setup_zone ()
+{
+	local zone="$1"
+	
+	if [ -n "$zone" -a ! -f "$zone" ]; then
+		echo "ods_setup_zone: Zone file $zone does not exist" >&2
+		return 1
+	fi
+	
+	if [ -n "$zone" ]; then
+		if ! cp -- "$zone" "$INSTALL_ROOT/var/opendnssec/unsigned/" 2>/dev/null; then
+			echo "ods_setup_conf: unable to copy/install zone file $zone to $INSTALL_ROOT/var/opendnssec/unsigned/" >&2
+			return 1
+		fi
+		
+		return 0
+	fi
+	
+	if [ -d unsigned ]; then
+		ls -1 unsigned/ | while read zone; do
+			if [ -f "unsigned/$zone" ]; then
+				if ! cp -- "unsigned/$zone" "$INSTALL_ROOT/var/opendnssec/unsigned/" 2>/dev/null; then
+					echo "ods_setup_conf: unable to copy/install zone file $zone to $INSTALL_ROOT/var/opendnssec/unsigned/" >&2
+					return 1
+				fi
+			fi
+		done
+	fi
+	
+	return 0
+}
+
 ods_reset_env ()
 {
 	echo "ods_reset_env: resetting opendnssec environment"
-	echo "y" | ods-enforcer setup &&
-	log_this softhsm-init-token softhsm --init-token --slot 0 --label OpenDNSSEC --pin 1234 --so-pin 1234 ||
-	return 1
 	
-	if ! log_grep softhsm-init-token stdout "The token has been initialized."; then
+	ods_softhsm_init_token 0 &&
+	echo "y" | log_this "ods-ksmutil-setup" ods-ksmutil setup &&
+	return 0
+	
+	return 1
+}
+
+ods_kill ()
+{
+	if ! pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null; then
+		return 0
+	fi
+	
+	echo "ods_kill: Killing OpenDNSSEC"
+	try_run 15 ods-control stop
+	
+	if pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null; then
+		sleep 2
+		pkill -QUIT '(ods-enforcerd|ods-signerd)' 2>/dev/null
+		if pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null; then
+			sleep 2
+			pkill -TERM '(ods-enforcerd|ods-signerd)' 2>/dev/null
+			if pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null; then
+				sleep 2
+				pkill -KILL '(ods-enforcerd|ods-signerd)' 2>/dev/null
+				pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null &&
+				sleep 2
+			fi
+		fi
+	fi
+	
+	if pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null; then
+		echo "ods_kill: Tried to kill ods-enforcerd and ods-signerd but some are still alive!" >&2
 		return 1
 	fi
+	
+	return 0
+}
+
+ods_softhsm_init_token ()
+{
+	if [ -z "$1" ]; then
+		echo "usage: ods_softhsm_init_token <slot> [label] [pin] [so-pin]" >&2
+		exit 1
+	fi
+
+	local slot="$1"
+	local label="$2"
+	local pin="$3"
+	local so_pin="$4"
+	
+	if [ -z "$label" ]; then
+		label=OpenDNSSEC
+	fi
+	if [ -z "$pin" ]; then
+		pin=1234
+	fi
+	if [ -z "$so_pin" ]; then
+		so_pin=1234
+	fi
+	
+	if [ "$slot" -ge 0 -a "$slot" -lt 20 ] 2>/dev/null; then
+		log_remove "softhsm-init-token-$slot" &&
+		log_this "softhsm-init-token-$slot" softhsm --init-token --slot "$slot" --label "$label" --pin "$pin" --so-pin "$so_pin" ||
+		return 1
+		
+		if ! log_grep "softhsm-init-token-$slot" stdout "The token has been initialized."; then
+			return 1
+		fi
+	else
+		echo "ods_softhsm_init_token: Slot $slot invalid, must be integer between 0 and 20" >&2
+		exit 1
+	fi
+	
+	return 0
+}
+
+ods_find_softhsm_module ()
+{
+	local path
+	
+	for path in lib64/softhsm lib/softhsm lib64 lib; do
+		if [ -f "$INSTALL_ROOT/$path/libsofthsm.so" ]; then
+			export SOFTHSM_MODULE="$INSTALL_ROOT/$path/libsofthsm.so"
+			return 0
+		fi
+	done
+	
+	return 1
 }
