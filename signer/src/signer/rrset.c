2ddc27c0f7418ddb1b32611dfbdd03415f4328b0
Matthijs Mekking <matthijs@nlnetlabs.nl>
2012-06-20 11:51:26 +0000

    OPENDNSSEC-282: RRSIGs for records that become glue will be left around in the signed zone

--- signer/src/signer/rrset.c
+++ signer/src/signer/rrset.c
@@ -483,7 +483,7 @@ rrset_recycle(rrset_type* rrset, time_t signtime, ldns_rr_type dstatus,
     for (i=0; i < rrset->rrsig_count; i++) {
         drop_sig = 0;
         /* 0. Skip delegation, glue and occluded RRsets */
-        if (dstatus != LDNS_RR_TYPE_SOA && (delegpt != LDNS_RR_TYPE_SOA &&
+        if (dstatus != LDNS_RR_TYPE_SOA || (delegpt != LDNS_RR_TYPE_SOA &&
             rrset->rrtype != LDNS_RR_TYPE_DS)) {
             drop_sig = 1;
             goto recycle_drop_sig;
@@ -659,15 +659,17 @@ rrset_sign(hsm_ctx_t* ctx, rrset_type* rrset, time_t signtime)
     rrset->needs_signing = 0;
     /* Skip delegation, glue and occluded RRsets */
     if (dstatus != LDNS_RR_TYPE_SOA) {
-        ods_log_deeebug("[%s] skip signing RRset[%i]: occluded RRset",
-            rrset_str, rrset->rrtype);
+        log_rrset(ldns_rr_owner(rrset->rrs[0].rr), rrset->rrtype,
+            "[rrset] skip signing occluded RRset", LOG_DEEEBUG);
         return ODS_STATUS_OK;
     }
     if (delegpt != LDNS_RR_TYPE_SOA && rrset->rrtype != LDNS_RR_TYPE_DS) {
-        ods_log_deeebug("[%s] skip signing RRset[%i]: delegation RRset",
-            rrset_str, rrset->rrtype);
+        log_rrset(ldns_rr_owner(rrset->rrs[0].rr), rrset->rrtype,
+            "[rrset] skip signing delegation RRset", LOG_DEEEBUG);
         return ODS_STATUS_OK;
     }
+    log_rrset(ldns_rr_owner(rrset->rrs[0].rr), rrset->rrtype,
+        "[rrset] sign RRset", LOG_DEEEBUG);
     ods_log_assert(dstatus == LDNS_RR_TYPE_SOA ||
         (delegpt == LDNS_RR_TYPE_SOA || rrset->rrtype == LDNS_RR_TYPE_DS));
     /* Transmogrify rrset */
