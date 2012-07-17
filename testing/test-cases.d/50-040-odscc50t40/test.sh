011ebbdbcbe65ce571715c3869cac4567883d087
Jerry Lundström <jerry.lundstrom@iis.se>
2012-07-17 07:28:52 +0000

    Give test 50-040/50-050 more time to notice how many threads are created, also add sleep to 50-050 to give it time to send all logs to syslog before shutdown.

--- testing/test-cases.d/50-040-odscc50t40/test.sh
+++ testing/test-cases.d/50-040-odscc50t40/test.sh
@@ -12,11 +12,12 @@ log_this_timeout ods-control-start 30 ods-control start &&
 syslog_waitfor 60 'ods-enforcerd: .*Sleeping for' &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
 
+syslog_waitfor 10 'ods-signerd: .*\[worker\[1\]\] report for duty' &&
+
 log_this_timeout ods-control-stop 30 ods-control stop &&
 syslog_waitfor 60 'ods-enforcerd: .*all done' &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' &&
 
-syslog_grep 'ods-signerd: .*\[worker\[1\]\] report for duty' &&
 ! syslog_grep 'ods-signerd: .*\[worker\[2\]\] report for duty' &&
 return 0
 
