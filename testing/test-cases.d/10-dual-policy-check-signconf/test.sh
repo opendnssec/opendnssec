333bfceb084bc15b6a7558e7e53cc78fae680018
Jerry Lundström <jerry.lundstrom@iis.se>
2012-08-15 11:42:56 +0000

    Removed space after OptOut

--- testing/test-cases.d/10-dual-policy-check-signconf/test.sh
+++ testing/test-cases.d/10-dual-policy-check-signconf/test.sh
@@ -22,7 +22,7 @@ $GREP -q -- "<Default>PT1209600S</Default>" "$INSTALL_ROOT/var/opendnssec/signco
 $GREP -q -- "<Denial>PT1296000S</Denial>" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
 $GREP -q -- "<Jitter>PT43200S</Jitter>" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
 $GREP -q -- "<InceptionOffset>PT3600S</InceptionOffset>" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
-$GREP -q -- "<OptOut />" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
+$GREP -q -- "<OptOut/>" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
 $GREP -q -- "<TTL>PT3600S</TTL>" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
 $GREP -q -- "<Minimum>PT3600S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
 $GREP -q -- "<Serial>unixtime</Serial>" "$INSTALL_ROOT/var/opendnssec/signconf/ods.xml" &&
@@ -34,7 +34,7 @@ $GREP -q -- "<Default>PT1814400S</Default>" "$INSTALL_ROOT/var/opendnssec/signco
 $GREP -q -- "<Denial>PT1728000S</Denial>" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
 $GREP -q -- "<Jitter>PT36000S</Jitter>" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
 $GREP -q -- "<InceptionOffset>PT3000S</InceptionOffset>" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
-! $GREP -q -- "<OptOut />" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
+! $GREP -q -- "<OptOut/>" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
 $GREP -q -- "<TTL>PT3400S</TTL>" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
 $GREP -q -- "<Minimum>PT6000S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
 $GREP -q -- "<Serial>counter</Serial>" "$INSTALL_ROOT/var/opendnssec/signconf/ods2.xml" &&
