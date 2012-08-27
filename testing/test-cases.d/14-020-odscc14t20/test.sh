184738c34652230e44c64b54581c0627b5788596
Jerry Lundström <jerry.lundstrom@iis.se>
2012-08-27 07:56:03 +0000

    Use static ports, add mysql conf, add missing command link.

--- testing/test-cases.d/14-020-odscc14t20/test.sh
+++ testing/test-cases.d/14-020-odscc14t20/test.sh
@@ -8,21 +8,14 @@
 ## It requires a checker tool like wdiff or ldns-verify-zone to review
 ## the result (possibly with an known good file).
 
-## Configuration set up
-ods_get_random_port 2 &&
-LISTENER_PORT=$RND_PORT &&
-SLAVE_PORT=$(($LISTENER_PORT + 1)) &&
-cp conf.xml.in conf.xml &&
-cp addns.xml.in addns.xml &&
-apply_parameter "LISTENER_PORT" "$LISTENER_PORT" conf.xml &&
-apply_parameter "SLAVE_PORT" "$SLAVE_PORT" addns.xml &&
+if [ -n "$HAVE_MYSQL" ]; then
+	ods_setup_conf conf.xml conf-mysql.xml
+fi &&
 
-# Have to reset up the conf, because we apply special parameters
-ods_setup_conf &&
 ods_reset_env &&
 
 ## Start master name server
-ods_ldns_testns $SLAVE_PORT ods.datafile &&
+ods_ldns_testns 15353 ods.datafile &&
 
 ## Start OpenDNSSEC
 log_this_timeout ods-control-start 30 ods-control start &&
@@ -35,7 +28,7 @@ syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
 ## Check signed zone file [when we decide on auditor tool]
 
 ## Stop
-log_this_timeout ods-control-stop 30 ods-control stop
+log_this_timeout ods-control-stop 30 ods-control stop &&
 syslog_waitfor 60 'ods-enforcerd: .*all done' &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' &&
 ods_ldns_testns_kill &&
