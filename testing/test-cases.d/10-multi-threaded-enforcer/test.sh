6052710aa006d5d5d700b43228d98c39df6ce44b
Jerry Lundström <jerry.lundstrom@iis.se>
2012-09-04 13:28:16 +0000

    Multi-threaded Enforcer test

---
+++ testing/test-cases.d/10-multi-threaded-enforcer/test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#
+# Configure and sign 4 zones with one repository using multi-threaded Enforcer
+# and 3 different policies, 1 with shared keys and 2 without.
+
+if [ -n "$HAVE_MYSQL" ]; then
+	ods_setup_conf conf.xml conf-mysql.xml
+fi &&
+
+ods_reset_env &&
+
+log_this_timeout ods-control-enforcer-start 60 ods-control enforcer start &&
+syslog_waitfor 60 'ods-enforcerd: .*Sleeping for' &&
+syslog_waitfor 60 'ods-enforcerd: .*\[worker\[4\]\]: started' &&
+
+log_this_timeout ods-control-signer-start 60 ods-control signer start &&
+syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
+
+syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
+syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods2' &&
+syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods3' &&
+syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods4' &&
+
+test -f "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
+test -f "$INSTALL_ROOT/var/opendnssec/signed/ods2" &&
+test -f "$INSTALL_ROOT/var/opendnssec/signed/ods3" &&
+test -f "$INSTALL_ROOT/var/opendnssec/signed/ods4" &&
+
+log_this_timeout ods-control-start 60 ods-control stop &&
+syslog_waitfor 60 'ods-enforcerd: .*all done' &&
+syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' &&
+return 0
+
+ods_kill
+return 1
