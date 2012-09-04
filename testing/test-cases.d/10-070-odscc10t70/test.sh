1b10d5fbbd283fad5b5260636694bae8e97fac85
Jerry Lundström <jerry.lundstrom@iis.se>
2012-09-04 22:23:59 +0000

    OPENDNSSEC-130: Revert the high jacked 10-070 and created a own test for pin daemon, also set it to off until we can get it working.

--- testing/test-cases.d/10-070-odscc10t70/test.sh
+++ testing/test-cases.d/10-070-odscc10t70/test.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env bash
 #
-# Test of the PIN storage feature, new in 1.4
 # Configure no PIN while PIN is needed, expect failure
-# Then login and start everything successfully
 
 ods_reset_env &&
 
 if [ -n "$HAVE_MYSQL" ]; then
-        ods_setup_conf conf.xml conf-mysql.xml
+	ods_setup_conf conf.xml conf-mysql-no-module.xml
+else
+	ods_setup_conf conf.xml conf-no-module.xml
 fi &&
 
-# Make sure the PIN is cleared from shared memory
-log_this clear-pin "ipcrm -M 0x0d50d5ec"
+! log_this ods-hsmutil-purge ods-hsmutil purge SoftHSM  &&
+log_grep ods-hsmutil-purge stderr 'Incorrect PIN for repository SoftHSM' &&
 
 ! log_this_timeout ods-control-enforcer-start 60 ods-control enforcer start &&
-syslog_waitfor 10 'ods-enforcerd: .*hsm_check_pin(): No PIN in shared memory. Please login with "ods-hsmutil login"' &&
+syslog_waitfor 10 'ods-enforcerd: .*Incorrect PIN for repository SoftHSM' &&
 
 ! log_this_timeout ods-control-signer-start 60 ods-control signer start &&
-syslog_waitfor 10 'ods-signerd: .*\[hsm\].*hsm_check_pin(): No PIN in shared memory. Please login with "ods-hsmutil login"' &&
-
-# Now login and expect succes
-#log_this ods-hsmutil-login ods-hsmutil login SoftHSM &&
+syslog_waitfor 10 'ods-signerd: .*\[hsm\].*Incorrect PIN for repository SoftHSM' &&
 
 ! pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null &&
 return 0
