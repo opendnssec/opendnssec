0454fe0593533954f964fa78bb18e8194727e2fa
Matthijs Mekking <matthijs@nlnetlabs.nl>
2012-09-05 15:34:28 +0000

    New test: test outgoing notify

---
+++ testing/test-cases.d/14-dns-output-notify/test.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+## Test basic Output DNS Adapter
+## It requires setting up a zone in OpenDNSSEC with Output DNS Adapter,
+## non-default zonelist.xml, non-default conf.xml, additional addns.xml.
+## It requires setting up a secondary name server (ldns-testns) to accept the notifies.
+## It requires drill or dig to fetch the axfr.
+## It requires a checker tool like wdiff or ldns-verify-zone to review
+## the result (possibly with an known good file).
+
+if [ -n "$HAVE_MYSQL" ]; then
+	ods_setup_conf conf.xml conf-mysql.xml
+fi &&
+
+ods_reset_env &&
+
+## Start master name server
+ods_ldns_testns 15353 ods.datafile &&
+
+## Start OpenDNSSEC
+log_this_timeout ods-control-start 60 ods-control start &&
+syslog_waitfor 60 'ods-enforcerd: .*Sleeping for' &&
+syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
+
+## Wait for signed zone file
+syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
+## Check if NOTIFY is send and accepted
+log_waitfor ldns-testns stdout 5 'comparepkt: match!'
+
+## Check signed zone file [when we decide on auditor tool]
+
+## Stop
+log_this_timeout ods-control-stop 60 ods-control stop &&
+syslog_waitfor 60 'ods-enforcerd: .*all done' &&
+syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' &&
+ods_ldns_testns_kill &&
+return 0
+
+## Test failed. Kill stuff
+ods_ldns_testns_kill
+ods_kill
+return 1
