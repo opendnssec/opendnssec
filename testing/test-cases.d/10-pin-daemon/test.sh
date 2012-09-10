dbecdfaa1c492e476a1ef659c2c1fa0b643ea0be
Sara Dickinson <sara@sinodun.com>
2012-09-10 10:34:15 +0000

    OPENDNSSEC-130: Updated script that can now login with a PIN. Works on local CentOS machine. Will turn test on later to test in jenkins.

--- testing/test-cases.d/10-pin-daemon/test.sh
+++ testing/test-cases.d/10-pin-daemon/test.sh
@@ -19,10 +19,25 @@ syslog_waitfor 10 'ods-enforcerd: .*hsm_check_pin(): No PIN in shared memory. Pl
 ! log_this_timeout ods-control-signer-start 60 ods-control signer start &&
 syslog_waitfor 10 'ods-signerd: .*\[hsm\].*hsm_check_pin(): No PIN in shared memory. Please login with "ods-hsmutil login"' &&
 
-# Now login and expect succes
-#log_this ods-hsmutil-login ods-hsmutil login SoftHSM &&
-
 ! pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null &&
+
+# Now login and expect succes
+
+# Problems using a pipe in the log_this command so doing it directly for now...
+log_this ods-hsmutil-login echo "Logging in with PIN...." &&
+echo "1234" | ods-hsmutil login && 
+log_this ods-hsmutil-login echo "Successfully logged in" &&
+#log_this ods-hsmutil-login 'echo "1234" | ods-hsmutil login'  &&
+#log_grep ods-hsmutil-login stdout "Enter PIN for token SoftHSM:" && 
+#log_grep ods-hsmutil-login stdout "The tokens are now logged in." &&
+
+log_this_timeout ods-control-start 60 ods-control start &&
+syslog_waitfor 60 'ods-enforcerd: .*Sleeping for' &&
+syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
+
+log_this_timeout ods-control-stop 60 ods-control stop &&
+syslog_waitfor 60 'ods-enforcerd: .*all done' &&
+syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' && 
 return 0
 
 ods_kill
