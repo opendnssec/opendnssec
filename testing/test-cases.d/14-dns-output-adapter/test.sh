9fb57e6d3697abdaff707718c190e175c2f677c4
Matthijs Mekking <matthijs@nlnetlabs.nl>
2012-09-12 14:04:23 +0000

    DNS output adapter also tests: - ixfr overflow udp - fallback to axfr if no ixfr available
    and update kasp

--- testing/test-cases.d/14-dns-output-adapter/test.sh
+++ testing/test-cases.d/14-dns-output-adapter/test.sh
@@ -23,7 +23,24 @@ syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
 ## Retry NOTIFY
 syslog_waitfor 120 'ods-signerd: .*\[notify\] notify max retry for zone ods, 127\.0\.0\.1 unreachable' &&
 
-## Check signed zone file [when we decide on auditor tool]
+## See if we can transfer the signed zone
+log_this_timeout axfr 10 drill -p 15354 @127.0.0.1 axfr ods &&
+log_waitfor axfr stdout 5 'ods\..*IN.*AXFR' &&
+log_waitfor axfr stdout 5 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
+log_waitfor axfr stdout 5 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
+## See if we send overflow UDP if does not fit.
+log_this_timeout ixfr 10 drill -p 15354 @127.0.0.1 ixfr ods &&
+syslog_waitfor 10 'ods-signerd: .*\[axfr\] axfr fallback zone ods' &&
+syslog_waitfor 10 'ods-signerd: .*\[axfr\] axfr udp overflow zone ods' &&
+log_waitfor ixfr stdout 5 'ods\..*IN.*IXFR' &&
+log_waitfor ixfr stdout 5 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
+! (log_waitfor ixfr stdout 5 'ods\..*600.*IN.*MX.*10.*mail\.ods\.') &&
+
+## See if we fallback to AXFR if IXFR not available.
+log_this_timeout ixfr-tcp 10 drill -t -p 15354 @127.0.0.1 ixfr ods &&
+log_waitfor ixfr-tcp stdout 5 'ods\..*IN.*IXFR' &&
+log_waitfor ixfr-tcp stdout 5 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
+log_waitfor ixfr-tcp stdout 5 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
 
 ## Stop
 log_this_timeout ods-control-stop 60 ods-control stop &&
