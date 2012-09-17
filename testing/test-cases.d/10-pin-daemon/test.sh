507591b1420741ee0da96b8fdb72b87bd2917f3f
Jerry Lundström <jerry.lundstrom@iis.se>
2012-09-17 12:07:08 +0000

    Disabling pin daemon test. Cleanup the test, clear with both hex and int value of key since it differs on platforms.

--- testing/test-cases.d/10-pin-daemon/test.sh
+++ testing/test-cases.d/10-pin-daemon/test.sh
@@ -4,19 +4,19 @@
 # Configure no PIN while PIN is needed, expect failure
 # Then login and start everything successfully
 
+# Make sure the PIN is cleared from shared memory
+# try key as hex and int, ignore results
+log_this clear-pin-hex ipcrm -M 0x0d50d5ec
+log_this clear-pin-int ipcrm -M 223401452
+
 if [ -n "$HAVE_MYSQL" ]; then
         ods_setup_conf conf.xml conf-mysql.xml
 fi &&
 
 ods_reset_env &&
 
-# Make sure the PIN is cleared from shared memory
-log_this clear-pin "ipcrm -M 0x0d50d5ec" &&
-
 # Make sure the login fails 
-log_this ods-hsmutil-login echo "Logging in wrong PIN...." &&
-! echo "123" | ods-hsmutil login &&
-log_this ods-hsmutil-login echo "Login failed." &&
+! echo "123" | log_this ods-hsmutil-login-fail ods-hsmutil login &&
 
 ! log_this_timeout ods-control-enforcer-start 60 ods-control enforcer start &&
 syslog_waitfor 10 'ods-enforcerd: .*hsm_check_pin(): No PIN in shared memory. Please login with "ods-hsmutil login"' &&
@@ -27,14 +27,7 @@ syslog_waitfor 10 'ods-signerd: .*\[hsm\].*hsm_check_pin(): No PIN in shared mem
 ! pgrep -u `id -u` '(ods-enforcerd|ods-signerd)' >/dev/null 2>/dev/null &&
 
 # Now login and expect success
-
-# Problems using a pipe in the log_this command so doing it directly for now...
-log_this ods-hsmutil-login echo "Logging in with PIN...." &&
-echo "1234" | ods-hsmutil login && 
-log_this ods-hsmutil-login echo "Successfully logged in" &&
-#log_this ods-hsmutil-login 'echo "1234" | ods-hsmutil login'  &&
-#log_grep ods-hsmutil-login stdout "Enter PIN for token SoftHSM:" && 
-#log_grep ods-hsmutil-login stdout "The tokens are now logged in." &&
+echo "1234" | log_this ods-hsmutil-login ods-hsmutil login && 
 
 log_this_timeout ods-control-start 60 ods-control start &&
 syslog_waitfor 60 'ods-enforcerd: .*Sleeping for' &&
