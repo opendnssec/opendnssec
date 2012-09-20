39f4513aec0169374bdfb914d7e5fb80748594d0
Matthijs Mekking <matthijs@nlnetlabs.nl>
2012-09-20 08:37:57 +0000

    use log_grep instead of log_waitfor

--- testing/test-cases.d/14-dns-output-adapter/test.sh
+++ testing/test-cases.d/14-dns-output-adapter/test.sh
@@ -25,21 +25,21 @@ syslog_waitfor 120 'ods-signerd: .*\[notify\] notify max retry for zone ods, 127
 
 ## See if we can transfer the signed zone
 log_this_timeout axfr 10 drill -p 15354 @127.0.0.1 axfr ods &&
-log_waitfor axfr stdout 5 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
-log_waitfor axfr stdout 5 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
+log_grep axfr stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
+log_grep axfr stdout 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
 ## See if we send overflow UDP if does not fit.
 log_this_timeout ixfr 10 drill -p 15354 @127.0.0.1 ixfr ods &&
 syslog_waitfor 10 'ods-signerd: .*\[axfr\] axfr fallback zone ods' &&
 syslog_waitfor 10 'ods-signerd: .*\[axfr\] axfr udp overflow zone ods' &&
-log_waitfor ixfr stdout 5 'ods\..*IN.*IXFR' &&
-log_waitfor ixfr stdout 5 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
-! (log_waitfor ixfr stdout 5 'ods\..*600.*IN.*MX.*10.*mail\.ods\.') &&
+log_grep ixfr stdout 'ods\..*IN.*IXFR' &&
+log_grep ixfr stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
+! (log_grep ixfr stdout 'ods\..*600.*IN.*MX.*10.*mail\.ods\.') &&
 
 ## See if we fallback to AXFR if IXFR not available.
 
 log_this_timeout ixfr-tcp 10 dig +tcp -t ixfr=0 -p 15354 @127.0.0.1 ods &&
-log_waitfor ixfr-tcp stdout 5 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
-log_waitfor ixfr-tcp stdout 5 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
+log_grep ixfr-tcp stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
+log_grep ixfr-tcp stdout 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
 
 ## Stop
 log_this_timeout ods-control-stop 60 ods-control stop &&
--- testing/test-cases.d/14-dns-retry-expire/test.sh
+++ testing/test-cases.d/14-dns-retry-expire/test.sh
@@ -35,7 +35,7 @@ ods_ldns_testns_kill &&
 
 ## See if we can transfer the signed zone
 log_this_timeout drill 10 drill -p 15354 @127.0.0.1 axfr ods &&
-log_waitfor drill stdout 5 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*30.*5.*31.*3600' &&
+log_grep drill stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*30.*5.*31.*3600' &&
 
 ## See if SOA RETRY is being done
 syslog_waitfor 35 'ods-signerd: .*\[xfrd\] zone ods make request round 0 master' &&
@@ -46,7 +46,7 @@ syslog_waitfor 5 'ods-signerd: .*\[xfrd] zone ods sets timer timeout retry 5' &&
 ## See if it stops serving zone transfer after the SOA EXPIRE interval
 sleep 35 &&
 log_this_timeout drill 10 drill -p 15354 @127.0.0.1 axfr ods &&
-log_waitfor drill stderr 5 'Error in AXFR: SERVFAIL' &&
+log_grep drill stderr 'Error in AXFR: SERVFAIL' &&
 
 ## Stop
 log_this_timeout ods-control-stop 60 ods-control stop &&
