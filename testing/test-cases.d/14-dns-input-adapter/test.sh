632acd143e41a7e950aeaeb60ed94be72a95b0c8
Matthijs Mekking <matthijs@nlnetlabs.nl>
2012-10-30 10:28:46 +0000

    add ixfr to dns input adapter test, including tcp fallback

--- testing/test-cases.d/14-dns-input-adapter/test.sh
+++ testing/test-cases.d/14-dns-input-adapter/test.sh
@@ -23,6 +23,19 @@ syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
 ## Check signed zone file [when we decide on auditor tool]
 test -f "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
 
+ods-signer verbosity 5 &&
+
+## Fake notify
+ldns-notify -p 15354 -s 1001 -r 2 -z ods 127.0.0.1 &&
+
+## Request IXFR/UDP
+syslog_waitfor 10 'ods-signerd: .*\[xfrd\] zone ods sending udp query id=.* qtype=IXFR to 127.0.0.1' &&
+syslog_waitfor 10 'ods-signerd: .*\[xfrd\] zone ods received too short udp reply from 127.0.0.1, retry tcp' &&
+
+## Request IXFR/TCP
+syslog_waitfor 10 'ods-signerd: .*\[xfrd\] zone ods request ixfr to 127.0.0.1' &&
+syslog_waitfor 10 'ods-signerd: .*\[xfrd\] reschedule task for zone ods: disk serial=1001 acquired=.*, memory serial=1000 acquired=.*' &&
+
 ## Stop
 log_this_timeout ods-control-stop 60 ods-control stop &&
 syslog_waitfor 60 'ods-enforcerd: .*all done' &&
