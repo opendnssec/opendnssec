53905e1013f5cb0273a0b565edc4267e6e750da1
Sara Dickinson <sara@sinodun.com>
2012-11-16 17:59:52 +0000

    Switch the ldns version used in the trunk jenkins tests to 1.6.16 to ensure no problems are seen.

--- testing/build-ldns.sh
+++ testing/build-ldns.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 source `dirname "$0"`/lib.sh && init || exit 1
 
-LDNS="ldns-1.6.13"
+LDNS="ldns-1.6.16"
 LDNS_URL="http://nlnetlabs.nl/downloads/ldns/$LDNS.tar.gz"
 LDNS_FILENAME="$LDNS.tar.gz"
 LDNS_HASH_TYPE="sha1"
-LDNS_HASH="859f633d10b763f06b602e2113828cbbd964c7eb"
+LDNS_HASH="5b4fc6c5c3078cd061905c47178478cb1015c62a"
 
 check_if_built ldns && exit 0
 start_build ldns
