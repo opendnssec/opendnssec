504e32ae818e7f478fd0428698232f70d492d624
Sara Dickinson <sara@sinodun.com>
2012-11-19 16:55:43 +0000

    Add coverage for a number of signer bugs into the 10-validate-test-zones test.

--- testing/test-cases.d/10-validate-test-zones/test.sh
+++ testing/test-cases.d/10-validate-test-zones/test.sh
@@ -14,6 +14,9 @@ ods_reset_env &&
 log_this_timeout ods-control-enforcer-start 60 ods-control enforcer start &&
 syslog_waitfor 60 'ods-enforcerd: .*Sleeping for' &&
 
+#########################################################################
+# Basic checks of signing test zones
+
 log_this_timeout ods-control-signer-start 60 ods-control signer start &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
 
@@ -26,18 +29,53 @@ test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
 syslog_waitfor 60 'ods-signerd: .*\[STATS\] all.rr.binary.org' &&
 test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.binary.org" &&
 
+# OPENDNSSEC-231: Make sure we can support reverse classless zones
+syslog_waitfor 60 'ods-signerd: .*\[STATS\] 64/1.0.168.192.IN-ADDR.ARPA' &&
+test -f "$INSTALL_ROOT/var/opendnssec/signed/64-1.0.168.192.in-addr.arpa" &&
+
 # Validate the output when we have a validation tool.....
-# In the mean time:
+
+#########################################################################
+# Tests to cover signing specific bugs
 
 # SUPPORT-40. Double check that all records down to the forth level appear in the output
 $GREP -q -- "^test.example.com..*86400.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
 $GREP -q -- "^test1.test.example.com..*86400.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
 
+# OPENDSNSEC-290. Update the zone by changing a CNAME record to an A record. 
+ods_setup_zone test/all.rr.org &&
+log_this_timeout ods-update-zone 10 ods-signer sign all.rr.org &&
+
+syslog_waitfor_count 60 2 'ods-signerd: .*\[STATS\] all.rr.org' &&
+test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
+
+# OPENDNSSEC-247. Update the SOA minimum in the policy and make sure the 
+# NSEC TTL changes.
+$GREP -q -- "<Minimum>PT300S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
+$GREP -q -- "300.*IN.*NSEC3" "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
+mv kasp.xml kasp.xml_orig &&
+cp test/kasp.xml kasp.xml &&
+log_this ods-update-policy ods_setup_conf kasp.xml &&
+log_this_timeout ods-update-policy 10 ods-ksmutil update kasp &&
+syslog_waitfor 60 'ods-enforcerd: .*Called signer engine:.*ods-signer update all.rr.org' &&
+$GREP -q -- "<Minimum>PT600S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
+syslog_waitfor 60 'ods-signerd: .*zone all.rr.org set soa ttl to 600' &&
+
+syslog_waitfor_count 60 3 'ods-signerd: .*\[STATS\] all.rr.org' &&
+test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
+# SAD -> MM: This check fails...
+#$GREP -q -- "600.*IN.*NSEC3" "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
+
+#########################################################################
+
 log_this_timeout ods-control-start 60 ods-control stop &&
 syslog_waitfor 60 'ods-enforcerd: .*all done' &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' &&
+
+mv kasp.xml_orig kasp.xml &&
 return 0
 
 ods_kill
+mv kasp.xml_orig kasp.xml &&
 return 1
 
