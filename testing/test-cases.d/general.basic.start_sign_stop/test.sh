de11f911c300449f626ca1e82347f858f3b95a37
Sara Dickinson <sara@sinodun.com>
2013-04-18 09:43:17 +0000

    TEST: port validns usage to 1.3 branch

--- testing/test-cases.d/general.basic.start_sign_stop/test.sh
+++ testing/test-cases.d/general.basic.start_sign_stop/test.sh
@@ -2,6 +2,12 @@
 
 #TEST: Start, sign a single zone with one repository, stop. 
 
+#case "$DISTRIBUTION" in
+#        redhat )
+                append_path /usr/sbin
+#                ;;
+#esac
+
 if [ -n "$HAVE_MYSQL" ]; then
 	ods_setup_conf conf.xml conf-mysql.xml
 fi &&
@@ -16,6 +22,14 @@ syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
 
 syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
 test -f "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
+
+# Validate the output on redhat
+#case "$DISTRIBUTION" in
+#        redhat )
+                log_this validate-zone-ods validns -s -p all "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
+                log_grep validate-zone-ods stdout 'validation errors:   0'
+#                ;;
+#esac &&
 
 log_this_timeout ods-control-start 60 ods-control stop &&
 syslog_waitfor 60 'ods-enforcerd: .*all done' &&
--- testing/test-cases.d/signer.zones.validate_many_zones/test.sh
+++ testing/test-cases.d/signer.zones.validate_many_zones/test.sh
@@ -5,6 +5,14 @@
 #TEST: Will eventually add validation into this to check the output
 #TEST: For now use it to check any signing bugs with explicit tests
 
+# So we can use validns 0.7 it is installed from source so need to
+# specify this path
+#case "$DISTRIBUTION" in
+#        redhat )
+                append_path /usr/sbin
+#                ;;
+#esac
+
 
 if [ -n "$HAVE_MYSQL" ]; then
 	ods_setup_conf conf.xml conf-mysql.xml
@@ -18,7 +26,7 @@ syslog_waitfor 60 'ods-enforcerd: .*Sleeping for' &&
 #########################################################################
 # Basic checks of signing test zones
 
-log_this_timeout ods-control-signer-start 60 ods-control signer start &&
+log_this_timeout ods-control-signer-start 60 ods-signerd -1 &&
 syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
 
 syslog_waitfor 60 'ods-signerd: .*\[STATS\] example.com' &&
@@ -34,21 +42,21 @@ test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.binary.org" &&
 syslog_waitfor 60 'ods-signerd: .*\[STATS\] 64/1.0.168.192.IN-ADDR.ARPA' &&
 test -f "$INSTALL_ROOT/var/opendnssec/signed/64-1.0.168.192.in-addr.arpa" &&
 
-# Validate the output when we have a validation tool.....
+# Validate the output on redhat
+#case "$DISTRIBUTION" in
+#        redhat )
+                # can't use -p all as the zone has only 1 NS per name
+                log_this validate-zone-ods validns -s -p cname-other-data -p dname -p dnskey -p nsec3param-not-apex -p mx-alias -p ns-alias -p rp-txt-exists -p tlsa-host "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
+                log_grep validate-zone-ods stdout 'validation errors:   0'
+                log_this validate-zone-all.rr.org validns -s -p all "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
+                log_grep validate-zone-all.rr.org stdout 'validation errors:   0' &&
+                # The other two zone types don't seem to be supported by validns
+#                ;;
+#esac &&
+
 
 #########################################################################
 # Tests to cover signing specific bugs
-
-#SUPPORT-40 - Double check that all records down to the forth level appear in the output
-$GREP -q -- "^test.example.com..*86400.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
-$GREP -q -- "^test1.test.example.com..*86400.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
-
-#OPENDSNSEC-290 - Update the zone by changing a CNAME record to an A record. 
-ods_setup_zone test/all.rr.org &&
-log_this_timeout ods-update-zone 10 ods-signer sign all.rr.org &&
-
-syslog_waitfor_count 60 2 'ods-signerd: .*\[STATS\] all.rr.org' &&
-test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
 
 #OPENDNSSEC-247 - Update the SOA minimum in the policy and make sure the NSEC TTL changes.
 $GREP -q -- "<Minimum>PT300S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
@@ -57,12 +65,16 @@ mv kasp.xml kasp.xml_orig &&
 cp test/kasp.xml kasp.xml &&
 log_this ods-update-policy ods_setup_conf kasp.xml &&
 log_this_timeout ods-update-policy 10 ods-ksmutil update kasp &&
-syslog_waitfor 60 'ods-enforcerd: .*Called signer engine:.*ods-signer update all.rr.org' &&
+syslog_waitfor_count 60 2 'ods-enforcerd: .*Sleeping for' &&
 $GREP -q -- "<Minimum>PT600S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
+
+log_this_timeout ods-control-signer-start 60 ods-signerd  &&
+syslog_waitfor 60 'ods-signerd: .*\[engine\] signer started' &&
 # SAD -> MM: This is only logged when using SQLite...
 #syslog_waitfor 60 'ods-signerd: .*zone all.rr.org set soa ttl to 600' &&
 
-syslog_waitfor_count 60 3 'ods-signerd: .*\[STATS\] all.rr.org' &&
+log_this ods-resign-diff_ttl ods-signer sign all.rr.org &&
+syslog_waitfor_count 60 2 'ods-signerd: .*\[STATS\] all.rr.org' &&
 test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
 # SAD -> MM: This check fails...
 #$GREP -q -- "600.*IN.*NSEC3" "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
@@ -76,7 +88,9 @@ syslog_waitfor 60 'ods-signerd: .*\[engine\] signer shutdown' &&
 mv kasp.xml_orig kasp.xml &&
 return 0
 
+echo '*********** ERROR **********'
 ods_kill
-mv kasp.xml_orig kasp.xml &&
+mv kasp.xml_orig kasp.xml
 return 1
+
 
