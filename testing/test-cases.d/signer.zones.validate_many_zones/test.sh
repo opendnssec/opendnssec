96ffb44d4e74f49b3462aea3a4699b9a1e2c7c5d
Sara Dickinson <sara@sinodun.com>
2013-04-18 13:06:45 +0000

    TEST: port validns usage - fix rogue && in test script

--- testing/test-cases.d/signer.zones.validate_many_zones/test.sh
+++ testing/test-cases.d/signer.zones.validate_many_zones/test.sh
@@ -47,9 +47,9 @@ case "$DISTRIBUTION" in
         redhat )
                 # can't use -p all as the zone has only 1 NS per name
                 log_this validate-zone-ods validns -s -p cname-other-data -p dname -p dnskey -p nsec3param-not-apex -p mx-alias -p ns-alias -p rp-txt-exists -p tlsa-host "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
-                log_grep validate-zone-ods stdout 'validation errors:   0'
+                log_grep validate-zone-ods stdout 'validation errors:   0' &&
                 log_this validate-zone-all.rr.org validns -s -p all "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
-                log_grep validate-zone-all.rr.org stdout 'validation errors:   0' &&
+                log_grep validate-zone-all.rr.org stdout 'validation errors:   0' 
                 # The other two zone types don't seem to be supported by validns
                 ;;
 esac &&
