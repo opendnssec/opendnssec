57cfcb01ccf5efb9bd10801db343478b6d202b33
Sara Dickinson <sara@sinodun.com>
2013-07-23 14:33:12 +0000

    OPENDNSSEC-428: Add --zonecount as an optional parameter to 'ods-ksmutil key generate' TEST: add enforcer.keys.generate test to cover this

--- enforcer/utils/ksmutil.c
+++ enforcer/utils/ksmutil.c
@@ -113,6 +113,7 @@ char *o_keytype = NULL;
 char *o_time = NULL;
 char *o_retire = NULL;
 char *o_zone = NULL;
+char *o_zonecount = NULL;
 char *o_keytag = NULL;
 static int all_flag = 0;
 static int auto_accept_flag = 0;
@@ -334,9 +335,10 @@ usage_keygen ()
 {
     fprintf(stderr,
             "  key generate\n"
-		    "\t--policy <policy>\n"
-            "\t--interval <interval>\n"
-            "\t--auto-accept\n");
+		    "\t--policy <policy>                        aka -p\n"
+            "\t--interval <interval>                    aka -n\n"
+            "\t[--zonecount <zone count>]               aka -Z\n"
+            "\t--auto-accept                            aka -A\n");
 }
 
     void
@@ -3716,12 +3718,13 @@ main (int argc, char *argv[])
         {"keytag",  required_argument, 0, 'x'},
         {"retire",  required_argument, 0, 'y'},
         {"zone",    required_argument, 0, 'z'},
+		{"zonecount", required_argument, 0, 'Z'},
         {0,0,0,0}
     };
 
     progname = argv[0];
 
-    while ((ch = getopt_long(argc, argv, "aAb:c:de:fFg:hi:j:k:mMn:o:p:q:r:s:t:vVw:x:y:z:", long_options, &option_index)) != -1) {
+    while ((ch = getopt_long(argc, argv, "aAb:c:de:fFg:hi:j:k:mMn:o:p:q:r:s:t:vVw:x:y:z:Z:", long_options, &option_index)) != -1) {
         switch (ch) {
             case 'a':
                 all_flag = 1;
@@ -3818,6 +3821,9 @@ main (int argc, char *argv[])
 				}
 
                 break;
+			case 'Z':
+				o_zonecount = StrStrdup(optarg);
+				break;
             default:
                 usage();
                 exit(1);
@@ -6967,28 +6973,58 @@ int cmd_genkeys()
     } 
     DbFreeResult(result); 
 
-    if (status == 0) { 
+    if (status != 0) {
+        printf("Could not count zones on policy %s\n", policy->name);
+        db_disconnect(lock_fd);
+		if (ctx) {
+			hsm_destroy_context(ctx);
+		}
+		hsm_close();
+		KsmPolicyFree(policy);
+        return status; 
+    }
+    printf("Info: %d zone(s) found on policy \"%s\"\n", zone_count, policy->name);
+
+	/* If the zone count has been specified manually then use this 
+	instead but report how it differs from the actual number of zones*/
+    if (o_zonecount) {
+	  /* Check the value is numeric*/
+      if (StrIsDigits(o_zonecount)) {
+        status = StrStrtoi(o_zonecount, &zone_count);
+        if (status != 0) {
+            printf("Error: Unable to convert zonecount \"%s\"; to an integer\n", o_zonecount);
+            db_disconnect(lock_fd);
+            KsmPolicyFree(policy);
+            exit(1);
+        }
+      } else {
+          printf("Error: zonecount \"%s\"; should be numeric only\n", o_zonecount);
+          db_disconnect(lock_fd);
+          KsmPolicyFree(policy);
+          exit(1);
+      }
+      /* Check the value is greater than 0*/
+      if (zone_count < 1) { 
+          printf("Error: zonecount parameter value of %d is invalid - the value must be greater than 0\n", zone_count);
+	      db_disconnect(lock_fd);
+          KsmPolicyFree(policy);
+          exit(1); 
+      }
+	  printf("Info: Keys will actually be generated for %d zone(s) as specified by zone count parameter\n", zone_count);
+	}
+	else {
         /* make sure that we have at least one zone */ 
         if (zone_count == 0) { 
-            printf("No zones on policy %s, skipping...", policy->name);
-	    db_disconnect(lock_fd);
-	    if (ctx) {
-		    hsm_destroy_context(ctx);
-	    }
-	    hsm_close();
+            printf("No zones on policy %s, skipping...\n", policy->name);
+	    	db_disconnect(lock_fd);
+		    if (ctx) {
+			    hsm_destroy_context(ctx);
+		    }
+		    hsm_close();
             KsmPolicyFree(policy);
             return status; 
         } 
-    } else {
-        printf("Could not count zones on policy %s", policy->name);
-        db_disconnect(lock_fd);
-	if (ctx) {
-		hsm_destroy_context(ctx);
 	}
-	hsm_close();
-	KsmPolicyFree(policy);
-        return status; 
-    }
 
     /* Find out how many ksk keys are needed for the POLICY */
     status = KsmKeyPredict(policy->id, KSM_TYPE_KSK, policy->shared_keys, interval, &ksks_needed, policy->ksk->rollover_scheme, zone_count);
---
+++ testing/test-cases-daily.d/enforcer.keys.generate/test.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+#
+#TEST: Test to check the operation of the 'ods-ksmutil key generate' command
+#TEST: including the new -zonecount parameter
+
+if [ -n "$HAVE_MYSQL" ]; then
+        ods_setup_conf conf.xml conf-mysql.xml
+fi &&
+
+ods_reset_env &&
+
+##################  Default behaviour ###########################
+# Fail with no zones
+log_this ods-ksmutil-no_zones_policy   ods-ksmutil key generate --interval PT40M --policy  Policy1 &&
+log_grep ods-ksmutil-no_zones_policy   stdout "Info: 0 zone(s) found on policy \"Policy1\"" &&
+log_grep ods-ksmutil-no_zones_policy   stdout "No zones on policy Policy1, skipping..." &&
+
+# Add a zone
+log_this ods-ksmutil-setup_zone_and_keys   ods-ksmutil zone add --zone ods --input $INSTALL_ROOT/var/opendnssec/unsigned/ods.xml --policy Policy1 --signerconf $INSTALL_ROOT/var/opendnssec/signconf/ods.xml &&
+log_grep ods-ksmutil-setup_zone_and_keys   stdout "Imported zone: ods" &&
+log_this ods-ksmutil-setup_zone_and_keys   ods-ksmutil zone list &&
+log_grep ods-ksmutil-setup_zone_and_keys   stdout "Found Zone: ods; on policy Policy1" &&
+
+# Generate keys
+echo "y" | log_this ods-ksmutil-setup_zone_and_keys   ods-ksmutil key generate --interval PT4M --policy  Policy1 &&
+log_grep ods-ksmutil-setup_zone_and_keys   stdout "Info: 1 zone(s) found on policy \"Policy1\"" &&
+log_grep ods-ksmutil-setup_zone_and_keys   stdout "This will create 2 KSKs (2048 bits) and 5 ZSKs (2048 bits)" &&
+log_this ods-ksmutil-keylist1   ods-hsmutil list &&
+log_grep ods-ksmutil-keylist1 stdout "7 keys found." && 
+
+####
+
+##################  Using zone count ###########################
+# Fail with 0 zone count
+! log_this ods-ksmutil-zonecount_fail   ods-ksmutil key generate --interval PT40M --policy  Policy2 --zonecount 0 &&
+log_grep ods-ksmutil-zonecount_fail   stdout "Error: zonecount parameter value of 0 is invalid - the value must be greater than 0" &&
+# Fail with non-numeric zone count
+! log_this ods-ksmutil-zonecount_fail   ods-ksmutil key generate --interval PT40M --policy  Policy2 --zonecount bob &&
+log_grep ods-ksmutil-zonecount_fail   stdout "Error: zonecount \"bob\"; should be numeric only" &&
+
+# Generate keys
+echo "y" | log_this ods-ksmutil-zonecount   ods-ksmutil key generate --interval PT40M --policy Policy2 --zonecount 3 &&
+log_grep ods-ksmutil-zonecount   stdout "Info: 0 zone(s) found on policy \"Policy2\"" &&
+log_grep ods-ksmutil-zonecount   stdout "Info: Keys will actually be generated for 3 zone(s) as specified by zone count parameter" &&
+log_grep ods-ksmutil-zonecount   stdout "This will create 9 KSKs (2048 bits) and 18 ZSKs (2048 bits)" &&
+log_this ods-ksmutil-keylist2   ods-hsmutil list &&
+log_grep ods-ksmutil-keylist2 stdout "34 keys found." && 
+
+return 0
+
+echo
+echo "************ERROR******************"
+echo
+ods_kill
+return 1
+
