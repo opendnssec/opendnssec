b7cf86d0ca1353f4333a87f4d43d9d9fbee733f9
Sara Dickinson <sara@sinodun.com>
2013-09-14 14:43:54 +0000

    TEST: enforcer-ng tests. More timing tweaks to keep openbsd happy

--- testing/test-cases.d/general.basic.start_stop/test.sh
+++ testing/test-cases.d/general.basic.start_stop/test.sh
@@ -2,7 +2,7 @@
 
 #TEST: Start and stop using ods-control and default conf files. Check the deamons behave.
 
-ods_reset_env &&
+ods_reset_env 5 &&
 
 ods_start_ods-control &&
 ods_stop_ods-control &&
