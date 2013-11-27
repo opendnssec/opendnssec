a9f9f36cece4eb6388269ac5aed5741b38060169
Matthijs Mekking <matthijs@nlnetlabs.nl>
2013-11-27 10:49:47 +0000

    fix memleak in libhsm

--- libhsm/src/lib/libhsm.c
+++ libhsm/src/lib/libhsm.c
@@ -2194,6 +2194,9 @@ hsm_sign_params_new()
 {
     hsm_sign_params_t *params;
     params = malloc(sizeof(hsm_sign_params_t));
+    if (!params) {
+        return NULL;
+    }
     params->algorithm = LDNS_SIGN_RSASHA256;
     params->flags = LDNS_KEY_ZONE_KEY;
     params->inception = 0;
@@ -2979,6 +2982,7 @@ hsm_get_dnskey(hsm_ctx_t *ctx,
 
     rdata = hsm_get_key_rdata(ctx, session, key);
     if (rdata == NULL) {
+        ldns_rr_free(dnskey);
         return NULL;
     }
     ldns_rr_push_rdf(dnskey, rdata);
