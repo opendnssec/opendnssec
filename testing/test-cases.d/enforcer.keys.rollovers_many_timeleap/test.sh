170849c749d91960c087ca985bf1fcd91bf7a5ab
Sara Dickinson <sara@sinodun.com>
2014-02-11 17:14:10 +0000

    TEST: Extend the test that uses time leap to help Yuri debug problems.

--- testing/test-cases.d/enforcer.keys.rollovers_many_timeleap/test.sh
+++ testing/test-cases.d/enforcer.keys.rollovers_many_timeleap/test.sh
@@ -45,12 +45,12 @@ ods_reset_env 20 &&
 ##################  SETUP TIME 0 ###########################
 log_this ods-enforcer-output date &&
 ods_start_enforcer &&
-log_this ods-enforcer-output echo "----- Expect generate/publish" &&
+log_this ods-enforcer-output echo "------- Expect generate/publish" &&
 log_this ods-enforcer-output ods-enforcer key list --verbose &&
 log_this ods-enforcer-output ods-enforcer rollover list &&
 
 #### TIME 1: Keys are Published/Ready
-log_this ods-enforcer-output echo "--------------------------------------------" &&
+log_this ods-enforcer-output echo "--------------- TIME LEAP 1 -----------------" &&
 log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
 log_this ods-enforcer-output echo "--------------------------------------------" &&
 
@@ -59,7 +59,7 @@ log_this ods-enforcer-output ods-enforcer key list  --verbose &&
 log_this ods-enforcer-output ods-enforcer rollover list &&
 
 #### TIME 2: Keys are Ready/Active -> do ds-submit and ds-seen
-log_this ods-enforcer-output echo "--------------------------------------------" &&
+log_this ods-enforcer-output echo "--------------- TIME LEAP 2 -----------------" &&
 log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
 log_this ods-enforcer-output echo "--------------------------------------------" &&
 
@@ -79,30 +79,25 @@ log_this ods-enforcer-output ods-enforcer rollover list &&
 log_this ods-enforcer-output echo "----- Do ds-seen" &&
 KSK_CKA_ID1=`log_grep -o ods-enforcer-output stdout "ods1[[:space:]].*KSK[[:space:]].*ready     waiting for ds-submit" | awk '{print $9}'` &&
 log_this ods-enforcer-output ods-enforcer key ds-seen --zone ods1 --cka_id $KSK_CKA_ID1 && sleep 1 &&
+log_this ods-enforcer-output ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output ods-enforcer key list --debug &&
+log_this ods-enforcer-output ods-enforcer rollover list &&
 
-log_this ods-enforcer-output echo "----- Expect active/active [Doesn't happen]" &&
+#### TIME 2.5: Keys are both active after DS TTL passed since ds-seen
+log_this ods-enforcer-output echo "----- Wait for DS TTL to pass  " &&
+log_this ods-enforcer-output echo "--------------- TIME LEAP 2.5 -----------------" &&
+log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output echo "--------------------------------------------" &&
+
+log_this ods-enforcer-output echo "----- Expect active/active " &&
 log_this ods-enforcer-output ods-enforcer key list  --verbose &&
 log_this ods-enforcer-output ods-enforcer key list --debug &&
 log_this ods-enforcer-output ods-enforcer rollover list &&
  
-log_this ods-enforcer-output echo "----- Sleep then enforce, Expect active/active [Still doesn't happen]" &&
-sleep 2 && log_this ods-enforcer-output ods-enforcer enforce && sleep 2 &&
-log_this ods-enforcer-output ods-enforcer key list  --verbose &&
-log_this ods-enforcer-output ods-enforcer key list --debug &&
-log_this ods-enforcer-output ods-enforcer rollover list &&
 
 #### TIME 3: Keys are finally both active
-log_this ods-enforcer-output echo "--------------------------------------------" &&
-log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
-log_this ods-enforcer-output echo "--------------------------------------------" &&
-
-log_this ods-enforcer-output echo "----- Expect active/active" &&
-log_this ods-enforcer-output ods-enforcer key list  --verbose &&
-log_this ods-enforcer-output ods-enforcer key list --debug &&
-log_this ods-enforcer-output ods-enforcer rollover list &&
-
-#### TIME 4: New KSK appears
-log_this ods-enforcer-output echo "--------------------------------------------" &&
+log_this ods-enforcer-output echo "----- Next event is ZSK auto rollover " &&
+log_this ods-enforcer-output echo "--------------- TIME LEAP 3 ----------------" &&
 log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
 log_this ods-enforcer-output echo "--------------------------------------------" &&
 
@@ -111,35 +106,133 @@ log_this ods-enforcer-output ods-enforcer key list  --verbose &&
 log_this ods-enforcer-output ods-enforcer key list --debug &&
 log_this ods-enforcer-output ods-enforcer rollover list &&
 
-#### TIME 5: New KSK should be ready
-log_this ods-enforcer-output echo "--------------------------------------------" &&
+#### TIME 4: New ZSK appears
+log_this ods-enforcer-output echo "--------------- TIME LEAP 4 -----------------" &&
 log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
 log_this ods-enforcer-output echo "--------------------------------------------" &&
 
-log_this ods-enforcer-output echo "----- Expect active/active/publish [Oops - active ZSK is now retired....]" &&
+log_this ods-enforcer-output echo "----- Expect active/active/ready [BUG? Original ZSK should not be retired!]" &&
 log_this ods-enforcer-output ods-enforcer key list  --verbose &&
 log_this ods-enforcer-output ods-enforcer key list --debug &&
 log_this ods-enforcer-output ods-enforcer rollover list &&
 
+#### TIME 5: New ZSK should be ready
+log_this ods-enforcer-output echo "--------------- TIME LEAP 5 ----------------" &&
+log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output echo "--------------------------------------------" &&
+
+log_this ods-enforcer-output echo "----- Expect active/retire/active" &&
+log_this ods-enforcer-output ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output ods-enforcer key list --debug &&
+log_this ods-enforcer-output ods-enforcer rollover list &&
+
+#### TIME 6: Rollover done, back to 2 active keys?
+log_this ods-enforcer-output echo "--------------- TIME LEAP 6 ----------------" &&
+log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output echo "--------------------------------------------" &&
+log_this ods-enforcer-output echo "----- Expect active/active [BUG? I see dead keys!]" &&
+log_this ods-enforcer-output ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output ods-enforcer key list --debug &&
+log_this ods-enforcer-output ods-enforcer rollover list &&
+
+#### TIME 7: Next ZSK rollover starts
+log_this ods-enforcer-output echo "--------------- TIME LEAP 7 ----------------" &&
+log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output echo "--------------------------------------------" &&
+log_this ods-enforcer-output echo "----- Expect active/active/publish [I see dead keys!]" &&
+log_this ods-enforcer-output ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output ods-enforcer key list --debug &&
+
+#### TIME 8: Next ZSK rollover continues
+log_this ods-enforcer-output echo "--------------- TIME LEAP 8 ----------------" &&
+log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output echo "--------------------------------------------" &&
+log_this ods-enforcer-output echo "----- Expect active/active/ready [I see dead keys!]" &&
+log_this ods-enforcer-output ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output ods-enforcer key list --debug &&
+
+#### TIME 9: Next ZSK rollover starts
+log_this ods-enforcer-output echo "--------------- TIME LEAP 9 ----------------" &&
+log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output echo "--------------------------------------------" &&
+log_this ods-enforcer-output echo "----- Expect active/retire/active [I see dead keys!]" &&
+log_this ods-enforcer-output ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output ods-enforcer key list --debug &&
+log_this ods-enforcer-output ods-enforcer rollover list &&
 
 #### Lets try to roll the KSK manually now
-log_this ods-enforcer-output echo "----- Do manual key rollover for KSK" &&
-log_this ods-enforcer-output ods-enforcer key rollover  --zone ods1 --keytype KSK &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
 
-log_this ods-enforcer-output echo "----- Expect a new KSK to be published?" &&
-log_this ods-enforcer-output ods-enforcer key list  --verbose &&
-log_this ods-enforcer-output ods-enforcer key list --debug &&
-log_this ods-enforcer-output ods-enforcer rollover list &&
+log_this ods-enforcer-output_manual echo "----- Do manual key rollover for KSK" &&
+log_this ods-enforcer-output_manual ods-enforcer key rollover  --zone ods1 --keytype KSK &&
+log_this ods-enforcer-output_manual echo "----- Expect a new KSK to be published? Check." &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual echo "----- BUG? The rollover list doesn't seem to know about manual roll.....!!!" &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
 
-#### TIME 6: 
-log_this ods-enforcer-output echo "--------------------------------------------" &&
-log_this ods-enforcer-output 'ods-enforcer time leap' && sleep 1 &&
-log_this ods-enforcer-output echo "--------------------------------------------" &&
+#### TIME 10: Expect KSK to be ready 
+log_this ods-enforcer-output_manual echo "--------------- TIME LEAP 10 ----------------" &&
+log_this ods-enforcer-output_manual 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output_manual echo "--------------------------------------------" &&
+log_this ods-enforcer-output_manual echo "----- Expect a new KSK to be ready? Hmm - actually all that happened was one of the ZSKs changed state in debug..." &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
 
-log_this ods-enforcer-output echo "----- Expect a new KSK to be published?" &&
-log_this ods-enforcer-output ods-enforcer key list  --verbose &&
-log_this ods-enforcer-output ods-enforcer key list --debug &&
-log_this ods-enforcer-output ods-enforcer rollover list &&
+#### TIME 11: Expect KSK to be ready 
+log_this ods-enforcer-output_manual echo "--------------- TIME LEAP 11 ----------------" &&
+log_this ods-enforcer-output_manual 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output_manual echo "--------------------------------------------" &&
+log_this ods-enforcer-output_manual echo "----- Expect a new KSK to be ready/ds-submit? Check. BUG? But as before I now have no active KSK here...." &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
+
+log_this ods-enforcer-output_manual echo "----- Do ds-submit" &&
+log_this ods-enforcer-output_manual 'ods-enforcer key ds-submit --zone ods1 --force' && sleep 1 &&
+
+log_this ods-enforcer-output_manual echo "----- Expect KSKs to be active/ready(ds-seen)" &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
+
+log_this ods-enforcer-output_manual echo "----- Do ds-seen" &&
+KSK_CKA_ID2=`log_grep -o ods-enforcer-output_manual stdout "ods1[[:space:]].*KSK[[:space:]].*ready     waiting for ds-submit" | awk '{print $9}'` &&
+log_this ods-enforcer-output_manual ods-enforcer key ds-seen --zone ods1 --cka_id $KSK_CKA_ID2 && sleep 1 &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
+
+#### TIME 12: Expect KSK to be active 
+log_this ods-enforcer-output_manual echo "----- Wait for DS TTL to pass  " &&
+log_this ods-enforcer-output_manual echo "--------------- TIME LEAP 12 ----------------" &&
+log_this ods-enforcer-output_manual 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output_manual echo "--------------------------------------------" &&
+log_this ods-enforcer-output_manual echo "----- Expect a new KSK to be active since DS TTL has passed?" &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
+
+log_this ods-enforcer-output_manual echo "----- Do ds-retract" &&
+log_this ods-enforcer-output_manual 'ods-enforcer key ds-retract --zone ods1 --force' && sleep 1 &&
+
+log_this ods-enforcer-output_manual echo "----- Expect old KSK to be ??? BUG? State doesn't seem to have changed?" &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
+
+#### TIME 12.5: ???
+log_this ods-enforcer-output_manual echo "--------------- TIME LEAP 12.5 -----------------" &&
+log_this ods-enforcer-output_manual 'ods-enforcer time leap' && sleep 1 &&
+log_this ods-enforcer-output_manual echo "--------------------------------------------" &&
+
+log_this ods-enforcer-output_manual echo "----- Expect active KSK " &&
+log_this ods-enforcer-output_manual ods-enforcer key list  --verbose &&
+log_this ods-enforcer-output_manual ods-enforcer key list --debug &&
+log_this ods-enforcer-output_manual ods-enforcer rollover list &&
 
 
 ods_stop_enforcer &&
