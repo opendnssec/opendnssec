7d870f4878d085532853d84ed808ab5785037145
Jerry Lundström <lundstrom.jerry@gmail.com>
2014-03-17 13:41:31 +0100

    Added code for db_backend_sqlite_create() (untested)

--- enforcer-ng/src/db/db_backend_sqlite.c
+++ enforcer-ng/src/db/db_backend_sqlite.c
@@ -238,6 +238,19 @@ db_result_t* db_backend_sqlite_next(void* data, int finish) {
 
 int db_backend_sqlite_create(void* data, const db_object_t* object, const db_object_field_list_t* object_field_list, const db_value_set_t* value_set) {
     db_backend_sqlite_t* backend_sqlite = (db_backend_sqlite_t*)data;
+    const db_object_field_t* object_field;
+    const db_value_t* value;
+    char sql[4*1024];
+    char* sqlp;
+    int ret, left, bind, first;
+    sqlite3_stmt* statement = NULL;
+    size_t value_pos;
+    int to_int;
+    sqlite3_int64 to_int64;
+    db_type_int32_t int32;
+    db_type_uint32_t uint32;
+    db_type_int64_t int64;
+    db_type_uint64_t uint64;
 
     if (!__sqlite3_initialized) {
         return DB_ERROR_UNKNOWN;
@@ -245,8 +258,186 @@ int db_backend_sqlite_create(void* data, const db_object_t* object, const db_obj
     if (!backend_sqlite) {
         return DB_ERROR_UNKNOWN;
     }
+    if (!object) {
+        return DB_ERROR_UNKNOWN;
+    }
+    if (!object_field_list) {
+        return DB_ERROR_UNKNOWN;
+    }
+    if (!value_set) {
+        return DB_ERROR_UNKNOWN;
+    }
 
-    return DB_ERROR_UNKNOWN;
+    left = sizeof(sql);
+    sqlp = sql;
+
+    if ((ret = snprintf(sqlp, left, "INSERT INTO %s (", db_object_table(object))) >= left) {
+        return DB_ERROR_UNKNOWN;
+    }
+    sqlp += ret;
+    left -= ret;
+
+    object_field = db_object_field_list_begin(object_field_list);
+    first = 1;
+    while (object_field) {
+        /* TODO: Skip primary key? */
+        if (first) {
+            if ((ret = snprintf(sqlp, left, " %s", db_object_field_name(object_field))) >= left) {
+                return DB_ERROR_UNKNOWN;
+            }
+            first = 0;
+        }
+        else {
+            if ((ret = snprintf(sqlp, left, ", %s", db_object_field_name(object_field))) >= left) {
+                return DB_ERROR_UNKNOWN;
+            }
+        }
+        sqlp += ret;
+        left -= ret;
+
+        object_field = db_object_field_next(object_field);
+    }
+
+    if ((ret = snprintf(sqlp, left, " ) VALUES (")) >= left) {
+        return DB_ERROR_UNKNOWN;
+    }
+    sqlp += ret;
+    left -= ret;
+
+    object_field = db_object_field_list_begin(object_field_list);
+    first = 1;
+    while (object_field) {
+        /* TODO: Skip primary key? */
+        if (first) {
+            if ((ret = snprintf(sqlp, left, " ?")) >= left) {
+                return DB_ERROR_UNKNOWN;
+            }
+            first = 0;
+        }
+        else {
+            if ((ret = snprintf(sqlp, left, ", ?")) >= left) {
+                return DB_ERROR_UNKNOWN;
+            }
+        }
+        sqlp += ret;
+        left -= ret;
+
+        object_field = db_object_field_next(object_field);
+    }
+
+    if ((ret = snprintf(sqlp, left, " )")) >= left) {
+        return DB_ERROR_UNKNOWN;
+    }
+    sqlp += ret;
+    left -= ret;
+
+    ret = sqlite3_prepare_v2(backend_sqlite->db,
+        sql,
+        sizeof(sql),
+        &statement,
+        NULL);
+    if (ret != SQLITE_OK) {
+        ods_log_info("DB SQL %s", sql);
+        ods_log_info("DB Err %d\n", ret);
+        if (statement) {
+            sqlite3_finalize(statement);
+        }
+        return DB_ERROR_UNKNOWN;
+    }
+
+    bind = 1;
+    for (value_pos = 0; value_pos < db_value_set_size(value_set); value_pos++) {
+        if (!(value = db_value_set_at(value_set, value_pos))) {
+            sqlite3_finalize(statement);
+            return DB_ERROR_UNKNOWN;
+        }
+
+        switch (db_value_type(value)) {
+        case DB_TYPE_PRIMARY_KEY:
+        case DB_TYPE_INT32:
+            if (db_value_to_int32(value, &int32)) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            to_int = int32;
+            ret = sqlite3_bind_int(statement, bind++, to_int);
+            if (ret != SQLITE_OK) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            break;
+
+        case DB_TYPE_UINT32:
+            if (db_value_to_uint32(value, &uint32)) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            to_int = uint32;
+            ret = sqlite3_bind_int(statement, bind++, to_int);
+            if (ret != SQLITE_OK) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            break;
+
+        case DB_TYPE_INT64:
+            if (db_value_to_int64(value, &int64)) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            to_int64 = int64;
+            ret = sqlite3_bind_int64(statement, bind++, to_int64);
+            if (ret != SQLITE_OK) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            break;
+
+        case DB_TYPE_UINT64:
+            if (db_value_to_uint64(value, &uint64)) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            to_int64 = uint64;
+            ret = sqlite3_bind_int64(statement, bind++, to_int64);
+            if (ret != SQLITE_OK) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            break;
+
+        case DB_TYPE_TEXT:
+            ret = sqlite3_bind_text(statement, bind++, db_value_text(value), 0, SQLITE_STATIC);
+            if (ret != SQLITE_OK) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            break;
+
+        case DB_TYPE_ENUM:
+            ret = sqlite3_bind_int(statement, bind++, db_value_enum_value(value));
+            if (ret != SQLITE_OK) {
+                sqlite3_finalize(statement);
+                return DB_ERROR_UNKNOWN;
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    ret = sqlite3_step(statement);
+    while (ret == SQLITE_BUSY) {
+        usleep(100);
+        ret = sqlite3_step(statement);
+    }
+    sqlite3_finalize(statement);
+    if (ret != SQLITE_DONE) {
+        return DB_ERROR_UNKNOWN;
+    }
+
+    return DB_OK;
 }
 
 db_result_list_t* db_backend_sqlite_read(void* data, const db_object_t* object, const db_join_list_t* join_list, const db_clause_list_t* clause_list) {
