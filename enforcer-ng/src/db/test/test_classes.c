e30161dfffef2c47d1aa14cce78900e668b12424
Jerry Lundström <lundstrom.jerry@gmail.com>
2014-03-19 13:36:28 +0100

    Complete test coverage for db_backend

--- enforcer-ng/src/db/test/test_classes.c
+++ enforcer-ng/src/db/test/test_classes.c
@@ -35,6 +35,7 @@
 
 static db_backend_handle_t* backend_handle = NULL;
 static db_backend_t* backend = NULL;
+static int backend_data = 0;
 
 int init_suite_classes(void) {
     if (backend_handle) {
@@ -54,36 +55,109 @@ int clean_suite_classes(void) {
     return 0;
 }
 
+int __db_backend_handle_initialize(void* data) {
+    CU_ASSERT(data == &backend_data);
+    return 0;
+}
+
+int __db_backend_handle_shutdown(void* data) {
+    CU_ASSERT(data == &backend_data);
+    return 0;
+}
+
+int __db_backend_handle_connect(void* data, const db_configuration_list_t* configuration_list) {
+    CU_ASSERT(data == &backend_data);
+    CU_ASSERT((void*)configuration_list == &backend_data);
+    return 0;
+}
+
+int __db_backend_handle_disconnect(void* data) {
+    CU_ASSERT(data == &backend_data);
+    return 0;
+}
+
+int __db_backend_handle_create(void* data, const db_object_t* object, const db_object_field_list_t* object_field_list, const db_value_set_t* value_set) {
+    CU_ASSERT(data == &backend_data);
+    CU_ASSERT((void*)object == &backend_data);
+    CU_ASSERT((void*)object_field_list == &backend_data);
+    CU_ASSERT((void*)value_set == &backend_data);
+    return 0;
+}
+
+db_result_list_t* __db_backend_handle_read(void* data, const db_object_t* object, const db_join_list_t* join_list, const db_clause_list_t* clause_list) {
+    CU_ASSERT(data == &backend_data);
+    CU_ASSERT((void*)object == &backend_data);
+    CU_ASSERT((void*)join_list == &backend_data);
+    CU_ASSERT((void*)clause_list == &backend_data);
+    return (db_result_list_t*)&backend_data;
+}
+
+int __db_backend_handle_update(void* data, const db_object_t* object, const db_object_field_list_t* object_field_list, const db_value_set_t* value_set, const db_clause_list_t* clause_list) {
+    CU_ASSERT(data == &backend_data);
+    CU_ASSERT((void*)object == &backend_data);
+    CU_ASSERT((void*)object_field_list == &backend_data);
+    CU_ASSERT((void*)value_set == &backend_data);
+    CU_ASSERT((void*)clause_list == &backend_data);
+    return 0;
+}
+
+int __db_backend_handle_delete(void* data, const db_object_t* object, const db_clause_list_t* clause_list) {
+    CU_ASSERT(data == &backend_data);
+    CU_ASSERT((void*)object == &backend_data);
+    CU_ASSERT((void*)clause_list == &backend_data);
+    return 0;
+}
+
+void __db_backend_handle_free(void* data) {
+    CU_ASSERT(data == &backend_data);
+}
+
+int __db_backend_handle_transaction_begin(void* data) {
+    CU_ASSERT(data == &backend_data);
+    return 0;
+}
+
+int __db_backend_handle_transaction_commit(void* data) {
+    CU_ASSERT(data == &backend_data);
+    return 0;
+}
+
+int __db_backend_handle_transaction_rollback(void* data) {
+    CU_ASSERT(data == &backend_data);
+    return 0;
+}
+
 void test_class_db_backend_handle(void) {
     CU_ASSERT_PTR_NOT_NULL_FATAL((backend_handle = db_backend_handle_new()));
-    /*
-    int db_backend_handle_initialize(const db_backend_handle_t*);
-    int db_backend_handle_shutdown(const db_backend_handle_t*);
-    int db_backend_handle_connect(const db_backend_handle_t*, const db_configuration_list_t*);
-    int db_backend_handle_disconnect(const db_backend_handle_t*);
-    int db_backend_handle_create(const db_backend_handle_t*, const db_object_t*, const db_object_field_list_t*, const db_value_set_t*);
-    db_result_list_t* db_backend_handle_read(const db_backend_handle_t*, const db_object_t*, const db_join_list_t*, const db_clause_list_t*);
-    int db_backend_handle_update(const db_backend_handle_t*, const db_object_t*, const db_object_field_list_t*, const db_value_set_t*, const db_clause_list_t*);
-    int db_backend_handle_delete(const db_backend_handle_t*, const db_object_t*, const db_clause_list_t*);
-    int db_backend_handle_transaction_begin(const db_backend_handle_t*);
-    int db_backend_handle_transaction_commit(const db_backend_handle_t*);
-    int db_backend_handle_transaction_rollback(const db_backend_handle_t*);
-    const void* db_backend_handle_data(const db_backend_handle_t*);
-    int db_backend_handle_set_initialize(db_backend_handle_t*, db_backend_handle_initialize_t);
-    int db_backend_handle_set_shutdown(db_backend_handle_t*, db_backend_handle_shutdown_t);
-    int db_backend_handle_set_connect(db_backend_handle_t*, db_backend_handle_connect_t);
-    int db_backend_handle_set_disconnect(db_backend_handle_t*, db_backend_handle_disconnect_t);
-    int db_backend_handle_set_create(db_backend_handle_t*, db_backend_handle_create_t);
-    int db_backend_handle_set_read(db_backend_handle_t*, db_backend_handle_read_t);
-    int db_backend_handle_set_update(db_backend_handle_t*, db_backend_handle_update_t);
-    int db_backend_handle_set_delete(db_backend_handle_t*, db_backend_handle_delete_t);
-    int db_backend_handle_set_free(db_backend_handle_t*, db_backend_handle_free_t);
-    int db_backend_handle_set_transaction_begin(db_backend_handle_t*, db_backend_handle_transaction_begin_t);
-    int db_backend_handle_set_transaction_commit(db_backend_handle_t*, db_backend_handle_transaction_commit_t);
-    int db_backend_handle_set_transaction_rollback(db_backend_handle_t*, db_backend_handle_transaction_rollback_t);
-    int db_backend_handle_set_data(db_backend_handle_t*, void*);
-    int db_backend_handle_not_empty(const db_backend_handle_t*);
-    */
+
+    CU_ASSERT(!db_backend_handle_set_initialize(backend_handle, __db_backend_handle_initialize));
+    CU_ASSERT(!db_backend_handle_set_shutdown(backend_handle, __db_backend_handle_shutdown));
+    CU_ASSERT(!db_backend_handle_set_connect(backend_handle, __db_backend_handle_connect));
+    CU_ASSERT(!db_backend_handle_set_disconnect(backend_handle, __db_backend_handle_disconnect));
+    CU_ASSERT(!db_backend_handle_set_create(backend_handle, __db_backend_handle_create));
+    CU_ASSERT(!db_backend_handle_set_read(backend_handle, __db_backend_handle_read));
+    CU_ASSERT(!db_backend_handle_set_update(backend_handle, __db_backend_handle_update));
+    CU_ASSERT(!db_backend_handle_set_delete(backend_handle, __db_backend_handle_delete));
+    CU_ASSERT(!db_backend_handle_set_free(backend_handle, __db_backend_handle_free));
+    CU_ASSERT(!db_backend_handle_set_transaction_begin(backend_handle, __db_backend_handle_transaction_begin));
+    CU_ASSERT(!db_backend_handle_set_transaction_commit(backend_handle, __db_backend_handle_transaction_commit));
+    CU_ASSERT(!db_backend_handle_set_transaction_rollback(backend_handle, __db_backend_handle_transaction_rollback));
+    CU_ASSERT(!db_backend_handle_set_data(backend_handle, &backend_data));
+
+    CU_ASSERT_FATAL(!db_backend_handle_not_empty(backend_handle));
+    CU_ASSERT(db_backend_handle_data(backend_handle) == &backend_data);
+
+    CU_ASSERT(!db_backend_handle_initialize(backend_handle));
+    CU_ASSERT(!db_backend_handle_shutdown(backend_handle));
+    CU_ASSERT(!db_backend_handle_connect(backend_handle, (db_configuration_list_t*)&backend_data));
+    CU_ASSERT(!db_backend_handle_disconnect(backend_handle));
+    CU_ASSERT(!db_backend_handle_create(backend_handle, (db_object_t*)&backend_data, (db_object_field_list_t*)&backend_data, (db_value_set_t*)&backend_data));
+    CU_ASSERT(db_backend_handle_read(backend_handle, (db_object_t*)&backend_data, (db_join_list_t*)&backend_data, (db_clause_list_t*)&backend_data) == (db_result_list_t*)&backend_data);
+    CU_ASSERT(!db_backend_handle_update(backend_handle, (db_object_t*)&backend_data, (db_object_field_list_t*)&backend_data, (db_value_set_t*)&backend_data, (db_clause_list_t*)&backend_data));
+    CU_ASSERT(!db_backend_handle_delete(backend_handle, (db_object_t*)&backend_data, (db_clause_list_t*)&backend_data));
+    CU_ASSERT(!db_backend_handle_transaction_begin(backend_handle));
+    CU_ASSERT(!db_backend_handle_transaction_commit(backend_handle));
+    CU_ASSERT(!db_backend_handle_transaction_rollback(backend_handle));
 }
 
 void test_class_db_backend(void) {
@@ -97,19 +171,19 @@ void test_class_db_backend(void) {
     backend_handle = NULL;
     CU_ASSERT_FATAL(db_backend_handle(backend) == local_backend_handle);
     CU_ASSERT_FATAL(!db_backend_not_empty(backend));
-    /*
-    int db_backend_initialize(const db_backend_t*);
-    int db_backend_shutdown(const db_backend_t*);
-    int db_backend_connect(const db_backend_t*, const db_configuration_list_t*);
-    int db_backend_disconnect(const db_backend_t*);
-    int db_backend_create(const db_backend_t*, const db_object_t*, const db_object_field_list_t*, const db_value_set_t*);
-    db_result_list_t* db_backend_read(const db_backend_t*, const db_object_t*, const db_join_list_t*, const db_clause_list_t*);
-    int db_backend_update(const db_backend_t*, const db_object_t*, const db_object_field_list_t*, const db_value_set_t*, const db_clause_list_t*);
-    int db_backend_delete(const db_backend_t*, const db_object_t*, const db_clause_list_t*);
-    int db_backend_transaction_begin(const db_backend_t*);
-    int db_backend_transaction_commit(const db_backend_t*);
-    int db_backend_transaction_rollback(const db_backend_t*);
-    */
+
+    CU_ASSERT(!db_backend_initialize(backend));
+    CU_ASSERT(!db_backend_shutdown(backend));
+    CU_ASSERT(!db_backend_connect(backend, (db_configuration_list_t*)&backend_data));
+    CU_ASSERT(!db_backend_disconnect(backend));
+    CU_ASSERT(!db_backend_create(backend, (db_object_t*)&backend_data, (db_object_field_list_t*)&backend_data, (db_value_set_t*)&backend_data));
+    CU_ASSERT(db_backend_read(backend, (db_object_t*)&backend_data, (db_join_list_t*)&backend_data, (db_clause_list_t*)&backend_data) == (db_result_list_t*)&backend_data);
+    CU_ASSERT(!db_backend_update(backend, (db_object_t*)&backend_data, (db_object_field_list_t*)&backend_data, (db_value_set_t*)&backend_data, (db_clause_list_t*)&backend_data));
+    CU_ASSERT(!db_backend_delete(backend, (db_object_t*)&backend_data, (db_clause_list_t*)&backend_data));
+    CU_ASSERT(!db_backend_transaction_begin(backend));
+    CU_ASSERT(!db_backend_transaction_commit(backend));
+    CU_ASSERT(!db_backend_transaction_rollback(backend));
+
     db_backend_free(backend);
     backend = NULL;
     CU_PASS("db_backend_handle_free");
