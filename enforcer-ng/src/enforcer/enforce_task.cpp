1a0946cfff4b7f3375bd5138a8c7ef32709199d0
Yuri Schaeffer <yuri@nlnetlabs.nl>
2014-03-21 16:36:30 +0100

    log msg to submit/retract a DS record

--- enforcer-ng/src/enforcer/enforce_task.cpp
+++ enforcer-ng/src/enforcer/enforce_task.cpp
@@ -251,13 +251,27 @@ perform_enforce(int sockfd, engine_type *engine, int bForceUpdate,
 				if (enfZone.signerConfNeedsWriting())
 					bSignerConfNeedsWriting = true;
 				
+				bool bSubmitThisZone = false;
+				bool bRetractThisZone = false;
 				KeyDataList &kdl = enfZone.keyDataList();
 				for (int k=0; k<kdl.numKeys(); ++k) {
 					if (kdl.key(k).dsAtParent() == DS_SUBMIT)
-						bSubmitToParent = true;
+						bSubmitThisZone = true;
 					if (kdl.key(k).dsAtParent() == DS_RETRACT)
-						bRetractFromParent = true;
+						bRetractThisZone = true;
 				}
+				if (bSubmitThisZone || bRetractThisZone) {
+					for (int k=0; k<kdl.numKeys(); ++k) {
+						if (kdl.key(k).dsAtParent() == DS_SUBMIT)
+							ods_log_warning("[%s] please submit DS with keytag %d for zone %s", 
+								module_str, kdl.key(k).keytag()&0xFFFF, enfzone.name().c_str());
+						if (kdl.key(k).dsAtParent() == DS_RETRACT)
+							ods_log_warning("[%s] please retract DS with keytag %d for zone %s", 
+								module_str, kdl.key(k).keytag()&0xFFFF, enfzone.name().c_str());
+					}
+				}
+				bSubmitToParent |= bSubmitThisZone;
+				bRetractFromParent |= bRetractThisZone;
 				
 				if (t_next == -1) {
 					client_printf(sockfd,
