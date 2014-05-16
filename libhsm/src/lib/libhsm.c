64115fcbfe339ace1ced6f3639825186565fb615
Jerry Lundström <lundstrom.jerry@gmail.com>
2014-05-16 17:57:37 +0200

    Fix a memory leak in libhsm

--- libhsm/src/lib/libhsm.c
+++ libhsm/src/lib/libhsm.c
@@ -2672,7 +2672,10 @@ hsm_get_key_id(hsm_ctx_t *ctx, const hsm_key_t *key)
 
     /* this is plain binary data, we need to convert it to hex */
     id_str = malloc(len * 2 + 1);
-    if (!id_str) return NULL;
+    if (!id_str) {
+        free(id);
+        return NULL;
+    }
 
     hsm_hex_unparse(id_str, id, len);
 
