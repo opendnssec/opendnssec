22a8f51d8cbee1cdb31a025a6f900b3f0e4c2923
Yuri Schaeffer <yuri@nlnetlabs.nl>
2014-07-01 11:49:23 +0200

    Remove c++ cruft

--- enforcer-ng/src/keystate/keystate_list_cmd.h
+++ enforcer-ng/src/keystate/keystate_list_cmd.h
@@ -30,14 +30,6 @@
 #ifndef _KEYSTATE_LIST_CMD_H_
 #define _KEYSTATE_LIST_CMD_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 struct cmd_func_block* key_list_funcblock(void);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _KEYSTATE_LIST_CMD_H_ */
