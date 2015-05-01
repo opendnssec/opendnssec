e0ea420b0b4db2a086d21c32a4f707345b851988
Hoda Rohani <hoda@nlnetlabs.nl>
2015-05-01 15:12:54 +0200

    updating signconf files

--- testing/test-cases.d/signer.zones.validate_many_zones/test.sh
+++ testing/test-cases.d/signer.zones.validate_many_zones/test.sh
@@ -57,8 +57,9 @@ esac &&
 # Tests to cover signing specific bugs
 
 #SUPPORT-40 - Double check that all records down to the forth level appear in the output
-$GREP -q -- "^test.example.com..*86400.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
-$GREP -q -- "^test1.test.example.com..*86400.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
+# having TTL = 600 due to MaxZoneTTL in kasp.xml
+$GREP -q -- "^test.example.com..*600.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
+$GREP -q -- "^test1.test.example.com..*600.*IN.*NS.*ns2.example.com." "$INSTALL_ROOT/var/opendnssec/signed/example.com" &&
 
 #OPENDSNSEC-290 - Update the zone by changing a CNAME record to an A record.
 ods_setup_zone test/all.rr.org &&
@@ -68,14 +69,16 @@ syslog_waitfor_count 60 2 'ods-signerd: .*\[STATS\] all.rr.org' &&
 test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
 
 #OPENDNSSEC-247 - Update the SOA minimum in the policy and make sure the NSEC TTL changes.
-$GREP -q -- "<Minimum>PT300S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
+$GREP -q -- "<Minimum>PT5M</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
 $GREP -q -- "300.*IN.*NSEC3" "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
 cp kasp.xml kasp.xml_orig &&
 cp test/kasp.xml kasp.xml &&
 log_this ods-update-policy ods_setup_conf kasp.xml &&
-log_this_timeout ods-update-policy 10 ods-ksmutil update kasp &&
-syslog_waitfor 60 'ods-enforcerd: .*Called signer engine:.*ods-signer update all.rr.org' &&
-$GREP -q -- "<Minimum>PT600S</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
+log_this_timeout ods-update-policy 10 ods-enforcer update kasp &&
+#syslog_waitfor 60 'ods-enforcerd: .*Called signer engine:.*ods-signer update all.rr.org' &&
+syslog_waitfor 60 'ods-enforcerd: .*\[enforcer\] updateZone: processing all.rr.org with policyName default' &&
+syslog_waitfor 60 'ods-signerd: \[cmdhandler\] zone all.rr.org scheduled for immediate re-sign' &&
+$GREP -q -- "<Minimum>PT10M</Minimum>" "$INSTALL_ROOT/var/opendnssec/signconf/all.rr.org" &&
 syslog_waitfor_count 60 3 'ods-signerd: .*\[STATS\] all.rr.org' &&
 test -f "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
 $GREP -q -- "600.*IN.*NSEC3" "$INSTALL_ROOT/var/opendnssec/signed/all.rr.org" &&
