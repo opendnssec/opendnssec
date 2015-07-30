e0d9e5ff0db698290219b44f90e2afeb435e9f2d
halderen <berry@nlnetlabs.nl>
2015-07-30 09:05:07 +0200

    OPENDNSSEC-652: - longer sleep period after zone add for account for asyncronous workings - longer sleeps after time leap necessary - more time leaps necessary as zones are not processed at same time (see   in file comment).

--- testing/test-cases.d/enforcer.ods-enforcer.update_ok/test.sh
+++ testing/test-cases.d/enforcer.ods-enforcer.update_ok/test.sh
@@ -12,7 +12,7 @@ ods_reset_env &&
 ods_start_enforcer &&
 
 # Add our new zone.
-log_this_timeout ods-enforcer-zone-add 5 ods-enforcer zone add -z ods2 &&
+log_this_timeout ods-enforcer-zone-add 15 ods-enforcer zone add -z ods2 &&
 
 # This zone should not have been picked up by the enforcer yet, so it should be in the list...
 log_this ods-enforcer-zone-list1 ods-enforcer zone list &&
@@ -46,11 +46,36 @@ log_grep ods-enforcer-zone-list1 stdout 'ods2[[:space:]].*default' &&
 #log_this ods-enforcer-zone-list2 ods-enforcer zone list &&
 #log_grep ods-enforcer-zone-list2 stdout 'ods2[[:space:]].*default' &&
 
-sleep 1 &&
+# There need to be five leaps,
+# - one to add the ods zone (the ods2 zone is added as a consequence of
+# the zone add command)
+# - one to put the ods2 ZSK from generated to publish state)
+# - another one for the ods KSK to publish (since they are not
+# added at the same time they will have separate transitions
+# - one to put to ZSK to active and KSK to ready for ods2
+# - and finally one to to the same for ods zone
+# note that this relies heavily on the internal workings of the
+# enforcer.  If it decides to break up the now combined ZSK and
+# KSK transitions, or do ods and ods2 at the same time, the test
+# is too fragile and will break.
+# It would be much better to leap incrementally (i.e. also do
+# the intermediate steps) to the expected time from the policy the
+# keys are in the right state, regardless of how many times a 
+# leap should take place.
+# And the sleep commands necessary are awfull of course to guard
+# against the asynchronisity of the commands execution.
+
+sleep 5 &&
 log_this ods-enforcer-time-leap ods-enforcer time leap &&
-sleep 1 &&
+sleep 5 &&
 log_this ods-enforcer-time-leap ods-enforcer time leap &&
-sleep 1 &&
+sleep 5 &&
+log_this ods-enforcer-time-leap ods-enforcer time leap &&
+sleep 5 &&
+log_this ods-enforcer-time-leap ods-enforcer time leap &&
+sleep 5 &&
+log_this ods-enforcer-time-leap ods-enforcer time leap &&
+sleep 5 &&
 
 # Check that we have 2 keys per zone
 log_this ods-enforcer-key-list2 ods-enforcer key list &&
