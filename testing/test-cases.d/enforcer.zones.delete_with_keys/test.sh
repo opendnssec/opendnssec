85ed5159d760f69f501b81ace8275c324c09d5d1
Hoda Rohani <hoda@nlnetlabs.nl>
2015-08-17 09:34:15 +0200

    waiting more for redhat

--- testing/test-cases.d/enforcer.zones.delete_with_keys/test.sh
+++ testing/test-cases.d/enforcer.zones.delete_with_keys/test.sh
@@ -15,7 +15,7 @@ ods_start_enforcer &&
 # Add our test zone. We already have the standard one and a "spare" one on a different policy
 log_this_timeout ods-enforcer-zone-add 5 ods-enforcer zone add -z test.delete --policy non-default &&
 
-sleep 60 &&
+sleep 300 &&
 
 
 # Check the presence of all signconfs
