27d4932d84664f0f85fc08ae246a1b309e55899b
halderen <berry@nlnetlabs.nl>
2015-08-28 10:02:44 +0200

    - OPENDNSSEC-642: remove test part as per previous commit

--- testing/test-cases.d/enforcer.keys.allocation/test.sh
+++ testing/test-cases.d/enforcer.keys.allocation/test.sh
@@ -11,12 +11,6 @@ fi &&
 
 ods_reset_env &&
 
-rm -rf base &&
-mkdir  base &&
-
-# Used only to create a gold while setting up the test
-#rm -rf gold && mkdir gold &&
-
 ##################  SETUP ###########################
 # Start enforcer (Zones already exist and we let it generate keys itself)
 ods_start_enforcer &&
@@ -120,16 +114,6 @@ test `ods-hsmutil list | grep ^SoftHSM | wc -l` -eq 16 &&
 # echo "Make sure the shared policies use fewer keys" &&
 # test `ods-hsmutil list | grep ^SoftHSM | grep RSA/1024 | wc -l` -lt \
 #      `ods-hsmutil list | grep ^SoftHSM | grep RSA/2048 | wc -l` &&
-
-for zone in non-share1 non-share2 non-share3 share1 share2 share3; do
-	# Used only to create a gold while setting up the test
-	#cp $INSTALL_ROOT/var/opendnssec/signconf/$zone.xml gold/signconf_"$zone".xml        
-	cp $INSTALL_ROOT/var/opendnssec/signconf/$zone.xml base/signconf_"$zone".xml
-done &&
-
-# compare all the signconf files
-log_this ods-compare-signconfs  ods_compare_gold_vs_base_signconf &&
-rm -rf base &&
 
 return 0
 
