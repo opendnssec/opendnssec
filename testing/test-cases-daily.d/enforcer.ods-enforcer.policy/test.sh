06e283bcc2caed6a6fcd84eae2a60c4fbd2490bf
(Berry) A.W. van Halderen <berry@halderen.net>
2015-09-21 14:58:25 +0200

    Revert "enforcer.ods-enforcer.policy test"

--- testing/test-cases-daily.d/enforcer.ods-enforcer.policy/test.sh
+++ testing/test-cases-daily.d/enforcer.ods-enforcer.policy/test.sh
@@ -93,11 +93,10 @@ $diff_ignore_whitespace "$INSTALL_ROOT/etc/opendnssec/kasp.xml" "kasp.xml.gold_e
 echo "************kasp OK******************" &&
 
 # Now use purge to clean up policy 3 and it will also remove policy 2
-log_this ods-enforcer-policy-purge_1 "ods-enforcer policy purge" &&
-
-# The kasp file hasn't been changed, only db has these purges
-#sed  -e 's#>.*</Salt>#/>#g' "$INSTALL_ROOT/etc/opendnssec/kasp.xml" > "$INSTALL_ROOT/etc/opendnssec/kasp.xml2" &&
-#$diff_ignore_whitespace -I "^<?xml" "$INSTALL_ROOT/etc/opendnssec/kasp.xml2"  kasp.xml.gold_export_default_policy &&
+echo "y" | log_this ods-enforcer-policy-purge_1 "ods-enforcer policy purge" &&
+# check the kasp has been updated
+sed  -e 's#>.*</Salt>#/>#g' "$INSTALL_ROOT/etc/opendnssec/kasp.xml" > "$INSTALL_ROOT/etc/opendnssec/kasp.xml2" &&
+$diff_ignore_whitespace -I "^<?xml" "$INSTALL_ROOT/etc/opendnssec/kasp.xml2"  kasp.xml.gold_export_default_policy &&
 echo "************export OK******************" &&
 
 # Export the remaining policy
@@ -110,11 +109,9 @@ echo "************export OK******************" &&
 echo &&
 
 # Now check we export an empty policy 
-log_this ods-enforcer-remove-zone ods-enforcer zone delete --all &&
+echo "y" | log_this ods-enforcer-remove-zone ods-enforcer zone delete --all &&
 # Now use purge to remomve the remainin policy
-log_this ods-enforcer-policy-purge_2 "ods-enforcer policy purge" &&
-
-ods-enforcer policy export --all > "$INSTALL_ROOT/etc/opendnssec/kasp.xml"&&
+echo "y" | log_this ods-enforcer-policy-purge_2 "ods-enforcer policy purge" &&
 # check the kasp has been updated
 $diff_ignore_whitespace  "$INSTALL_ROOT/etc/opendnssec/kasp.xml"  kasp.xml.gold_export_empty &&
 echo "************empty kasp OK******************" &&
@@ -125,17 +122,17 @@ echo &&
 # check the invalid XML won't import, and we should get the expected errors
 log_this ods-enforcer-import-invalidXML cp -- "kasp_invalid.xml" "$INSTALL_ROOT/etc/opendnssec/kasp.xml" &&
 ### TO FIX: this command should fail
-! log_this ods-enforcer-import-invalidXML ods-enforcer policy import &&
+log_this ods-enforcer-import-invalidXML ods-enforcer policy import &&
 ### TO FIX: this text should be found
 #log_grep ods-enforcer-import-invalidXML stderr 'ods-kaspcheck returned an error, please check your policy' &&
-log_grep ods-enforcer-import-invalidXML stderr "Unable to validate the KASP XML, please run ods-kaspcheck for more details!" &&
+log_grep ods-enforcer-import-invalidXML stdout "error: reading and processing kasp.xml file failed" &&
 echo "****************check the invalid XML OK*********************" &&
 echo &&
 
 # check the file won't import if a repo does not exist, and we should get the expected errors
 log_this ods-enforcer-import-invalidXML_1 cp -- "kasp_missing_repo.xml" "$INSTALL_ROOT/etc/opendnssec/kasp.xml" &&
 ### TO FIX: this command should fail
-! log_this ods-enforcer-import-invalidXML_1 ods-enforcer policy import &&
+log_this ods-enforcer-import-invalidXML_1 ods-enforcer policy import &&
 ### TO FIX: this text should be found
 #log_grep ods-enforcer-import-invalidXML_1 stdout "ERROR: Unknown repository (bob) defined for KSK in default policy in " &&
 echo "****************check the invalid XML OK*********************" &&
@@ -143,11 +140,11 @@ echo &&
 
 # Test incomplete command line for export
 ### TO FIX: this command should fail
-! log_this ods-enforcer-export-incomplete-parameters ods-enforcer policy export &&
-log_grep ods-enforcer-export-incomplete-parameters stderr 'Either --all or --policy needs to be given' &&
+log_this ods-enforcer-export-incomplete-parameters ods-enforcer policy export &&
+log_grep ods-enforcer-export-incomplete-parameters stdout 'expected --policy <policy> | --all  option' &&
 ### TO FIX: this command should fail
-! log_this ods-enforcer-export-incomplete-parameters ods-enforcer policy &&
-log_grep ods-enforcer-export-incomplete-parameters stderr 'Unknown command policy' &&
+log_this ods-enforcer-export-incomplete-parameters ods-enforcer policy &&
+log_grep ods-enforcer-export-incomplete-parameters stdout 'Unknown command policy' &&
 echo "************incomplete parameter validation OK******************" &&
 echo &&
 
