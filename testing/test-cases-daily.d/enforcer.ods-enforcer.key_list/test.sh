fbdf1bf72557333d865882a1150c8827faa7f445
halderen <berry@nlnetlabs.nl>
2015-10-29 23:43:23 +0100

    OPENDNSSEC-704: Port all test cases from 1.4, however the check is done quite different In 1.4, the test would rely on timeshift only performing one step at a time, and it being very fixed on the states the key would be in.  In 2.0, the effect is far less deterministic.  To counter this, we just loop over a number of states. For this test it is not important in which order the key runs though states, just that the key states are entered at some time, to test the key list command.
    All test cases are ported with the exception of:
    - key purge, since key purge is not yet implemented AND there is a specific other
      test present to test the key purge functionality.
    - tests to assert a key comes in generate state, since this state is so transisional
      that it cannot be captured consistently.  It would be possible with pre-generation
      of keys.

--- testing/test-cases-daily.d/enforcer.ods-enforcer.key_list/test.sh
+++ testing/test-cases-daily.d/enforcer.ods-enforcer.key_list/test.sh
@@ -2,50 +2,116 @@
 #
 #TEST: Test to check that key list works correctly
 
-# Lets use parameters for the timing intervals so they are easy to change
-SHORT_TIMEOUT=11    # Timeout when checking log output. DS lock out wait is 10 sec so use 11 for this
-LONG_TIMEOUT=40     # Timeout when waiting for enforcer run to have happened
-SLEEP_INTERVAL=50   # This should be just shorter than the enforcer run interval in conf.xml
+# expect <filepattern> <regexpattern>
+# expect that the regular expression pattern is present in at least
+# one of the files
+expect ()
+{
+  local name=$1
+  local pattern=$2
+  echo expecting $2
+  cat _log.$BUILD_TAG.$name.stdout | sed -e '/^[[:alpha:]]*:/d' | $GREP  -- "$pattern"
+}
+# expectbut <filepattern> <regexpattern>
+# expect that no other than the regular expression pattern is present in any of
+# the files
+expectbut ()
+{
+  local name=$1
+  local pattern=$2
+  echo expecting-but $2
+  cat _log.$BUILD_TAG.$name.stdout | sed -e '/^[[:alpha:]]*:/d' | $GREP  -v -- "$pattern"
+}
 
 if [ -n "$HAVE_MYSQL" ]; then
         ods_setup_conf conf.xml conf-mysql.xml
-else 
-        ods_setup_conf conf.xml conf.xml
 fi &&
 
 ods_reset_env &&
 
-ods_ods-control_enforcer_start &&
+ods_start_enforcer &&
 
 ##################  SETUP ###########################
 # Add a zone
-log_this ods-enforcer-setup_zone_and_keys   ods-enforcer zone add --zone ods --policy Policy1 &&
-log_grep ods-enforcer-setup_zone_and_keys   stdout "Zone ods added successfully" &&
-log_this ods-enforcer-setup_zone_and_keys   ods-enforcer zone list &&
-log_grep ods-enforcer-setup_zone_and_keys   stdout "ods[ \t][ \t]*Policy1[ \t]" &&
+echo -n "LINE: ${LINENO} " && log_this ods-enforcer-setup_zone_and_keys   ods-enforcer zone add --zone ods --policy default &&
+echo -n "LINE: ${LINENO} " && log_this list-first                         ods-enforcer key list --verbose --all &&
+echo -n "LINE: ${LINENO} " && log_grep ods-enforcer-setup_zone_and_keys   stdout "Zone ods added successfully" &&
+echo -n "LINE: ${LINENO} " && log_this ods-enforcer-setup_zone_and_keys   ods-enforcer zone list &&
+echo -n "LINE: ${LINENO} " && log_grep ods-enforcer-setup_zone_and_keys   stdout "ods[ \t][ \t]*default[ \t]" &&
+echo -n "LINE: ${LINENO} " && log_this list-second                        ods-enforcer key list --verbose --all &&
+echo -n "LINE: ${LINENO} " && log_this list-third                         ods-enforcer key list --verbose --all &&
+echo -n "LINE: ${LINENO} " && log_this list-fourth                        ods-enforcer key list --verbose --all &&
+ods_idle &&
+echo -n "LINE: ${LINENO} " && log_this list-fifth                        ods-enforcer key list --verbose --all &&
 
-ods_enforcer_idle &&
-ods_enforcer_leap_over 900 &&
-echo `ods-enforcer key ds-seen 2>/dev/null | sed -e 's/^ods[[:space:]]\+KSK[[:space:]]\+[[:digit:]]\+[[:space:]]\+\([[:xdigit:]]\{32\}\)[[:space:]]*/\1/p' -e d` &&
-ods-enforcer key ds-seen -z ods -k `ods-enforcer key ds-seen 2>/dev/null | sed -e 's/^ods[[:space:]]\+KSK[[:space:]]\+[[:digit:]]\+[[:space:]]\+\([[:xdigit:]]\{32\}\)[[:space:]]*/\1/p' -e d` &&
-ods_enforcer_idle &&
+echo -n "LINE: ${LINENO} " && ods_enforcer_idle &&
+echo -n "LINE: ${LINENO} " && ods_enforcer_leap_over 900 &&
+echo -n "LINE: ${LINENO} " && ods-enforcer key ds-seen -z ods -k `ods-enforcer key ds-seen 2>/dev/null | sed -e 's/^ods[[:space:]]\+KSK[[:space:]]\+[[:digit:]]\+[[:space:]]\+\([[:xdigit:]]\{32\}\)[[:space:]]*/\1/p' -e d` &&
+echo -n "LINE: ${LINENO} " && ods_enforcer_idle &&
 
-echo -n "LINE: ${LINENO} " && log_this list-ods           ods-enforcer key list --zone ods &&
-echo -n "LINE: ${LINENO} " && log_this list-nonods        ods-enforcer key list --zone ods &&
-echo -n "LINE: ${LINENO} " && log_this list-zsk           ods-enforcer key list --keytype ZSK &&
-echo -n "LINE: ${LINENO} " && log_this list-ksk           ods-enforcer key list --keytype KSK &&
-echo -n "LINE: ${LINENO} " && log_this list-active        ods-enforcer key list --keystate active &&
-echo -n "LINE: ${LINENO} " && log_this list-activepublish ods-enforcer key list --keystate active,publish &&
-echo -n "LINE: ${LINENO} " && log_this list-publishzsk    ods-enforcer key list --keystate publish --keytype ZSK &&
-echo -n "LINE: ${LINENO} " && log_this list-all           ods-enforcer key list --all &&
-echo -n "LINE: ${LINENO} " && log_this list-allzsk        ods-enforcer key list --all --keytype ZSK &&
+# using --zone you should only get keys for that selected zone
+echo -n "LINE: ${LINENO} " && log_this list-ods    ods-enforcer key list --zone ods &&
+echo -n "LINE: ${LINENO} " && log_this list-nonods ods-enforcer key list --zone sdo &&
+echo -n "LINE: ${LINENO} " && expect list-ods '^ods ' &&
+echo -n "LINE: ${LINENO} " && ! expect list-nonods '^ods ' &&
+
+# using --keystate and --all you should get an error
+echo -n "LINE: ${LINENO} " && ! log_this list-conflict ods-enforcer key list --keystate publish --all &&
 
 i=0 &&
 while [ $i -lt 25 ]; do
-  log_this list-$i ods-enforcer key list --verbose &&
-  log_this list-$i ods-enforcer time leap &&
+  log_this list-$i-zsk              ods-enforcer key list --keytype ZSK &&
+  log_this list-$i-ksk              ods-enforcer key list --keytype KSK &&
+  log_this list-$i-active           ods-enforcer key list --keystate active &&
+  log_this list-$i-activepublish    ods-enforcer key list --keystate active,publish &&
+  log_this list-$i-publishzsk       ods-enforcer key list --keystate publish --keytype ZSK &&
+  log_this list-$i-generateverbose  ods-enforcer key list --keystate generate --verbose &&
+  log_this list-$i-all              ods-enforcer key list --all &&
+  log_this list-$i-allzsk           ods-enforcer key list --all --keytype ZSK &&
+  log_this list-$i-activezskverbose ods-enforcer key list --verbose --keystate active --keytype ZSK &&
+  log_this list-$i-plain ods-enforcer key list &&
+  log_this list-$i-verbose ods-enforcer key list --verbose &&
+  log_this list-$i-verboseall ods-enforcer key list --verbose --all &&
+  log_this list-$i-parsable ods-enforcer key list --parsable &&
+  log_this list-$i-debug ods-enforcer key list --debug &&
+  log_this timeleap ods-enforcer time leap &&
   i=`expr $i + 1`
 done &&
+
+# When listing ZSKs, we should only see ZSKs, and see a ZSK at least
+# once.  Likewise for KSKs of course.
+echo -n "LINE: ${LINENO} " && ! expectbut list-*-zsk "ZSK" &&
+echo -n "LINE: ${LINENO} " && ! expectbut list-*-ksk "KSK" &&
+echo -n "LINE: ${LINENO} " && expect list-*-zsk "ZSK" &&
+echo -n "LINE: ${LINENO} " && expect list-*-ksk "KSK" &&
+
+# key list --verbose may not contain keys in state generate
+echo -n "LINE: ${LINENO} " && ! expect list-*-verbose "generate" &&
+# key list --verbose may not contain keys in state dead
+echo -n "LINE: ${LINENO} " && ! expect list-*-verbose "dead" &&
+
+# key list --verbose --all could contain keys in state generate
+# TODO without pre-generation of keys it is currently not possible to guarantee
+#      that a key in state generate will exist, otherwise we could check it with:
+# echo -n "LINE: ${LINENO} " && expect list-*-verboseall "generate" &&
+
+# with --keystate generate --verbose we should not see keys in active state
+echo -n "LINE: ${LINENO} " && ! expect list-*-generateverbose "active" &&
+# TODO but possible in state generate, without pre-generation of keys it is
+#      currently not possible to guarantee that a key in state will exist
+#      otherwise we could check it with:
+# echo -n "LINE: ${LINENO} " && expect list-*-generateverbose "generate" &&
+
+# with --keystate active we should not see keys in publish state
+echo -n "LINE: ${LINENO} " && ! expect list-*-active "publish" &&
+
+# it is valid to use --verbose --keystate active --keytype ZSK
+echo -n "LINE: ${LINENO} " && expect list-*-activezskverbose "ZSK" &&
+echo -n "LINE: ${LINENO} " && expect list-*-activezskverbose "active" &&
+# this should not list KSK keys and show at least once a ZSK in active state
+# this should list only active, zsk keys
+echo -n "LINE: ${LINENO} " && ! expectbut list-*-activezskverbose "ZSK" &&
+echo -n "LINE: ${LINENO} " && ! expectbut list-*-activezskverbose "active" &&
 
 ods_ods-control_enforcer_stop &&
 
