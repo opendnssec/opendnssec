6ceb7296ee728831833624d16c52f86e31542379
Yuri Schaeffer <yuri@nlnetlabs.nl>
2015-11-02 15:04:20 +0100

    free key states

--- enforcer/src/enforcer/enforcer.c
+++ enforcer/src/enforcer/enforcer.c
@@ -2741,19 +2741,26 @@ removeDeadKeys(db_connection_t *dbconn, key_data_t** keylist,
         if (key_purgable) {
 			/* key is purgable, is it time yet? */
             if (now >= key_time) {
+                key_state_t* ks_ds = key_data_get_cached_ds(keylist[i]);
+                key_state_t* ks_dk = key_data_get_cached_dnskey(keylist[i]);
+                key_state_t* ks_rd = key_data_get_cached_rrsigdnskey(keylist[i]);
+                key_state_t* ks_rs = key_data_get_cached_rrsig(keylist[i]);
+
                 ods_log_info("[%s] %s deleting key: %s", module_str, scmd,
                     hsm_key_locator(key_data_cached_hsm_key(keylist[i])));
 
-                if (key_state_delete(key_data_get_cached_ds(keylist[i]))
-                    || key_state_delete(key_data_get_cached_dnskey(keylist[i]))
-                    || key_state_delete(key_data_get_cached_rrsigdnskey(keylist[i]))
-                    || key_state_delete(key_data_get_cached_rrsig(keylist[i]))
+                if (   key_state_delete(ks_ds) || key_state_delete(ks_dk)
+                    || key_state_delete(ks_rd) || key_state_delete(ks_rs)
                     || key_data_delete(keylist[i])
                     || hsm_key_factory_release_key_id(hsm_key_id(key_data_cached_hsm_key(keylist[i])), dbconn))
                 {
                     /* TODO: better log error */
                     ods_log_error("[%s] %s: key_state_delete() || key_data_delete() || hsm_key_factory_release_key() failed", module_str, scmd);
                 }
+                key_state_free(ks_ds);
+                key_state_free(ks_dk);
+                key_state_free(ks_rd);
+                key_state_free(ks_rs);
             } else {
                 minTime(key_time, &first_purge);
             }
