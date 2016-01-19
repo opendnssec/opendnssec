12bfc05245a9a972a319424e8ba53ade6ff07db8
halderen <berry@nlnetlabs.nl>
2016-01-19 15:17:38 +0100

    OPENDNSSEC-721: migration in jenkins/build environment for softhsm1 to softhsm2 requires a softhsm2 based installation, however with softhsm1 build scripts.  Just make a small script which puts things right.

---
+++ testing/make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e 
+
+if [ \! -f $INSTALL_ROOT/.botan.ok ] ; then
+  rm -f Botan-1.10.10.tgz
+  wget 'http://botan.randombit.net/releases/Botan-1.10.10.tgz'
+  gzip -d < Botan-1.10.10.tgz | tar xf -
+  cd Botan-1.10.10
+  ./configure.py --prefix="$INSTALL_ROOT"
+  make
+  make install
+  cd ..
+  touch $INSTALL_ROOT/.botan.ok
+  echo "1.10.10" > $INSTALL_ROOT/.botan.build
+fi
+
+if [ \! -f $INSTALL_ROOT/.softhsm2.ok ] ; then
+  rm -f softhsm-2.0.0.tar.gz
+  wget 'https://dist.opendnssec.org/source/softhsm-2.0.0.tar.gz'
+  gzip -d < softhsm-2.0.0.tar.gz | tar xf -
+  cd softhsm-2.0.0
+  ./configure --prefix="$INSTALL_ROOT" \
+              --disable-non-paged-memory \
+              --with-migrate \
+              --with-crypto-backend=botan \
+              --with-botan="$INSTALL_ROOT"
+  make
+  make install
+  cp "src/lib/common/softhsm2.conf" "$INSTALL_ROOT/etc/softhsm2.conf.build"
+  touch $INSTALL_ROOT/.softhsm2.ok
+  echo "2.0.0" > $INSTALL_ROOT/.softhsm2.build
+fi
+
+exit 0
