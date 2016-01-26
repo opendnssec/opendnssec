5ed955aa0208f334ffca64868d1af2d4d7385ff9
halderen <berry@nlnetlabs.nl>
2016-01-26 08:01:02 +0100

    OPENDNSSEC-721: switch to OpenSSL rather than Botan based SoftHSM. The performance of Botan is very bad.

--- testing/make.sh
+++ testing/make.sh
@@ -6,30 +6,34 @@ if [ -n "$INSTALL_TAG" -a -z "$INSTALL_ROOT" ]; then
   INSTALL_ROOT="$WORKSPACE_ROOT/root/$INSTALL_TAG"
 fi
 
-if [ \! -f $INSTALL_ROOT/.botan.ok ] ; then
-  rm -f Botan-1.10.10.tgz
-  wget 'http://botan.randombit.net/releases/Botan-1.10.10.tgz'
-  gzip -d < Botan-1.10.10.tgz | tar xf -
-  cd Botan-1.10.10
-  ./configure.py --prefix="$INSTALL_ROOT"
-  make
-  make install
-  cd ..
-  touch $INSTALL_ROOT/.botan.ok
-  echo "1.10.10" > $INSTALL_ROOT/.botan.build
-fi
+# if [ \! -f $INSTALL_ROOT/.botan.ok ] ; then
+#   rm -f Botan-1.10.10.tgz
+#   wget 'http://botan.randombit.net/releases/Botan-1.10.10.tgz'
+#   gzip -d < Botan-1.10.10.tgz | tar xf -
+#   cd Botan-1.10.10
+#   ./configure.py --prefix="$INSTALL_ROOT"
+#   make
+#   make install
+#   cd ..
+#   touch $INSTALL_ROOT/.botan.ok
+#   echo "1.10.10" > $INSTALL_ROOT/.botan.build
+# fi
 
 if [ \! -f $INSTALL_ROOT/.softhsm2.ok ] ; then
   rm -f softhsm-2.0.0.tar.gz
   wget 'https://dist.opendnssec.org/source/softhsm-2.0.0.tar.gz'
   gzip -d < softhsm-2.0.0.tar.gz | tar xf -
   cd softhsm-2.0.0
+  # ./configure --prefix="$INSTALL_ROOT" \
+  #             --disable-non-paged-memory \
+  #             --with-migrate \
+  #             --with-crypto-backend=botan \
+  #             --disable-ecc --disable-gost \
+  #             --with-botan="$INSTALL_ROOT"
   ./configure --prefix="$INSTALL_ROOT" \
               --disable-non-paged-memory \
               --with-migrate \
-              --with-crypto-backend=botan \
-              --disable-ecc --disable-gost \
-              --with-botan="$INSTALL_ROOT"
+              --with-crypto-backend=openssl
   make
   make install
   cp "src/lib/common/softhsm2.conf" "$INSTALL_ROOT/etc/softhsm2.conf.build"
