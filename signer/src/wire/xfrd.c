e724cc1f14c10ec55083734d21e1a31330d69e52
halderen <berry@nlnetlabs.nl>
2016-03-01 13:39:40 +0100

    In case a XFR connection could not be opened, this log message results in a core dump because when the addns has just been changed, it uses the wrong structure to output additional information.

--- signer/src/wire/xfrd.c
+++ signer/src/wire/xfrd.c
@@ -1364,7 +1364,7 @@ xfrd_tcp_open(xfrd_type* xfrd, tcp_set_type* set)
     }
     if (fcntl(fd, F_SETFL, O_NONBLOCK) == -1) {
         ods_log_error("[%s] zone %s cannot fcntl tcp socket to %s: %s",
-            xfrd_str, zone->name, xfrd->master->address, strerror(errno));
+            xfrd_str, zone->name, ((dnsin_type*)(xfrd->zone->adinbound->inbound))->request_xfr->address, strerror(errno));
         xfrd_set_timer_now(xfrd);
         xfrd_tcp_release(xfrd, set, 0);
         return 0;
