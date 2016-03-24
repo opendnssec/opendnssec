e132ec02133fb190335d33ee62de58a86e15715b
Hoda Rohani <hoda@nlnetlabs.nl>
2016-03-24 12:03:59 +0100

    Implementing 'ods-enforcer key import' command
    Conflicts:
    	enforcer/src/enforcer/enforcer.c

--- enforcer/src/enforcer/enforcer.c
+++ enforcer/src/enforcer/enforcer.c
@@ -2585,10 +2585,9 @@ updatePolicy(engine_type *engine, db_connection_t *dbconn, policy_t *policy,
 		tag = keytag(hsm_key_locator(hsmkey), hsm_key_algorithm(hsmkey),
 			((hsm_key_role(hsmkey) == HSM_KEY_ROLE_KSK
 				|| hsm_key_role(hsmkey) == HSM_KEY_ROLE_CSK)
-				? 1 : 0),
-			&success);
-		if (!success
-			|| key_data_set_keytag(mutkey, tag))
+				? 1 : 0), &success);
+		if (success || key_data_set_keytag(mutkey, tag))
+>>>>>>> 1d48509... Implementing 'ods-enforcer key import' command
 		{
 			/* TODO: better log error */
 			ods_log_error("[%s] %s: error keytag", module_str, scmd);
