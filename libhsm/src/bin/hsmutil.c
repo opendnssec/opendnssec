c4ede097aeca42de7a1e7034144e0007e205f620
halderen <berry@nlnetlabs.nl>
2016-04-05 11:45:48 +0200

    Slightly different/missing output caused unit tests to fail.

--- libhsm/src/bin/hsmutil.c
+++ libhsm/src/bin/hsmutil.c
@@ -594,8 +594,12 @@ main (int argc, char *argv[])
     }
 
     result = hsm_open2(parse_conf_repositories(config?config:HSM_DEFAULT_CONFIG), hsm_prompt_pin);
-    if (result) {
-        hsm_print_error(NULL);
+    if (result != HSM_OK) {
+        char* error =  hsm_get_error(NULL);
+        if (error != NULL) {
+            fprintf(stderr,"%s\n", error);
+            free(error);
+        }
         exit(-1);
     }
     ctx = hsm_create_context();
