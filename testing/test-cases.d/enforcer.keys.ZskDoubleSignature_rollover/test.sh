9bea7418443866b1f3b10066e6a699f5b2b3970c
Hoda Rohani <hoda@nlnetlabs.nl>
2016-07-22 15:34:52 +0200

    Create test for Double Signature ZSK Rollover

--- testing/test-cases.d/enforcer.keys.ZskDoubleSignature_rollover/test.sh
+++ testing/test-cases.d/enforcer.keys.ZskDoubleSignature_rollover/test.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-#TEST: ZSK Rollover - Pre-Publication Mechanism
-
+#TEST: ZSK Rollover - Double Signature Mechanism
 
 ods_signer_start () {
-	rm -f "$INSTALL_ROOT/var/opendnssec/signer/ods.backup2"
-        rm -f "$INSTALL_ROOT/var/opendnssec/signed/ods"
+        rm -f "$INSTALL_ROOT/var/opendnssec/signer/ods.backup2" 
+        rm -f "$INSTALL_ROOT/var/opendnssec/signed/ods" 
 
-	ods-signer start &&
-	sleep 5
+        ods-signer start &&
+        sleep 5
 }
 
 case "$DISTRIBUTION" in
@@ -21,7 +20,8 @@ if [ -n "$HAVE_MYSQL" ]; then
         ods_setup_conf conf.xml conf-mysql.xml
 fi &&
 
-ods_reset_env && 
+ods_reset_env &&
+
 echo &&
 echo "#################### START AND LEAP TIME #################### " &&
 echo -n "LINE: ${LINENO} " && ods-enforcer start && sleep 1 &&
@@ -35,7 +35,7 @@ echo -n "LINE: ${LINENO} " && ods-enforcer time leap && sleep 1 &&
 
 echo &&
 echo "########### VERIFY SIGNATURES IN THE SIGNED FILE ############ " &&
-echo -n "LINE: ${LINENO} " && ods_signer_start && 
+echo -n "LINE: ${LINENO} " && ods_signer_start &&
 
 echo -n "LINE: ${LINENO} " && syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
 echo -n "LINE: ${LINENO} " && test -f "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
@@ -47,30 +47,28 @@ echo -n "LINE: ${LINENO} " && ldns-verify-zone "$INSTALL_ROOT/var/opendnssec/sig
 echo -n "LINE: ${LINENO} " && ods-signer stop && sleep 4 &&
 
 echo &&
-echo "############## ROLL ZSK: PRE-PUBLICATION METHOD ############## " &&
+echo "############## ROLL ZSK: DOUBLE-SIGNATURE METHOD ############## " &&
 echo -n "LINE: ${LINENO} " && ods-enforcer key rollover -z ods --keytype zsk && sleep 1 &&
 
-# In the PrePublication mechanism, DNSKEY is published before the RRSIG
-# Check DNSKEY must be rumoured while RRSIG is hidden, also Pub is 1 and Act is 0 which means
-# before using the new key for signign, DNSKEY must be published
-echo -n "LINE: ${LINENO} " && ods-enforcer key list -d -p --all &&
-echo -n "LINE: ${LINENO} " && ZSK2=`ods-enforcer key list -d -p | grep "ZSK" | grep "rumoured;NA;hidden;1;0"| cut -d ";" -f9` &&
+# in Double Signature mechanism, DNSKEY and RRSIG are published at the same time,
+# also the Pub and Act must be 1 which means the key is published and used for signing.
+echo -n "LINE: ${LINENO} " && ods-enforcer key list -v -p --all &&
+echo -n "LINE: ${LINENO} " && ZSK2=`ods-enforcer key list -d -p | grep "ZSK" | grep "rumoured;NA;rumoured;1;1"| cut -d ";" -f9` &&
 
 echo &&
 echo "############# CHECK SIGNATURES AFTER ROLLOVER-1 ############# " &&
 echo -n "LINE: ${LINENO} " && ods_signer_start &&
-# There must be 2 ZSKs
+
+# There must be two ZSKs 
 echo -n "LINE: ${LINENO} " && count=`grep -c "DNSKEY[[:space:]]*256" "$INSTALL_ROOT/var/opendnssec/signed/ods"` &&
 echo -n "LINE: ${LINENO} " && [ $count -eq 2 ] &&
 
-# There must be one signature signed with the old zsk
-echo -n "LINE: ${LINENO} " && count=`grep -c "RRSIG[[:space:]]*MX" "$INSTALL_ROOT/var/opendnssec/signed/ods" ` &&
-echo -n "LINE: ${LINENO} " && [ $count -eq 1 ] &&
-
-echo -n "LINE: ${LINENO} " && grep "RRSIG[[:space:]]*MX" "$INSTALL_ROOT/var/opendnssec/signer/ods.backup2" | grep -v $ZSK2 | grep $ZSK1 &&
+# There must be two signature for resource records except for DNSKEY
+echo -n "LINE: ${LINENO} " && count=`grep -c "RRSIG[[:space:]]*MX" "$INSTALL_ROOT/var/opendnssec/signed/ods"` &&
+echo -n "LINE: ${LINENO} " && [ $count -eq 2 ] &&
 
 echo -n "LINE: ${LINENO} " && ldns-verify-zone "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
-echo -n "LINE: ${LINENO} " && ods-signer stop &&
+echo -n "LINE: ${LINENO} " && ods-signer stop && sleep 4 &&
 
 echo &&
 echo "############# CHECK SIGNATURES AFTER ROLLOVER-2 ############# " &&
@@ -79,15 +77,15 @@ echo -n "LINE: ${LINENO} " && ods-enforcer time leap && sleep 1 &&
 
 echo -n "LINE: ${LINENO} " && ods_signer_start &&
 echo -n "LINE: ${LINENO} " && count=`grep -c "RRSIG[[:space:]]*MX" "$INSTALL_ROOT/var/opendnssec/signed/ods"` &&
-echo -n "LINE: ${LINENO} " && [ $count -eq 1 ] &&
+echo -n "LINE: ${LINENO} " && [ $count -eq 2 ] &&
 
-echo -n "LINE: ${LINENO} " && grep "RRSIG[[:space:]]*MX" "$INSTALL_ROOT/var/opendnssec/signer/ods.backup2"| grep -v $ZSK1 | grep $ZSK2 &&
-echo -n "LINE: ${LINENO} " && ldns-verify-zone "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
+echo -n "LINE: ${LINENO} " && grep "RRSIG[[:space:]]*MX" "$INSTALL_ROOT/var/opendnssec/signer/ods.backup2" | grep $ZSK2 &&
+echo -n "LINE: ${LINENO} " && grep "RRSIG[[:space:]]*MX" "$INSTALL_ROOT/var/opendnssec/signer/ods.backup2" | grep $ZSK1 &&
 
 echo &&
 echo "############################ STOP ############################ " &&
 echo -n "LINE: ${LINENO} " && ods_stop_ods-control &&
 return 0
- 
+
 ods_kill
 return 1
