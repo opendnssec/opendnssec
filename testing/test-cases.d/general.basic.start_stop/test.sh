64662064daaa30eb681c00c4276b996e74ba296b
Hoda Rohani <hoda@nlnetlabs.nl>
2016-08-30 09:34:34 +0200

    enforcer client sleeps some seconds before returning back

--- testing/test-cases.d/general.basic.start_stop/test.sh
+++ testing/test-cases.d/general.basic.start_stop/test.sh
@@ -7,6 +7,12 @@ ods_reset_env &&
 ods_start_ods-control &&
 ods_stop_ods-control &&
 
+ods-enforcer start &&
+ods-enforcer zone add -z ods &&
+ods-enforcer stop &&
+ods-enforcer start &&
+ods-enforcer stop &&
+
 return 0
 
 ods_kill
