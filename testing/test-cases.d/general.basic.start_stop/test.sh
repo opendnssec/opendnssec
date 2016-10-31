c4806fe1d175fe3fa0b03e44d5dc9d7044fc8299
Hoda Rohani <hoda@nlnetlabs.nl>
2016-10-31 05:23:04 +0100

    print log file

--- testing/test-cases.d/general.basic.start_stop/test.sh
+++ testing/test-cases.d/general.basic.start_stop/test.sh
@@ -9,13 +9,22 @@ ods_stop_ods-control &&
 
 log_this ods-enforcer-start ods-enforcer start &&
 log_this ods-enforcer-stop ods-enforcer stop &&
+log_this ods-enforcer-start ods-enforcer start &&
 
 log_this ods-signer-start ods-signer start &&
 log_this ods-signer-stop ods-signer stop &&
+log_this ods-signer-start ods-signer start &&
 
+log_this ods-control-stop ods-control stop &&
 log_this ods-control-start ods-control start &&
 log_this ods-control-stop ods-control stop &&
 return 0
 
+cat _log.$BUILD_TAG.ods-enforcer-start.stdout
+cat _log.$BUILD_TAG.ods-enforcer-start.stderr
+cat _log.$BUILD_TAG.ods-signer-start.stdout
+cat _log.$BUILD_TAG.ods-signer-start.stderr
+cat _log.$BUILD_TAG.ods-control-start.stdout
+cat _log.$BUILD_TAG.ods-control-start.stderr
 ods_kill
 return 1
