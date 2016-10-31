9e417369a9e35d8cfe3d62fa28bef3ae99257cf6
Hoda Rohani <hoda@nlnetlabs.nl>
2016-10-31 04:41:52 +0100

    test start/stop

--- testing/test-cases.d/general.basic.start_stop/test.sh
+++ testing/test-cases.d/general.basic.start_stop/test.sh
@@ -7,6 +7,14 @@ ods_reset_env &&
 ods_start_ods-control &&
 ods_stop_ods-control &&
 
+log_this ods-enforcer-start ods-enforcer start &&
+log_this ods-enforcer-stop ods-enforcer stop &&
+
+log_this ods-signer-start ods-signer start &&
+log_this ods-signer-stop ods-signer stop &&
+
+log_this ods-control-start ods-control start &&
+log_this ods-control-stop ods-control stop &&
 return 0
 
 ods_kill
