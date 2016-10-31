ceea6fec7bf6c6120e60e4221e7bf5e25e45de33
halderen <berry@nlnetlabs.nl>
2016-10-31 10:59:14 +0100

    test

--- testing/test-cases.d/enforcer.ods-enforcer.zones_and_zonelist/test.sh
+++ testing/test-cases.d/enforcer.ods-enforcer.zones_and_zonelist/test.sh
@@ -292,6 +292,9 @@ echo "zones.xml contents OK" &&
 
 # #Finally run the signer to check all is well
 ods_start_signer 10 &&
+# Extra sleep to make sure signer is really started and able to receive
+# signconf command
+sleep 10 &&
 log_this ods-enforcer-signconf ods-enforcer signconf && 
 # The signconf will now be separate per zone, where there will now be a delay
 # where zone ods11 does not get removed because it is still busy
