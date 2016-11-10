06ab507150cd536559b529d9eb4708f26fe268e9
halderen <berry@nlnetlabs.nl>
2016-11-10 18:52:26 +0100

    OPENDNSSEC-855: reimplemented flush.

--- common/scheduler/schedule.h
+++ common/scheduler/schedule.h
@@ -66,7 +66,6 @@ struct schedule_struct {
     /* For testing. So we can verify al workers are waiting and nothing
      * is to be done. Used by enforcer_idle. */
     int num_waiting;
-    int flushcount;
 };
 
 /**
