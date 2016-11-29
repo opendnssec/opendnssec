ee85bb1a788f5aea0836fd800d8605865efcde89
halderen <berry@nlnetlabs.nl>
2016-11-29 20:04:15 +0100

    Give more insight in failing test.

--- testing/test-cases.d/general.basic.start_stop/test.sh
+++ testing/test-cases.d/general.basic.start_stop/test.sh
@@ -2,32 +2,25 @@
 
 #TEST: Start and stop using ods-control and default conf files. Check the deamons behave.
 
-ods_reset_env &&
+echo -n "LINE: ${LINENO} " && ods_reset_env &&
 
-ods_start_ods-control &&
-ods_stop_ods-control &&
+echo -n "LINE: ${LINENO} " && ods_start_ods-control &&
+echo -n "LINE: ${LINENO} " && ods_stop_ods-control &&
 
-log_this ods-enforcer-start ods-enforcer start &&
-sleep 5 &&
-log_this ods-enforcer-stop ods-enforcer stop &&
-sleep 5 &&
-log_this ods-enforcer-start ods-enforcer start &&
-sleep 5 &&
+echo -n "LINE: ${LINENO} " && log_this ods-enforcer-start ods-enforcer start &&
+echo -n "LINE: ${LINENO} " && log_this ods-enforcer-stop ods-enforcer stop &&
+echo -n "LINE: ${LINENO} " && log_this ods-enforcer-start ods-enforcer start &&
 
-log_this ods-signer-start ods-signer start &&
-sleep 5 &&
-log_this ods-signer-stop ods-signer stop &&
-sleep 5 &&
-log_this ods-signer-start ods-signer start &&
-sleep 5 &&
+echo -n "LINE: ${LINENO} " && log_this ods-signer-start ods-signer start &&
+echo -n "LINE: ${LINENO} " && log_this ods-signer-stop ods-signer stop &&
+echo -n "LINE: ${LINENO} " && log_this ods-signer-start ods-signer start &&
 
-log_this ods-control-stop ods-control stop &&
-sleep 5 &&
-log_this ods-control-start ods-control start &&
-sleep 5 &&
-log_this ods-control-stop ods-control stop &&
+echo -n "LINE: ${LINENO} " && log_this ods-control-stop ods-control stop &&
+echo -n "LINE: ${LINENO} " && log_this ods-control-start ods-control start &&
+echo -n "LINE: ${LINENO} " && log_this ods-control-stop ods-control stop &&
 return 0
 
+cat _*
 #cat _log.$BUILD_TAG.ods-enforcer-start.stdout
 #cat _log.$BUILD_TAG.ods-enforcer-start.stderr
 #cat _log.$BUILD_TAG.ods-signer-start.stdout
