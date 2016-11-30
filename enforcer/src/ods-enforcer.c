c5a1eea06f9de3b54544c2d4215fb70d606e38b2
Yuri Schaeffer <yuri@nlnetlabs.nl>
2016-11-30 11:40:22 +0100

    Don't return non null exit code when daemon already stopped

--- enforcer/src/ods-enforcer.c
+++ enforcer/src/ods-enforcer.c
@@ -353,11 +353,11 @@ interface_start(const char* cmd, const char* servsock_filename)
         (strlen(userbuf) != 0 && !strncmp(userbuf, "stop", 4))) {
         char line[80];
         FILE *cmd2 = popen("pgrep ods-enforcerd","r");
+        error = 0;
         if (fgets(line, 80, cmd2)) {
             pid_t pid = strtoul(line, NULL, 10);
             fprintf(stdout, "pid %d\n", pid);
             int time = 0;
-            error = 0;
             while (pid > 0) {
                if(kill(pid, 0) != 0) break;
                sleep(1);
