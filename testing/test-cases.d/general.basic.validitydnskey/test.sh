8306ed1dbb01316f480bd08e2aa2d911576c1126
Yuri Schaeffer <yuri@nlnetlabs.nl>
2017-01-23 15:26:23 +0100

    Test fails if enforcer generated a signconf in the same second as the signer consumed the last.

--- testing/test-cases.d/general.basic.validitydnskey/test.sh
+++ testing/test-cases.d/general.basic.validitydnskey/test.sh
@@ -45,6 +45,7 @@ echo -n "LINE: ${LINENO} " && ods_start_ods-control &&
 
 echo -n "LINE: ${LINENO} " && echo "verifying without keyset validity set" &&
 echo -n "LINE: ${LINENO} " && ods-enforcer zone add -z ods -p plainkeysetvalidity &&
+echo -n "LINE: ${LINENO} " && sleep 1 &&
 echo -n "LINE: ${LINENO} " && ods-enforcer time leap --attach &&
 echo -n "LINE: ${LINENO} " && syslog_waitfor_count 60 1 'ods-signerd: .*\[STATS\] ods' &&
 echo -n "LINE: ${LINENO} " && echo "  there should be no keyset entry in signconf" &&
@@ -57,6 +58,7 @@ echo -n "LINE: ${LINENO} " && rm -f "$INSTALL_ROOT/var/opendnssec/signed/ods" &&
 
 echo -n "LINE: ${LINENO} " && echo "verifying with keyset validity explicitly set" &&
 echo -n "LINE: ${LINENO} " && ods-enforcer zone add -z ods -p explicitkeysetvalidity &&
+echo -n "LINE: ${LINENO} " && sleep 1 &&
 echo -n "LINE: ${LINENO} " && ods-enforcer time leap --attach &&
 echo -n "LINE: ${LINENO} " && ods-enforcer queue &&
 echo -n "LINE: ${LINENO} " && syslog_waitfor_count 60 2 'ods-signerd: .*\[STATS\] ods' &&
