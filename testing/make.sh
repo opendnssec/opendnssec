e4d552335eb605dc0a0c4a46a4f39469c741eb6a
halderen <berry@nlnetlabs.nl>
2017-01-30 08:47:59 +0100

    Upgrade to SoftHSM 2.2.0

--- testing/make.sh
+++ testing/make.sh
@@ -6,39 +6,35 @@ if [ -n "$INSTALL_TAG" -a -z "$INSTALL_ROOT" ]; then
   INSTALL_ROOT="$WORKSPACE_ROOT/root/$INSTALL_TAG"
 fi
 
-# if [ \! -f $INSTALL_ROOT/.botan.ok ] ; then
-#   rm -f Botan-1.10.10.tgz
-#   wget 'http://botan.randombit.net/releases/Botan-1.10.10.tgz'
-#   gzip -d < Botan-1.10.10.tgz | tar xf -
-#   cd Botan-1.10.10
-#   ./configure.py --prefix="$INSTALL_ROOT"
-#   make
-#   make install
-#   cd ..
-#   touch $INSTALL_ROOT/.botan.ok
-#   echo "1.10.10" > $INSTALL_ROOT/.botan.build
-# fi
+if [ \! -f $INSTALL_ROOT/.botan.ok ] ; then
+  rm -f Botan-1.10.13.tgz
+  wget 'https://botan.randombit.net/releases/Botan-1.10.13.tgz'
+  gzip -d < Botan-1.10.13.tgz | tar xf -
+  cd Botan-1.10.13
+  ./configure.py --prefix="$INSTALL_ROOT"
+  make
+  make install
+  cd ..
+  touch $INSTALL_ROOT/.botan.ok
+  echo "1.10.13" > $INSTALL_ROOT/.botan.build
+fi
 
 if [ \! -f $INSTALL_ROOT/.softhsm2.ok ] ; then
-  rm -f softhsm-2.1.0.tar.gz
-  wget 'https://dist.opendnssec.org/source/softhsm-2.1.0.tar.gz'
-  gzip -d < softhsm-2.1.0.tar.gz | tar xf -
-  cd softhsm-2.1.0
-  # ./configure --prefix="$INSTALL_ROOT" \
-  #             --disable-non-paged-memory \
-  #             --with-migrate \
-  #             --with-crypto-backend=botan \
-  #             --disable-ecc --disable-gost \
-  #             --with-botan="$INSTALL_ROOT"
+  rm -f softhsm-2.2.0.tar.gz
+  wget 'https://dist.opendnssec.org/source/softhsm-2.2.0.tar.gz'
+  gzip -d < softhsm-2.2.0.tar.gz | tar xf -
+  cd softhsm-2.2.0
   ./configure --prefix="$INSTALL_ROOT" \
               --disable-non-paged-memory \
-              --with-migrate --disable-gost \
-              --with-crypto-backend=openssl
+              --with-migrate --disable-p11-kit \
+              --with-crypto-backend=botan \
+              --disable-ecc --disable-gost \
+              --with-botan="$INSTALL_ROOT"
   make
   make install
   cp "src/lib/common/softhsm2.conf" "$INSTALL_ROOT/etc/softhsm2.conf.build"
   touch $INSTALL_ROOT/.softhsm2.ok
-  echo "2.1.0" > $INSTALL_ROOT/.softhsm2.build
+  echo "2.2.0" > $INSTALL_ROOT/.softhsm2.build
 fi
 
 exit 0
