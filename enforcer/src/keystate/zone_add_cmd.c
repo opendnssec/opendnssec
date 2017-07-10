9a4432aecf6ae65b7bb2e5f4402518eb24de9cfb
Yuri Schaeffer <yuri@nlnetlabs.nl>
2017-07-10 16:17:04 +0200

    commit generated keys to database

--- enforcer/src/keystate/zone_add_cmd.c
+++ enforcer/src/keystate/zone_add_cmd.c
@@ -334,6 +334,7 @@ run(int sockfd, cmdhandler_ctx_type* context, const char *cmd)
      * On successful generate HSM keys and add/flush enforce task.
      */
     (void)hsm_key_factory_generate_policy(engine, db, policy, 0);
+    (void)dbw_commit(db);
     ods_log_debug("[%s] Flushing enforce task", module_str);
     (void)schedule_task(engine->taskq, enforce_task(engine, zone->name), 1, 0);
 
