508f64b269caa4ca2b2b2b0392d0c604a8130add
halderen <berry@nlnetlabs.nl>
2019-03-18 10:33:26 +0100

    - drop unused VERSION magic stuff. - on new aclocal versions, some m4 scripts aren't found.  Run aclocal   explicitly to capture m4 scripts.  This new method seems to be   the advocated way to call the autoconf/automake programs.

--- autogen.sh
+++ autogen.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
-#
-VERSION=version.m4
 
-if [ `dirname $0` = ".." ]; then
-	if [ -f ../${VERSION} ]; then
-		echo Creating ${VERSION} &&
-		rm -f ${VERSION} &&
-		ln ../${VERSION} ${VERSION} 2>/dev/null ||
-		ln -s ../${VERSION} ${VERSION} 2>/dev/null ||
-		cp ../${VERSION} ${VERSION}
-	fi
-fi &&
+case `uname` in Darwin*) glibtoolize --copy ;;
+  *) libtoolize --copy ;; esac
 
-echo "Running autoreconf" &&
-autoreconf --install --force
+aclocal -I m4 --install
+autoheader
+autoconf
+automake --foreign --add-missing --force-missing --copy
