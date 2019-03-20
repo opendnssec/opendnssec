91d74edaa159149fae685e7681911b78e3634a66
ximon18 <3304436+ximon18@users.noreply.github.com>
2019-03-20 11:46:06 +0100

    Removed output timestamping and colouring.

--- testing/test-cases.d/signer.support_229/test.sh
+++ testing/test-cases.d/signer.support_229/test.sh
@@ -19,77 +19,72 @@ THIRTY_ONE_DAYS_IN_SECONDS=$((31*24*60*60))
 LIGHT_GREEN='\033[0;92m'
 NO_COLOUR='\033[0m'
 
-# helper functions
-l() {
-    echo -en "${LIGHT_GREEN}$(date -u +'%b %d %H:%M:%S') UTC: LINE $1: ${NO_COLOUR}"
-}
-
 # entrypoint
 [ -n "$HAVE_MYSQL" ] && ods_setup_conf conf.xml conf-mysql.xml
 
 # ensure we start from a clean setup
-l $LINENO && ods_reset_env &&
+ods_reset_env &&
 
 # start both enforcer and signer
-l $LINENO && ods_start_ods-control &&
+ods_start_ods-control &&
 
 #########################################################################
 # wait for creation and signing of a single simple signed zone
 # assumes that the input zone file is present in test subdir unsigned/example.com
 # and is mentioned in file zonelist.xml in the test dir
-l $LINENO && syslog_waitfor 5 'ods-signerd: .*\[STATS\] example.com' &&
-l $LINENO && test -f "${SIGNED_ZONES}/example.com" &&
+syslog_waitfor 5 'ods-signerd: .*\[STATS\] example.com' &&
+test -f "${SIGNED_ZONES}/example.com" &&
 #########################################################################
 
 #########################################################################
 # check that DNSKEY "id" and RRSIG KEYTAG values (11324 and 52667 here) match, otherwise the zone is broken
 # example.com.     3600   IN      DNSKEY  256 3 7 <BASE64> ;{id = 11324 (zsk), size = 1024b}
 # example.com.    86400   IN      RRSIG   A 7 2 [0-9]+ 20190403012345 20190312213351 52667 example.com. <BASE64>
-l $LINENO && log_this cat-example.com-before cat "${SIGNED_ZONES}/example.com" &&
-l $LINENO && log_this key-status-before ods-enforcer key list --all --verbose &&
-l $LINENO && echo -n "Capturing RRSIG A count.." && RRSIG_A_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${RRSIG_A_COUNT}" &&
-l $LINENO && echo "Checking RRSIG A count ${RRSIG_A_COUNT} == 1.." && [ "${RRSIG_A_COUNT}" -eq "1" ] &&
-l $LINENO && echo -n "Capturing ZSK count.." && ZSK_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${ZSK_COUNT}" &&
-l $LINENO && echo "Checking ZSK count ${ZSK_COUNT} == 1.." && [ "${ZSK_COUNT}" -eq "1" ] &&
-l $LINENO && echo -n "Capturing ZSK KEYTAG (id).." && DNSKEYID=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | grep -Eo 'id = [0-9]+ \(zsk\)' | grep -Eo '[0-9]+') && echo " ${DNSKEYID}" &&
-l $LINENO && echo -n "Capturing RRSIG A KEYTAG.." && KEYTAG=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | awk '{print $11}') && echo " ${KEYTAG}" &&
-l $LINENO && echo -n "Capturing DNSKEY ${KEYTAG} status.." && ZSKSTATUS=$(ods-enforcer key list --all --verbose | awk '{print $2,$3,$10}' | grep ${KEYTAG}) && echo " ${ZSKSTATUS}" &&
-l $LINENO && echo "Checking ZSK ${KEYTAG} is ready.." && [ "${ZSKSTATUS}" == "ZSK ready ${KEYTAG}" ] &&
-l $LINENO && echo "Testing KEYTAG equality (DNSKEYID: $DNSKEYID == KEYTAG: $KEYTAG ?).." && [ "${DNSKEYID}" == "${KEYTAG}" ] &&
+log_this cat-example.com-before cat "${SIGNED_ZONES}/example.com" &&
+log_this key-status-before ods-enforcer key list --all --verbose &&
+echo -n "Capturing RRSIG A count.." && RRSIG_A_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${RRSIG_A_COUNT}" &&
+echo "Checking RRSIG A count ${RRSIG_A_COUNT} == 1.." && [ "${RRSIG_A_COUNT}" -eq "1" ] &&
+echo -n "Capturing ZSK count.." && ZSK_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${ZSK_COUNT}" &&
+echo "Checking ZSK count ${ZSK_COUNT} == 1.." && [ "${ZSK_COUNT}" -eq "1" ] &&
+echo -n "Capturing ZSK KEYTAG (id).." && DNSKEYID=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | grep -Eo 'id = [0-9]+ \(zsk\)' | grep -Eo '[0-9]+') && echo " ${DNSKEYID}" &&
+echo -n "Capturing RRSIG A KEYTAG.." && KEYTAG=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | awk '{print $11}') && echo " ${KEYTAG}" &&
+echo -n "Capturing DNSKEY ${KEYTAG} status.." && ZSKSTATUS=$(ods-enforcer key list --all --verbose | awk '{print $2,$3,$10}' | grep ${KEYTAG}) && echo " ${ZSKSTATUS}" &&
+echo "Checking ZSK ${KEYTAG} is ready.." && [ "${ZSKSTATUS}" == "ZSK ready ${KEYTAG}" ] &&
+echo "Testing KEYTAG equality (DNSKEYID: $DNSKEYID == KEYTAG: $KEYTAG ?).." && [ "${DNSKEYID}" == "${KEYTAG}" ] &&
 #########################################################################
 
 #########################################################################
 # leap to the time of the next key generation.
 # kasp.xml says the ZSK should be regenerated every 30 days.
-l $LINENO && log_this ods-enforcer-leap ods_enforcer_leap_over ${THIRTY_ONE_DAYS_IN_SECONDS} &&
+log_this ods-enforcer-leap ods_enforcer_leap_over ${THIRTY_ONE_DAYS_IN_SECONDS} &&
 #########################################################################
 
 #########################################################################
 # as above check for mismatched DNSKEY "id" and RRSIG.
 # if the bug is present this will fail.
-l $LINENO && log_this cat-example.com.xml-after cat "${SIGNCONF_FILE}" &&
-l $LINENO && log_this cat-example.com-after cat "${SIGNED_ZONES}/example.com" &&
-l $LINENO && log_this key-status-after ods-enforcer key list --all --verbose &&
-l $LINENO && echo -n "Capturing RRSIG A count.." && RRSIG_A_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${RRSIG_A_COUNT}" &&
-l $LINENO && echo "Checking RRSIG A count ${RRSIG_A_COUNT} == 1.." && [ "${RRSIG_A_COUNT}" -eq "1" ] &&
-l $LINENO && echo -n "Capturing ZSK count.." && ZSK_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${ZSK_COUNT}" &&
-l $LINENO && echo "Checking ZSK count ${ZSK_COUNT} == 1.." && [ "${ZSK_COUNT}" -eq "1" ] &&
-l $LINENO && echo -n "Capturing ZSK KEYTAG (id).." && DNSKEYID=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | grep -Eo 'id = [0-9]+ \(zsk\)' | grep -Eo '[0-9]+') && echo " ${DNSKEYID}" &&
-l $LINENO && echo -n "Capturing RRSIG A KEYTAG.." && KEYTAG=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | awk '{print $11}') && echo " ${KEYTAG}" &&
-l $LINENO && echo -n "Capturing DNSKEY ${DNSKEYID} status.." && ZSKDNSKEYSTATUS=$(ods-enforcer key list --all --verbose | awk '{print $2,$3,$10}' | grep ${DNSKEYID}) && echo " ${ZSKDNSKEYSTATUS}" &&
-l $LINENO && echo -n "Capturing DNSKEY ${KEYTAG} status.." && ZSKKEYTAGSTATUS=$(ods-enforcer key list --all --verbose | awk '{print $2,$3,$10}' | grep ${KEYTAG}) && echo " ${ZSKKEYTAGSTATUS}" &&
-l $LINENO && echo "Checking ZSK ${DNSKEYID} is active.." && [ "${ZSKDNSKEYSTATUS}" == "ZSK active ${DNSKEYID}" ] &&
-l $LINENO && echo "Checking ZSK ${KEYTAG} is retire.." && [ "${ZSKKEYTAGSTATUS}" == "ZSK retire ${KEYTAG}" ] &&
-l $LINENO && echo "Testing KEYTAG equality (DNSKEYID: $DNSKEYID == KEYTAG: $KEYTAG ?).." && [ "${DNSKEYID}" == "${KEYTAG}" ] &&
+log_this cat-example.com.xml-after cat "${SIGNCONF_FILE}" &&
+log_this cat-example.com-after cat "${SIGNED_ZONES}/example.com" &&
+log_this key-status-after ods-enforcer key list --all --verbose &&
+echo -n "Capturing RRSIG A count.." && RRSIG_A_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${RRSIG_A_COUNT}" &&
+echo "Checking RRSIG A count ${RRSIG_A_COUNT} == 1.." && [ "${RRSIG_A_COUNT}" -eq "1" ] &&
+echo -n "Capturing ZSK count.." && ZSK_COUNT=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | wc -l) && echo " ${ZSK_COUNT}" &&
+echo "Checking ZSK count ${ZSK_COUNT} == 1.." && [ "${ZSK_COUNT}" -eq "1" ] &&
+echo -n "Capturing ZSK KEYTAG (id).." && DNSKEYID=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+DNSKEY\s+256" "${SIGNED_ZONES}/example.com" | grep -Eo 'id = [0-9]+ \(zsk\)' | grep -Eo '[0-9]+') && echo " ${DNSKEYID}" &&
+echo -n "Capturing RRSIG A KEYTAG.." && KEYTAG=$(grep -E -- "^example.com.\s+[0-9]+\s+IN\s+RRSIG\s+A" "${SIGNED_ZONES}/example.com" | awk '{print $11}') && echo " ${KEYTAG}" &&
+echo -n "Capturing DNSKEY ${DNSKEYID} status.." && ZSKDNSKEYSTATUS=$(ods-enforcer key list --all --verbose | awk '{print $2,$3,$10}' | grep ${DNSKEYID}) && echo " ${ZSKDNSKEYSTATUS}" &&
+echo -n "Capturing DNSKEY ${KEYTAG} status.." && ZSKKEYTAGSTATUS=$(ods-enforcer key list --all --verbose | awk '{print $2,$3,$10}' | grep ${KEYTAG}) && echo " ${ZSKKEYTAGSTATUS}" &&
+echo "Checking ZSK ${DNSKEYID} is active.." && [ "${ZSKDNSKEYSTATUS}" == "ZSK active ${DNSKEYID}" ] &&
+echo "Checking ZSK ${KEYTAG} is retire.." && [ "${ZSKKEYTAGSTATUS}" == "ZSK retire ${KEYTAG}" ] &&
+echo "Testing KEYTAG equality (DNSKEYID: $DNSKEYID == KEYTAG: $KEYTAG ?).." && [ "${DNSKEYID}" == "${KEYTAG}" ] &&
 #########################################################################
 
 # Shutdown
-l $LINENO && ods_stop_ods-control &&
+ods_stop_ods-control &&
 
 # NOTE: You can change this to "return 1" to prevent testdir/_xxx log files being cleaned up
 return 0
 
 # One of the statements above failed, abort.
-l $LINENO && echo '*********** ERROR **********'
-l $LINENO && ods_kill
+echo '*********** ERROR **********'
+ods_kill
 return 1
