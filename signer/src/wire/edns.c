043e7c9ffa7548cbe0112b55997dd653a2197346
halderen <berry@nlnetlabs.nl>
2019-11-04 14:47:50 +0100

    SUPPORT-242: Skip over EDNS cookie option (thanks to Havard Eidnes and Ulrich-Lorenz Schlueter).

--- signer/src/wire/edns.c
+++ signer/src/wire/edns.c
@@ -112,6 +112,7 @@ edns_rr_parse(edns_rr_type* err, buffer_type* buffer)
     uint16_t opt_class;
     uint8_t  opt_version;
     uint16_t opt_flags;
+    uint16_t opt_rdlen;
 
     if (!err || !buffer) {
         ods_log_debug("[%s] parse: no edns rr or no packet buffer available",
@@ -137,7 +138,8 @@ edns_rr_parse(edns_rr_type* err, buffer_type* buffer)
     (void)buffer_read_u8(buffer); /* opt_extended_rcode */
     opt_version = buffer_read_u8(buffer);
     opt_flags = buffer_read_u16(buffer);
-    (void)buffer_read_u16(buffer); /* opt_rdlen */
+    opt_rdlen = buffer_read_u16(buffer);
+    buffer_skip(buffer, opt_rdlen);
 
     if (opt_version != 0) {
         /* The only error is VERSION not implemented */
