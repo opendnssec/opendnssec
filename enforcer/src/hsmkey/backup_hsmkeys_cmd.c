3635f343848a84cdf05824398ea56f11bbcb730e
halderen <berry@nlnetlabs.nl>
2024-05-07 23:42:44 +0200

    Give error upon bad usage of command

--- enforcer/src/hsmkey/backup_hsmkeys_cmd.c
+++ enforcer/src/hsmkey/backup_hsmkeys_cmd.c
@@ -240,8 +240,10 @@ run(cmdhandler_ctx_type* context, int argc, char* argv[])
         status = rollback(sockfd, dbconn, clause_list);
     else if (ods_check_command(argv[1],"list"))
         status = list(sockfd, dbconn, clause_list);
-    else
+    else {
+        client_printf_err(sockfd, "Usage:\n\nbackup [list|prepare|commit|rollback]\n   --repository <repository>                    aka -r\n\n");
         status = -1;
+    }
 
     db_clause_list_free(clause_list);
     return status;
