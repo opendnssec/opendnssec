2125cbff45a7ddd2ce84e2475d0b39841b2d96fd
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-03-05 09:56:06 +0100

    Switch back to debug compile and using assert as it gives more useful failure reports.

--- common/test/test_ods_replace.c
+++ common/test/test_ods_replace.c
@@ -1,29 +1,22 @@
-// Tested with: gcc -O3 -I ../ test_ods_replace.c ../file.c ../log.c ../duration.c ; ./a.out
+// Tested with: gcc -I ../ test_ods_replace.c ../file.c ../log.c ../duration.c ; ./a.out
 
 #include "../file.h"
 #include <assert.h>
 #include <string.h>
 
-void check(int res) {
-    if (res != 0) {
-        printf("FAILED\n");
-        exit(1);
-    }
-}
-
 void main(void) {
     // Arguments are: haystack, needle, replacement
-    check(strcmp(ods_replace("", "", ""), ""));
-    check(strcmp(ods_replace("", "", "1"), "1"));
-    check(strcmp(ods_replace("", "1", ""), ""));
-    check(strcmp(ods_replace("", "1", "1"), ""));
-    check(strcmp(ods_replace("1", "", ""), "1"));
-    check(strcmp(ods_replace("1", "", "1"), "11"));
-    check(strcmp(ods_replace("1", "1", ""), ""));
-    check(strcmp(ods_replace("1", "1", "1"), "1"));
-    check(strcmp(ods_replace("xxx", "notfound", "replacement"), "xxx"));
-    check(strcmp(ods_replace("foundatstartxxx", "foundatstart", "replacement"), "replacementxxx"));
-    check(strcmp(ods_replace("xxxfoundinmiddlexxx", "foundinmiddle", "replacement"), "xxxreplacementxxx"));
-    check(strcmp(ods_replace("xxxfoundatend", "foundatend", "replacement"), "xxxreplacement"));
-    check(strcmp(ods_replace("/home/runner/ROOT/var/opendnssec/signer/test-notify-command.sh %zone %zonefile", "%zonefile", "/home/runner/ROOT/var/opendnssec/signed/ods"), "/home/runner/ROOT/var/opendnssec/signer/test-notify-command.sh %zone /home/runner/ROOT/var/opendnssec/signed/ods"));
+    assert(0 == strcmp(ods_replace("", "", ""), ""));
+    assert(0 == strcmp(ods_replace("", "", "1"), "1"));
+    assert(0 == strcmp(ods_replace("", "1", ""), ""));
+    assert(0 == strcmp(ods_replace("", "1", "1"), ""));
+    assert(0 == strcmp(ods_replace("1", "", ""), "1"));
+    assert(0 == strcmp(ods_replace("1", "", "1"), "11"));
+    assert(0 == strcmp(ods_replace("1", "1", ""), ""));
+    assert(0 == strcmp(ods_replace("1", "1", "1"), "1"));
+    assert(0 == strcmp(ods_replace("xxx", "notfound", "replacement"), "xxx"));
+    assert(0 == strcmp(ods_replace("foundatstartxxx", "foundatstart", "replacement"), "replacementxxx"));
+    assert(0 == strcmp(ods_replace("xxxfoundinmiddlexxx", "foundinmiddle", "replacement"), "xxxreplacementxxx"));
+    assert(0 == strcmp(ods_replace("xxxfoundatend", "foundatend", "replacement"), "xxxreplacement"));
+    assert(0 == strcmp(ods_replace("/home/runner/ROOT/var/opendnssec/signer/test-notify-command.sh %zone %zonefile", "%zonefile", "/home/runner/ROOT/var/opendnssec/signed/ods"), "/home/runner/ROOT/var/opendnssec/signer/test-notify-command.sh %zone /home/runner/ROOT/var/opendnssec/signed/ods"));
 }
